d29a52207733742932e6a121d9e73bfdf12f64bb
dtaylor-530 <d.taylor530@gmail.com>
2018-05-26 11:15:53 +0100

    initial

--- UtilityInterface/Class1.cs
+++ UtilityInterface/Class1.cs
@@ -4,5 +4,48 @@ namespace UtilityInterface
 {
     public class Class1
     {
+        public interface IKey
+        {
+            string Key { get; set; }
+
+        }
+
+
+        public interface IPeriodic
+        {
+
+            DateTime Start { get; set; }
+            DateTime End { get; set; }
+
+        }
+
+
+
+
+
+        public interface ITimeValue
+        {
+
+            double Value { get; set; }
+            DateTime Time { get; set; }
+
+
+        }
+
+
+
+        public interface IDistibuted
+        {
+
+            double Weight { get; set; }
+
+        }
+
+
+        public interface IDbRow
+        {
+            Int64 Id { get; set; }
+            Int64 ParentId { get; set; }
+        }
     }
 }
