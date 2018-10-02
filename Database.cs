9476a2df22407df51b912dcfcc09e9381efb4d35
dtaylor-530 <d.taylor530@gmail.com>
2018-10-02 10:03:51 +0100

    update

--- Database.cs
+++ Database.cs
@@ -6,38 +6,40 @@ namespace UtilityInterface.Database
 {
     public interface IKey
     {
-        string Key { get; set; }
+        String Key { get; set; }
 
     }
 
-
-    public interface IDbRow
+    public interface IId
     {
         Int64 Id { get; set; }
+    }
+
+
+    public interface IChildRow: IId
+    {
         Int64 ParentId { get; set; }
     }
 
 
-    public interface IDbTimeValue
+    public interface IChildRow<T> : IChildRow where T: IId
+    {
+        T Parent { get; set; }
+    }
+
+
+    public interface ITimeValue
     {
         Int64 Time { get; set; }
         Int64 Value { get; set; }
     }
 
-    public interface IDbPeriod
+    public interface IPeriod
     {
 
         Int64 Start { get; set; }
         Int64 End { get; set; }
-
-    }
-
-    public interface IDbRow<T>
-    {
-        Int64 Id { get; set; }
-        Int64 ParentId { get; }
-        T Parent { get; set; }
-
+        
     }
 
 }
