d0f857feeb45d741c89981b8eca00bfadb578328
dtaylor-530 <d.taylor530@gmail.com>
2020-09-20 13:19:31 +0100

    Adds ISetGuid and IDbEntity

---
+++ NonGeneric/Database/IDbEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityInterface.NonGeneric.Database
+{
+    public interface IDbEntity : IId, ISetId, IGuid, ISetGuid, IEquatable<IDbEntity>
+    {
+        DateTime AddedTime { get; set; }
+
+    }
+}
---
+++ NonGeneric/Database/ISetGuid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityInterface.NonGeneric.Database
+{
+    public interface ISetGuid
+    {
+        Guid Guid { set; }
+    }
+}
