91ae7e5038572ea23f73a4197c3d80444d41a3ee
dtaylor-530 <d.taylor530@gmail.com>
2020-09-20 10:09:29 +0100

    Adds ISetId

---
+++ Generic/Database/ISetId.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityInterface.Generic.Database
+{
+    public interface ISetId<T>
+    {
+        T Id { set; }
+    }
+}
