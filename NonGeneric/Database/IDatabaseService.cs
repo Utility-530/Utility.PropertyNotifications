1a5a853b1f4f1225d21ebe1f3710fde5a7690993
dtaylor-530 <d.taylor530@gmail.com>
2021-08-19 06:58:25 +0100

    Changes IDatabaseService to IRepository

--- NonGeneric/Database/IDatabaseService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace UtilityInterface.NonGeneric.Database
-{
-    public interface IDatabaseService : IDisposable, ISelectAll, ISelect, ISelectById, IInsert, IInsertBulk, IUpdate, IUpdateBulk, IDelete, IDeleteBulk, IDeleteById
-    {
-
-    }
-}
---
+++ NonGeneric/Database/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UtilityInterface.NonGeneric.Database
+{
+    public interface IRepository : IDisposable, ISelectAll, ISelect, ISelectById, IInsert, IInsertBulk, IUpdate, IUpdateBulk, IDelete, IDeleteBulk, IDeleteById
+    {
+
+    }
+}
