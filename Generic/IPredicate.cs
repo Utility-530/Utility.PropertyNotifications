471c4191b7c402b5db36c4931653cd3945b65ddf
dtaylor-530 <d.taylor530@gmail.com>
2021-09-26 21:36:12 +0100

    Adds IPredicate

---
+++ Generic/IPredicate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityInterface.Generic
+{
+
+    public interface IPredicate<T>
+    {
+        bool Check(T value);
+    }
+
+}
