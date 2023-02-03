b9334fe037d7bd40ff686e62043d96e6206a0397
dtaylor-530 <d.taylor530@gmail.com>
2023-02-03 12:52:26 +0000

    Readds WithOptions to AutofacHelper

--- Utility.Helpers.Ex/Autofac.cs
+++ Utility.Helpers.Ex/Autofac.cs
@@ -14,44 +14,41 @@ namespace Utility.Helpers.Ex
         }
 
 
-        //        public static IRegistrationBuilder<TR, IConcreteActivatorData, SingleRegistrationStyle> WithOptions<TR, TY>(
-        //this IRegistrationBuilder<TR, IConcreteActivatorData, TY> builder,
-        //bool autoActivate = false,
-        //bool asSelf = true,
-        //bool asImplementedInterfaces = true,
-        //bool singleInstance = true,
-        //bool watch = true)
-        //where TY : SingleRegistrationStyle
-        //where TR : class
-        //        {
+        public static IRegistrationBuilder<TR, IConcreteActivatorData, SingleRegistrationStyle> WithOptions<TR, TY>(
+this IRegistrationBuilder<TR, IConcreteActivatorData, TY> builder,
+bool autoActivate = false,
+bool asSelf = true,
+bool asImplementedInterfaces = true,
+bool singleInstance = true,
+bool watch = true)
+where TY : SingleRegistrationStyle
+where TR : class
+        {
 
-        //            //if (autoActivate)
-        //            //{
-        //            //    builder.AutoActivate();
-        //            //}
-        //            //builder.Watch();
-        //            return builder.AsSelf().AsImplementedInterfaces().SingleInstance();
-        //        }
+            //if (autoActivate)
+            //{
+            //    builder.AutoActivate();
+            //}
+            //builder.Watch();
+            return builder.AsSelf().AsImplementedInterfaces().SingleInstance();
+        }
 
-        //        public static IRegistrationBuilder<TR, IConcreteActivatorData, SingleRegistrationStyle> Watch<TR, TY>(
-        //      this IRegistrationBuilder<TR, IConcreteActivatorData, TY> builder, bool autoActivate = true)
-        //      where TY : SingleRegistrationStyle
-        //      where TR : class
-        //        {
+        public static IRegistrationBuilder<TR, IConcreteActivatorData, SingleRegistrationStyle> Watch<TR, TY>(
+      this IRegistrationBuilder<TR, IConcreteActivatorData, TY> builder, bool autoActivate = true)
+      where TY : SingleRegistrationStyle
+      where TR : class
+        {
+            //builder
+            //    .OnActivated(a =>
+            //    {
+            //        if ((a.Instance ?? throw new Exception("Instance is null")) is INode node)
+            //        {
+            //            var graphExample = a.Context.Resolve<GraphExample>();   
+            //            graphExample.BuildVertex(new GraphX.Measure.Point(0,0), node.Key);
+            //        }
+            //    });
 
-
-
-        //            //builder
-        //            //    .OnActivated(a =>
-        //            //    {
-        //            //        if ((a.Instance ?? throw new Exception("Instance is null")) is INode node)
-        //            //        {
-        //            //            var graphExample = a.Context.Resolve<GraphExample>();   
-        //            //            graphExample.BuildVertex(new GraphX.Measure.Point(0,0), node.Key);
-        //            //        }
-        //            //    });
-
-        //            return builder;
-        //        }
+            return builder;
+        }
     }
 }
