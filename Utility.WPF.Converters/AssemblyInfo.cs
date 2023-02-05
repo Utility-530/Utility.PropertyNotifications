71bc561f955a6bcf959366d1f9464de7edd948f7
dtaylor-530 <d.taylor530@gmail.com>
2023-02-05 12:00:07 +0000

    Changes namespace of Utility.WPF.Converters to http://schemas.utility.com/converters

--- Utility.WPF.Converters/AssemblyInfo.cs
+++ Utility.WPF.Converters/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Windows.Markup;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: XmlnsPrefix("http://schemas.utility.com/utilitywpf", "utl")]
-[assembly: XmlnsDefinition("http://schemas.utility.com/utilitywpf", "Utility.WPF.Converters")]
+[assembly: XmlnsPrefix("http://schemas.utility.com/converters", "conv")]
+[assembly: XmlnsDefinition("http://schemas.utility.com/converters", "Utility.WPF.Converters")]
