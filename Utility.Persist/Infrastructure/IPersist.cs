d8bd15cbe89c4d4b539f42a507fad2aaf911379b
dtaylor-530 <d.taylor530@gmail.com>
2023-03-01 12:28:18 +0000

    Add interface Changes name of project to Utility.Persists

---
+++ Utility.Persist/Infrastructure/IPersist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Persist.Infrastructure
+{
+    public interface IPersist
+    {
+
+        void Save(Guid key);
+
+
+
+        void Load(Guid key);
+
+
+    }
+}
