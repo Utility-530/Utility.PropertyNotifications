405c698424d9559ececdbbcadb07b6f4abff8d04
dtaylor-530 <d.taylor530@gmail.com>
2023-03-28 19:32:27 +0100

    Updates

--- Utility.Instructions.Demo/MainWindow.xaml.cs
+++ Utility.Instructions.Demo/MainWindow.xaml.cs
@@ -10,30 +10,16 @@ using System.Text.Json;
 
 namespace Utility.Instructions.Demo
 {
-
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        const string chars = "abcdefghijklmnopqrstuvwxyz";
-
-        public static char AlphabetCharacter(int index)
-        {
-            return chars[index % 26];
-        }
-        int count = -1;
-
-        public string character => AlphabetCharacter(count).ToString();
-
-        readonly Random random = new Random();
         DynamicTree<Persist> instructions;
         readonly DynamicTree<TreeState> history;
         readonly Service service;
         readonly Dictionary<Guid, View> views = new();
         readonly View root;
-        bool historyFlag;
 
         public MainWindow()
         {
@@ -51,7 +37,7 @@ namespace Utility.Instructions.Demo
                     int index = 0;
 
                     Update(state, index);
-                    if (historyFlag == false)
+                    if (state.State!=State.Default)
                     {
 
                         history.Data = state;
@@ -62,7 +48,7 @@ namespace Utility.Instructions.Demo
             history
                 .Subscribe(state =>
                 {
-                    if (historyFlag == true)
+                    if (state.State != State.Add)
                     {
                         instructions.OnNext(state);
                     }
@@ -71,7 +57,7 @@ namespace Utility.Instructions.Demo
             TreeView2.ItemsSource = instructions.Tree.Items;
 
             TreeView3.ItemsSource = service.Root.Items;
-            //TreeView4.ItemsSource = root.Items;
+ 
             TreeView5.ItemsSource = history.Tree.Items;
         }
 
@@ -134,14 +120,12 @@ namespace Utility.Instructions.Demo
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = false;
-            instructions.Data = new Persist{ Name = Randoms.Names.Random(random)};
+            instructions.Data = new Persist{ Name = TextBoxContent.Text};
             instructions.Add();
         }
 
         private void Remove_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = false;
             instructions.Remove();
         }
 
@@ -150,56 +134,37 @@ namespace Utility.Instructions.Demo
 
         }
 
-        //private void RemoveHistory_Click(object sender, RoutedEventArgs e)
-        //{
-        //    instructions.RemoveFuture();
-        //}
-
         private void Up_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = false;
-
             if (instructions.CanMoveUp == false)
                 throw new Exception("V3 fds");
             instructions.MoveUp();
-            //foreach (var instruction in _instructions)
-            //    implementor.OnPrevious(instruction);
         }
 
         private void Forward_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = false;
-
             if (instructions.CanMoveForward == false)
                 throw new Exception("V3 fds");
             instructions.MoveForward();
-            //implementor.OnNext(instruction);
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = false;
-
             if (instructions.CanMoveBack == false)
                 throw new Exception("V3 fds");
             instructions.MoveBack();
-            //implementor.OnPrevious(instruction);
         }
 
         private void Down_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = false;
-
             if (instructions.CanMoveDown == false)
                 throw new Exception("V3 fds");
             instructions.MoveDown();
-            //implementor.OnPrevious(instruction);
         }
 
 
         private void Next_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = false;
             if (history.CanMoveDown == false)
                 throw new Exception("V3 fds");
 
@@ -246,19 +211,14 @@ namespace Utility.Instructions.Demo
 
         private void UpHistory_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = true;
             if (history.CanMoveUp == false)
                 throw new Exception("V3 fds");
 
             history.MoveUp();
-            //var current = history.Current;
-
-            //instructions.OnPrevious(current.Data);
         }
 
         private void DownHistory_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = true;
             if (history.CanMoveDown == false)
                 throw new Exception("V3 fds");
 
@@ -267,8 +227,6 @@ namespace Utility.Instructions.Demo
 
         private void ForwardHistory_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = true;
-
             if (history.CanMoveForward == false)
                 throw new Exception("V3 fds");
             history.MoveForward();
@@ -276,8 +234,6 @@ namespace Utility.Instructions.Demo
 
         private void BackHistory_Click(object sender, RoutedEventArgs e)
         {
-            historyFlag = true;
-
             if (history.CanMoveBack == false)
                 throw new Exception("V3 fds");
             history.MoveBack();
