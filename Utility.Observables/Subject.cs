619b798455d742edc07fb108b53333b57b2ef07f
dtaylor-530 <d.taylor530@gmail.com>
2023-03-28 18:56:14 +0100

    Adds Subject class

---
+++ Utility.Observables/Subject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Observables
+{
+    public class Subject : IObservable, IObserver
+    {
+        public List<IObserver> Observers => throw new NotImplementedException();
+
+        public List<object> Observations => throw new NotImplementedException();
+
+
+        public void OnNext(object value)
+        {
+            Observations.Add(value);
+            foreach (var observer in Observers)
+                observer.OnNext(value);
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
