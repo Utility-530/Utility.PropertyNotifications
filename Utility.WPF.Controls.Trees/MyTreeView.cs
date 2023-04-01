b417c02e0db31bede94670f23012f78c2f090f13
dtaylor-530 <d.taylor530@gmail.com>
2023-04-01 14:12:23 +0100

    Adds MyTreeView

---
+++ Utility.WPF.Controls.Trees/MyTreeView.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Utility.WPF.Controls.Trees
+{
+    public class MyTreeView : TreeView
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new MyTreeViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is MyTreeViewItem;
+        }
+        protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+        {
+            if (newValue is INotifyCollectionChanged collectionChanged)
+                collectionChanged.CollectionChanged += CollectionChanged_CollectionChanged;
+        }
+
+        private void CollectionChanged_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            HasItems2 = ItemsSource.Cast<object>().Any();
+        }
+
+        public bool HasItems2
+        {
+            get { return (bool)GetValue(HasItems2Property); }
+            set { SetValue(HasItems2Property, value); }
+        }
+
+        public static readonly DependencyProperty HasItems2Property =
+            DependencyProperty.Register("HasItems2", typeof(bool), typeof(MyTreeView), new PropertyMetadata(false));
+    }
+
+    public class MyTreeViewItem : TreeViewItem
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new MyTreeViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is MyTreeViewItem;
+        }
+
+        protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+        {
+            if (newValue is INotifyCollectionChanged collectionChanged)
+                collectionChanged.CollectionChanged += CollectionChanged_CollectionChanged;
+        }
+
+        private void CollectionChanged_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            HasItems2 = ItemsSource.Cast<object>().Any();
+        }
+
+        public bool HasItems2
+        {
+            get { return (bool)GetValue(HasItems2Property); }
+            set { SetValue(HasItems2Property, value); }
+        }
+
+        public static readonly DependencyProperty HasItems2Property =
+            DependencyProperty.Register("HasItems2", typeof(bool), typeof(MyTreeViewItem), new PropertyMetadata(false));
+
+
+    }
+}
