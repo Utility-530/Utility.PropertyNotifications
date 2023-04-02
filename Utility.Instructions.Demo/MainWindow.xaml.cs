0f9f671aa720d979527b06cb65423769489f97a5
dtaylor-530 <d.taylor530@gmail.com>
2023-04-02 19:01:49 +0100

    Introduces DynamicTreeView to MainWindow Removes redundant code Fixes what remains

--- Utility.Instructions.Demo/MainWindow.xaml.cs
+++ Utility.Instructions.Demo/MainWindow.xaml.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Collections.Generic;
 using Utility.Trees;
-using System.Collections;
-using System.Text.Json;
 
 namespace Utility.Instructions.Demo
 {
@@ -15,153 +9,46 @@ namespace Utility.Instructions.Demo
     /// </summary>
     public partial class MainWindow : Window
     {
-        DynamicTree<Persist> instructions;
-        readonly DynamicTree<ITree<Persist>> history;
-        readonly Service service;
-        readonly Dictionary<Guid, View> views = new();
+        DynamicTree instructions;
+        readonly DynamicTree history;
 
         public MainWindow()
         {
             InitializeComponent();
             instructions = new(new Tree<Persist>(new Persist() { Name = "root" }) { });
-            history = new(new Tree<ITree<Persist>>(instructions.Current));
+            history = new();
             instructions
-                .Subscribe(state =>
+                .Subscribe(tree =>
                 {
                     int index = 0;
 
-                   // Update(state, index);
-                    if (state.State != State.Default)
+                    // Update(state, index);
+                    if (history.Items.Count == 0)
                     {
-
-                        history.Data = state;
-                        history.State = State.Add;
+                        history.Current = new Tree(tree);
+                    }
+                    else if(tree != history.Current.Data)
+                    {
+                        history.Data = tree as ITree<Persist>;
+                        history.State = State.Down;
                     }
                 });
 
             history
-                .Subscribe(state =>
+                .Subscribe(tree =>
                 {
-                    if (state.State != State.Add)
+                    if (tree.State != State.Add)
                     {
-                        instructions.OnNext(state.Data);
+                        instructions.Current = tree.Data as ITree<Persist>;
                     }
                 });
 
-            TreeView3.ItemsSource = instructions.Tree.Items; // service.Root.Items;
-
-            TreeView5.ItemsSource = history.Tree.Items;
-        }
-
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
-            instructions.Data = new Persist { Name = TextBoxContent.Text };
-            instructions.State = State.Add;
-        }
-
-        private void Remove_Click(object sender, RoutedEventArgs e)
-        {
-            instructions.State = State.Remove;
-        }
-
-        private void TreeView_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-        {
-
-        }
-
-        private void Up_Click(object sender, RoutedEventArgs e)
-        {
-            instructions.State = State.Up;
-        }
-
-        private void Forward_Click(object sender, RoutedEventArgs e)
-        {
-
-            instructions.State = State.Forward;
-
-        }
-
-        private void Back_Click(object sender, RoutedEventArgs e)
-        {
-            instructions.State = State.Back;
-
-        }
-
-        private void Down_Click(object sender, RoutedEventArgs e)
-        {
-
-            instructions.State = State.Down;
+            ContentControl1.Content = instructions;
+            ContentControl2.Content = history;
 
         }
 
 
-        private void Next_Click(object sender, RoutedEventArgs e)
-        {
-            var current = history.Current;
-            instructions.OnNext(current.Data);
-
-        }
-
-        private void Previous_Click(object sender, RoutedEventArgs e)
-        {
-
-
-        }
-
-
-        private void Refresh_Click(object sender, RoutedEventArgs e)
-        {
-            this.TreeView.ItemTemplateSelector = null;
-            this.TreeView.ItemContainerStyleSelector = null;
-            this.TreeView.ItemContainerStyle = null;
-            this.TreeView.ItemTemplateSelector = MyDataTemplateSelector.Instance;
-            this.TreeView.ItemContainerStyleSelector = MyStyleSelector.Instance;
-        }
-
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            TreePersist.Instance.Save(instructions.Tree);
-        }
-
-        private void Load_Click(object sender, RoutedEventArgs e)
-        {
-            var load = TreePersist.Instance.Load<Persist>();
-            instructions = new(load);
-            TreeView2.ItemsSource = instructions.Tree.Items;
-        }
-
-        private void Run_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        #region History
-
-
-        private void UpHistory_Click(object sender, RoutedEventArgs e)
-        {
-            history.State = State.Up;
-        }
-
-        private void DownHistory_Click(object sender, RoutedEventArgs e)
-        {
-
-            history.State = State.Down;
-        }
-
-        private void ForwardHistory_Click(object sender, RoutedEventArgs e)
-        {
-
-            history.State = State.Forward;
-        }
-
-        private void BackHistory_Click(object sender, RoutedEventArgs e)
-        {
-
-            history.State = State.Back;
-        }
-
-        #endregion History
     }
 
 }
