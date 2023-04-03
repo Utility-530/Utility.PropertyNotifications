d14d7f22e97ef82dd3b679af316f7e6f8ab01285
dtaylor-530 <d.taylor530@gmail.com>
2023-04-03 11:46:00 +0100

    Adds demo usercontrol HasItems2UserControl

---
+++ Utility.WPF.Demo.Trees/HasItem2UserControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using Utility.Trees;
+
+namespace Utility.WPF.Demo.Trees
+{
+    /// <summary>
+    /// Interaction logic for HasItem2UserControl.xaml
+    /// </summary>
+    public partial class HasItem2UserControl : UserControl
+    {
+        public HasItem2UserControl()
+        {
+            InitializeComponent();
+
+            TreeView.ItemsSource = new[] { tree }; 
+            MyTreeView.ItemsSource = new[] { tree }; 
+        }
+
+        Tree tree = new Tree("root")
+        {
+
+        };
+
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            tree.Add("new");
+        }
+    }
+
+
+    
+}
