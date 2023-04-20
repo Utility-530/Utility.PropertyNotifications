80248d6454112fdb0d75d6b8a591b9e1b66a4cd4
dtaylor-530 <d.taylor530@gmail.com>
2023-04-20 11:48:00 +0100

    Initialise repository

---
+++ CoffeeFlow.WPF.Infrastructure/Infrastructure/Globals.cs
@@ -0,0 +1,25 @@
+﻿using CoffeeFlow.ViewModel;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CoffeeFlow.WPF.Infrastructure
+{
+    public class Globals
+    {
+        private Globals()
+        {
+
+        }
+
+        public Window MainWindow { get; set; }
+
+        public Dispatcher Dispatcher { get; set; }
+
+        public MainViewModel Main { get; set; }
+
+        public NetworkViewModel Network { get; set; }
+
+        public static Globals Instance { get; } = new();
+
+    }
+}
