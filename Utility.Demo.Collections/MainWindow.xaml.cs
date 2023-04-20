e508a4fd4687dc255ea13af72f2fd69684d082ea
dtaylor-530 <d.taylor530@gmail.com>
2023-04-20 11:36:58 +0100

    Add project files.

---
+++ Utility.Demo.Collections/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Utility.Collections;
+
+namespace Utility.WPF.Demo.Collections
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Collection.Context = System.Threading.SynchronizationContext.Current;
+            JulMar.Windows.Collections.Collection.Context = System.Threading.SynchronizationContext.Current;
+
+        }
+    }
+}
