8c5915d88bb6355d165b1fd7363112fc1b56d050
dtaylor-530 <d.taylor530@gmail.com>
2023-04-25 06:04:58 +0100

    Initialises

---
+++ Utility.Conversions/Helpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Conversions
+{
+    public static class Helpers
+    {
+        public static bool IsNullable(this Type type)
+        {
+            if (type == null)
+            {
+                return false;
+            }
+
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+    }
+}
