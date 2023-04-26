3f47a172fc73c058f1450623daf78f5c30bac28a
dec-lan-tay-lor <declan.taylor@puttshack.com>
2023-04-26 14:47:12 +0100

    Improves

--- Utility.Interfaces/Generic/IKey.cs
+++ Utility.Interfaces/Generic/IKey.cs
@@ -3,7 +3,7 @@ using Utility.Interfaces.NonGeneric;
 
 namespace Utility.Interfaces.Generic
 {
-    public interface IKey<T> : IEquatable<IKey<T>>
+    public interface IKey<T> : IEquatable<IKey<T>>, IEquatable
     {
         T Key { get; }
     }
