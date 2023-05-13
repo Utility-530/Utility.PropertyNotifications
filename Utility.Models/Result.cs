e3b79f751af7bfaf75d61c81f63e5c4734e5fcbc
dec-lan-tay-lor <declan.taylor@puttshack.com>
2023-05-13 08:03:53 +0100

    Adds Result

---
+++ Utility.Models/Result.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Utility.Models
+{
+    public record Cause;
+
+    public record Effect;
+
+    public record Response(double Completed, double Total): Effect
+    {
+        public Response() : this(1, 1)
+        {
+        }
+
+        public bool IsComplete => Completed == Total;
+    }
+
+    public record Request() : Cause;
+
+    public record Event(): Cause;
+
+}
