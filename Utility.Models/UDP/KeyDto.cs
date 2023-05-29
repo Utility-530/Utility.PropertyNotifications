559820242cb3f7cc727c64980feaa9f6b75be6e3
dec-lan-tay-lor <declan.taylor@puttshack.com>
2023-05-29 10:17:11 +0100

    Adds Guid and KeyDto

---
+++ Utility.Models/UDP/KeyDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Models.UDP
+{
+    public record KeyDto(Guid Guid, string Name);
+
+    public record GuidDto(Guid Source);
+
+    public record TypeDto(string InName, string OutName);
+
+}
