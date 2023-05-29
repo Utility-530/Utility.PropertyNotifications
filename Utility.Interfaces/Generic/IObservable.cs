fa3c49e06519592840da91f48fa546b0bda4a850
dec-lan-tay-lor <declan.taylor@puttshack.com>
2023-05-29 14:15:19 +0100

    Adds IObserver and IObservable to Generic

---
+++ Utility.Interfaces/Generic/IObservable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Utility.Interfaces.Generic
+{
+    public interface IObservable<T> : IEnumerable
+    {
+        IEnumerable<IObserver<T>> Observers { get; }
+
+        IDisposable Subscribe(Utility.Interfaces.Generic.IObserver<T> value);
+
+    }
+}
---
+++ Utility.Interfaces/Generic/IObserver.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Utility.Interfaces.Generic
+{
+    public interface IObserver<T> : System.IObserver<T>
+    {
+        void OnProgress(int complete, int total);
+    }
+}
