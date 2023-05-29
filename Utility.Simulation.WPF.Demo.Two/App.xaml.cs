781d63b4d2590c2338392384eb11574d7e339d30
dtaylor-530 <d.taylor530@gmail.com>
2023-05-29 17:08:08 +0100

    Adds external projects and new demo project

---
+++ Utility.Simulation.WPF.Demo.Two/App.xaml.cs
@@ -0,0 +1,69 @@
+﻿//using Autofac;
+using DryIoc;
+using Utility.Nodify.Core;
+using Utility.Nodify.Demo.Infrastructure;
+using Utility.Nodify.Operations;
+using System;
+using System.Windows;
+using Utility.Models;
+using Utility.Nodify.Operations.Infrastructure;
+using System.Threading;
+using Utility.Simulation.Infrastructure;
+using Utility.Simulation.Service;
+using Utility.Simulation.ViewModel;
+//using Resolver = Utility.Nodify.Operations.Resolver;
+using Message = Utility.Nodify.Operations.Message;
+using Utility.Collections;
+
+namespace Utility.Nodify.Demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var container = Bootstrapper.Build();
+
+
+            var dateTimeRange = new TimeTrack(DateTime.UnixEpoch, DateTime.Now);
+            var engine = new Utility.Simulation.Service.Engine(dateTimeRange);
+
+            var playerViewModel = new PlayerViewModel(SynchronizationContext.Current);
+            var playerengineService = new PlayerEngineService();
+            var rateViewModel = new RateViewModel(0.1, 8, 5);
+            var progressViewModel = new ProgressViewModel(SynchronizationContext.Current, dateTimeRange.Start, dateTimeRange.End);
+
+            //container.RegisterInstance(dateTimeRange);
+
+            container.RegisterInstance<object>(engine);
+            container.RegisterInstance<object>(playerViewModel);
+            container.RegisterInstance<object>(playerengineService);
+            container.RegisterInstance<object>(rateViewModel);
+            container.RegisterInstance<object>(progressViewModel);
+
+
+            container.Register<Diagram, Diagram1>();
+            container.Register<IOperationsFactory, DynamicOperationsFactory>();
+
+            ThreadSafeObservableCollection<Message>.Context = SynchronizationContext.Current;
+
+  
+            base.OnStartup(e);
+
+            Window window = new (){ Content = new MainViewModel { Player = playerViewModel, Rate = rateViewModel, Progress = progressViewModel } };
+
+            DockWindow dockWindow = new()
+            {
+                DataContext = container.Resolve<Utility.Nodify.Engine.ViewModels.MainViewModel>()
+            };
+
+            window.Show();
+            dockWindow.Show();
+        }
+    }
+
+
+
+}
---
+++ Utility.Simulation.WPF.Demo.Two/Diagrams/Operation.cs
@@ -0,0 +1,42 @@
+﻿using DryIoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utility.Nodify.Operations;
+using Utility.Nodify.Operations.Infrastructure;
+
+namespace Utility.Nodify.Demo.Infrastructure
+{
+
+    public class DynamicOperationsFactory : IOperationsFactory
+    {
+        private readonly IContainer container;
+
+        public DynamicOperationsFactory(IContainer container)
+        {
+            this.container = container;
+        }
+
+        public IEnumerable<OperationInfo> GetOperations()
+        {
+            return container.Resolve<OperationInfo[]>();
+     
+        }
+    }
+
+    public class LambdaOperation : IOperation
+    {
+        private readonly Func<IOValue[], IOValue[]> func;
+
+        public LambdaOperation(Func<IOValue[], IOValue[]> func)
+        {
+            this.func = func;
+        }
+
+        public IOValue[] Execute(params IOValue[] operands)
+        {
+            return func(operands);
+        }
+    }
+
+}
