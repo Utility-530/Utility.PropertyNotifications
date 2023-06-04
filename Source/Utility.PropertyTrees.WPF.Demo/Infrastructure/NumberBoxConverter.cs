0dd39b3911345efbefc970b92b8393f3f8051646
dec-lan-tay-lor <declan.taylor@puttshack.com>
2023-06-04 08:24:21 +0100

    Uses NumberBox in DataTemplate for Integer Adds NumberBoxConverter

---
+++ Source/Utility.PropertyTrees.WPF.Demo/Infrastructure/NumberBoxConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Utility.PropertyTrees.WPF.Demo
+{
+    public class NumberBoxConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return double.Parse(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.Parse(value.ToString());
+        }
+
+        public static NumberBoxConverter Instance { get; } = new NumberBoxConverter();  
+    }
+}
