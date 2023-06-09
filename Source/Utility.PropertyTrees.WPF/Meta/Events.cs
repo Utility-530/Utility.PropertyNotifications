37f1d04ce7c8217f95e1ee768c25265789432ee2
dec-lan-tay-lor <declan.taylor@puttshack.com>
2023-06-09 08:48:57 +0100

    Adds TreeMouseDoubleClickEvent

--- Source/Utility.PropertyTrees.WPF/Meta/Events.cs
+++ Source/Utility.PropertyTrees.WPF/Meta/Events.cs
@@ -7,5 +7,6 @@ namespace Utility.PropertyTrees.WPF.Meta
     public record TreeViewRequest(TreeView TreeView, ValueNode PropertyNode) : Request;
     public record TreeViewResponse(TreeView TreeView) : Response(TreeView);
     public record TreeClickEvent(ValueNode ValueNode) : Event;
+    public record TreeMouseDoubleClickEvent(ValueNode ValueNode) : Event;
 
 }
