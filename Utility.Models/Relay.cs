f9e0720612363e3c6126f039a2c2f013d68d3f1b
dtaylor-530 <d.taylor530@gmail.com>
2023-06-13 12:18:56 +0100

    Comments out Relay class

--- Utility.Models/Relay.cs
+++ Utility.Models/Relay.cs
@@ -4,19 +4,19 @@ using Utility.Observables.Generic;
 
 namespace Utility.Models
 {
-    public abstract class Relay : Relay<Message>, IKey<Key>, IRelay
-    {
-        public abstract Key Key { get; }
+    //public abstract class Relay : Relay<Message>, IKey<Key>, IRelay
+    //{
+    //    public abstract Key Key { get; }
 
-        public bool Equals(IKey<Key>? other)
-        {
-            return other?.Key == Key;
-        }
-        public bool Equals(IEquatable? other)
-        {
-            return Equals(other as IKey<Key>);
-        }
-    }
+    //    public bool Equals(IKey<Key>? other)
+    //    {
+    //        return other?.Key == Key;
+    //    }
+    //    public bool Equals(IEquatable? other)
+    //    {
+    //        return Equals(other as IKey<Key>);
+    //    }
+    //}
 
     public interface IRelay : IRelay<Message>, IKey<Key>
     {
