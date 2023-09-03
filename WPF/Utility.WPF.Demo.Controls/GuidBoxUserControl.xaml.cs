1e0b5da308d519fb97d6d79f2a1894a91af9876e
dtaylor-530 <d.taylor530@gmail.com>
2023-09-03 12:45:45 +0100

    Utility.WPF.Demo.Controls Adds GuidBoxUserControl

---
+++ WPF/Utility.WPF.Demo.Controls/GuidBoxUserControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Utility.WPF.Controls;
+
+namespace Utility.WPF.Demo.Controls
+{
+    /// <summary>
+    /// Interaction logic for GuidBoxUserControl.xaml
+    /// </summary>
+    public partial class GuidBoxUserControl : UserControl
+    {
+        public GuidBoxUserControl()
+        {
+            InitializeComponent();
+            One.Guid = Guid.NewGuid();
+            var binding = new Binding { Source = Guid.NewGuid() };
+            Two.SetBinding(GuidBox.GuidProperty, binding);
+        }
+    }
+}
