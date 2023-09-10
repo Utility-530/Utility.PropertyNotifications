f9c2629119b225013205ee1d1db774d0d09e8c57
dtaylor-530 <d.taylor530@gmail.com>
2023-09-10 11:40:18 +0100

    Utility.Guids Adds project

---
+++ Infrastructure/Utility.Guids/Guids.cs
@@ -0,0 +1,40 @@
+﻿namespace Utility
+{
+
+    public class Guids
+    {
+        public static readonly Guid History = Guid.Parse("58a13c8e-d9f4-4117-ab37-dacd58c45340");
+        public static readonly Guid Playback = Guid.Parse("07e2070f-3aa6-43e6-8924-d705c7f958af");
+        public static readonly Guid PropertyStore = Guid.Parse("f04c2e55-bf33-480c-a4e7-b4b7804d1735");
+        public static readonly Guid Resolver = Guid.Parse("222b7df3-9ae7-49d6-aabc-a492c6254718");
+        public static readonly Guid PropertyFilter = Guid.Parse("215cdcc6-ac25-41a7-b482-fdf1a58b0ecd");
+        public static readonly Guid PropertyActivator = Guid.Parse("24fadcc6-ac25-41a7-b482-fdf1a58b0ecd");
+        public static readonly Guid MethodActivator = Guid.Parse("72e33ffd-6ef6-4b33-b050-290fa7c97cc7");
+        public static readonly Guid DescriptorFilter = Guid.Parse("fca6811e-8d33-4863-a2cd-c200ac10189d");
+        public static readonly Guid MethodParameterActivator = Guid.Parse("ffabb47f-acad-45b3-a670-ab609d761fca");
+        public static readonly Guid MethodsExplorer = Guid.Parse("ffabb47f-acad-45b3-a670-ab609d761fca");
+
+
+        public static readonly Guid UdpServer = Guid.Parse("2978ac5e-368f-42b3-891e-d85c21c14499");
+        public static readonly Guid Property = Guid.Parse("e15c2e55-bf33-480c-a4e7-b4b7804d1735");
+        public static readonly Guid Interface = Guid.Parse("8eeb7df3-9ae7-49d6-aabc-a492c6254718");
+        public static readonly Guid Bootstrapper = Guid.Parse("111b7df3-9ae7-49d6-aabc-a492c6254718");
+        public static readonly Guid ViewModelEngine = Guid.Parse("78f35bd1-fc3c-44ca-8d86-f3a8a9d69d33");
+        public static readonly Guid ModelController = Guid.Parse("e123dab9-9022-4649-9dc4-754d492dd5a6");
+        public static readonly Guid ViewModelController = Guid.Parse("605d53f3-c195-461f-bada-0e41ef2f658c");
+        public static readonly Guid ViewController = Guid.Parse("ab289f2b-2fab-4c87-9b85-0946bad7b1fd");
+        public static readonly Guid RepositorySwitch = Guid.Parse("fca6811e-8d33-4863-a2cd-c200ac10189d");
+
+
+        public static readonly Guid ViewBuilder = Guid.Parse("26bb7df3-9ae7-49d6-aabc-a492c6254718");
+        public static readonly Guid MethodBuilder = Guid.Parse("06b8fedd-8188-47b3-b93c-76137033bb4b");
+        public static readonly Guid VisibilityController = Guid.Parse("bcbd8fe7-cbc3-442f-ae62-13c9845393a3");
+
+
+        public static readonly Guid Http = Guid.Parse("e5383010-93e4-4a20-be00-91261cbb7400");
+        public static readonly Guid LiteDB = Guid.Parse("5a704101-a493-44e1-b2fa-01834228f1fe");
+        public static readonly Guid SQLite = Guid.Parse("cb0a690e-d9d1-4791-a45e-00314ceea0db");
+        public static readonly Guid Tree = Guid.Parse("8ad10073-b168-47ea-bf72-a98287ba0c97");
+        public static readonly Guid InMemory = Guid.Parse("5bfbfbcc-a7c3-4970-94c9-111f8a8c76ac");
+    }
+}
