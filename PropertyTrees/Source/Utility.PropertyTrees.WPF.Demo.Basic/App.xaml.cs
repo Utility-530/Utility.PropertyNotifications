1902705e9099ece98ebb0a15482a5545ade8e258
dtaylor-530 <d.taylor530@gmail.com>
2023-10-15 19:53:37 +0100

    Utility.PropertyTrees.WPF.Demo.Basic Adds project

---
+++ PropertyTrees/Source/Utility.PropertyTrees.WPF.Demo.Basic/App.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Utility.Collections;
+using Application = System.Windows.Application;
+using Utility.Infrastructure;
+using Resolver = Utility.PropertyTrees.Services.Resolver;
+using System.Windows;
+using System.Threading;
+using DryIoc;
+using System.Collections.Specialized;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace Utility.PropertyTrees.WPF.Demo.Basic
+{
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SQLitePCL.Batteries.Init();
+            Collection.Context = BaseObject.Context = SynchronizationContext.Current ?? throw new System.Exception("sd w3w");
+
+            var property = Initialise();
+            ListBox listBox = new()
+            {
+                ItemsSource = property.Children
+            };
+
+            //property
+            //    .Children
+            //    .Subscribe(item =>
+            //    {
+            //        if (item is not NotifyCollectionChangedEventArgs args)
+            //            throw new Exception("rev re");
+
+            //        foreach(INode node in SelectNewItems<INode>(args))
+            //        {
+
+            //        }
+            //    },
+            //    e =>
+            //    {
+
+            //    },
+            //    () =>
+            //    {
+            //        //state.OnNext(State.Completed);
+            //    }
+            //  );
+
+
+            var window = new Window { Content = listBox };
+            window.Show();
+            base.OnStartup(e);
+
+//#if DEBUG
+//            Tracing.Enable();
+//#endif
+        }
+
+        private RootModelProperty Initialise()
+        {
+
+            var container = BootStrapper.Build();
+            var resolver = new Resolver(container);
+            BaseObject.Resolver = resolver;
+            resolver.Initialise();
+            var property = container.Resolve<RootModelProperty>();
+            return property;
+        }
+
+        private static IEnumerable<T> SelectNewItems<T>(NotifyCollectionChangedEventArgs args)
+        {
+            return args.NewItems?.Cast<T>() ?? Array.Empty<T>();
+        }
+
+    }
+
+
+}
---
+++ PropertyTrees/Source/Utility.PropertyTrees.WPF.Demo.Basic/Infrastructure/RepositorySwitchController.cs
@@ -0,0 +1,24 @@
+﻿using Utility.Infrastructure;
+using Utility.Models;
+using static Utility.Observables.Generic.ObservableExtensions;
+using Utility.Observables.NonGeneric;
+using Utility.Repos;
+using System;
+
+namespace Utility.PropertyTrees.Services
+{
+    internal class RepositorySwitchController : BaseObject
+    {
+        public override Key Key => new Key<RepositorySwitchController>(Guids.RepositorySwitch);
+
+        public Interfaces.Generic.IObservable<RepositorySwitchResponse> OnNext(RepositorySwitchRequest request)
+        {
+            return Create<RepositorySwitchResponse>(observer =>
+            {
+
+                observer.OnNext(new RepositorySwitchResponse(new Key<InMemoryRepository>(Utility.Guids.InMemory)));
+                return Disposer.Empty;
+            });
+        }
+    }
+}
