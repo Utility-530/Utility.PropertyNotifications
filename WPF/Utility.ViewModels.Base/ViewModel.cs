7d4fca042d4f233fc87db6fd01c5de044904d1f6
dtaylor-530 <d.taylor530@gmail.com>
2023-11-26 17:44:44 +0000

    Utility.ViewModels.Base Adds BaseViewModel and ViewModel

---
+++ WPF/Utility.ViewModels.Base/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.Interfaces.NonGeneric;
+
+namespace Utility.ViewModels.Base
+{
+    public class ViewModel: BaseViewModel, IValue
+    {
+        public object Value
+        {
+            //get => Get<object>(); set => Set(value);
+            get;set;
+        }
+    }
+}
