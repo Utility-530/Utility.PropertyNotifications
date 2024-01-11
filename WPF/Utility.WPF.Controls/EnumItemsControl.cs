1f6aa622fb245ae5a09e45c8205dfbe970d763ed
dtaylor-530 <d.taylor530@gmail.com>
2024-01-11 19:14:15 +0000

    Utility.WPF.Controls Fixes

--- WPF/Utility.WPF.Controls/EnumItemsControl.cs
+++ WPF/Utility.WPF.Controls/EnumItemsControl.cs
@@ -18,6 +18,7 @@ using System.Reactive.Subjects;
 using MintPlayer.ObservableCollection;
 using Utility.WPF.Helpers;
 using Utility.Models;
+using Utility.Infrastructure;
 
 namespace Utility.WPF.Controls
 {
