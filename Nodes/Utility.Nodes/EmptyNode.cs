12c864bb2cdbb71646b982c44abef729f4602d8c
dtaylor-530 <d.taylor530@gmail.com>
2024-01-20 22:19:44 +0000

    Utility.Nodes Adds EmptyNode

---
+++ Nodes/Utility.Nodes/EmptyNode.cs
@@ -0,0 +1,22 @@
+﻿using Utility.Trees.Abstractions;
+
+namespace Utility.Nodes
+{
+    public class EmptyNode : Node
+    {
+        public override async Task<object?> GetChildren()
+        {
+            return await Task.FromResult(Array.Empty<object>());
+        }
+
+        public override async Task<bool> HasMoreChildren()
+        {
+            return await Task.FromResult(false);
+        }
+
+        public override IReadOnlyTree ToNode(object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
