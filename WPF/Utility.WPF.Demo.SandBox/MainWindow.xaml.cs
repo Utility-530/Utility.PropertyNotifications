60194f5c3186c63973b30ff9dbf9b109171c6651
dtaylor-530 <d.taylor530@gmail.com>
2024-01-27 10:23:30 +0000

    Utility.WPF.Demo.SandBox Adds to

--- WPF/Utility.WPF.Demo.SandBox/MainWindow.xaml.cs
+++ WPF/Utility.WPF.Demo.SandBox/MainWindow.xaml.cs
@@ -1,5 +1,12 @@
-﻿using System.Collections;
+﻿using Jellyfish;
+using System;
+using System.Collections;
+using System.Linq;
 using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media.Animation;
+using Utility.WPF.Adorners;
+using Utility.WPF.Helpers;
 
 namespace Utility.WPF.Demo.SandBox
 {
@@ -12,6 +19,57 @@ namespace Utility.WPF.Demo.SandBox
         {
             InitializeComponent();
         }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //VisualStateManager.GoToElementState(this.Button3, "Pressed", false);
+
+        //    if (AdornerLayer.GetAdornerLayer(Button3) is { } adornerLayer)
+        //    {
+        //        adornerLayer.Clear(TextBox1);
+        //        AdornerLayer? adLayer = AdornerLayer.GetAdornerLayer(Button3);
+        //        RelayCommand relayCommand = new RelayCommand((e) => {
+        //            Button2_Click(default, default);
+
+        //        });
+        //        var adorner = new CancelAdorner(TextBox1, relayCommand) { Opacity = 0 };
+    
+
+        //        adLayer.Add(adorner);
+
+        //        DoubleAnimation doubleAnimation = new (1, new Duration(TimeSpan.FromSeconds(1)));
+        //        doubleAnimation.Completed += fadeOutAnimation_Completed;
+        //        doubleAnimation.Freeze();
+        //        adorner.BeginAnimation(UIElement.OpacityProperty, doubleAnimation);
+
+        //    }
+
+        //}
+
+        //private void fadeOutAnimation_Completed(object sender, EventArgs e)
+        //{
+        //    ;
+        //}
+
+        //private void Button2_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (AdornerLayer.GetAdornerLayer(Button3) is { } adornerLayer)
+        //    {
+        //        adornerLayer.Clear(TextBox1);
+        //        AdornerLayer? adLayer = AdornerLayer.GetAdornerLayer(Button3);
+        //        RelayCommand relayCommand = new RelayCommand((e) => {
+        //            Button_Click(default, default);
+        //        });
+        //        var adorner = new MaskAdorner(TextBox1, relayCommand) { Opacity=0};
+        //        adLayer.Add(adorner);
+
+        //        DoubleAnimation doubleAnimation = new DoubleAnimation(1, new Duration(TimeSpan.FromSeconds(1)));
+        //        doubleAnimation.Completed += fadeOutAnimation_Completed;
+        //        doubleAnimation.Freeze();
+        //        adorner.BeginAnimation(UIElement.OpacityProperty, doubleAnimation);
+
+        //    }
+        //}
     }
 
     public class ViewModel
