91fcf6417f76af16963d6ffb1eb6d1a195bd48ae
dtaylor-530 <d.taylor530@gmail.com>
2024-01-29 17:35:09 +0000

    Utility.ViewModels Removes value types from ViewModel

--- Infrastructure/Utility.ViewModels/ViewModel.cs
+++ Infrastructure/Utility.ViewModels/ViewModel.cs
@@ -4,45 +4,9 @@ namespace Utility.ViewModels
 {
     public class ViewModel : NotifyPropertyChangedBase
     {
-        private bool booleanValue;
-        private object referenceValue;
-        #region values
-
-        public string StringValue { get; set; }
-
-        public bool BooleanValue
-        {
-            get => booleanValue; set
-            {
-                booleanValue = value;
-                RaisePropertyChanged();
-
-            }
-        }
-        public double NumberValue { get; set; }
-        public DateTime DateTimeValue { get; set; }
-        public int IntegerValue { get; set; }
-
-        public object ReferenceValue
-        {
-            get
-            {
-                return referenceValue;
-            }
-
-            set
-            {
-                referenceValue = value;
-                RaisePropertyChanged();
-            }
-        }
-
-
-        #endregion values
 
 
         public Guid Guid { get; set; }
-        public Guid ParentGuid { get; set; }
         public string Name { get; set; }
         public bool IsExpanded { get; set; }
         public bool IsSelected { get; set; }
@@ -60,7 +24,7 @@ namespace Utility.ViewModels
         public double? Width { get; set; }
         public double? Height { get; set; }
 
-        public Orientation? Orientation { get; set; }
+        public Orientation Orientation { get; set; }
 
         public DataPresentationType DataPresentationType { get; set; }
 
@@ -71,7 +35,7 @@ namespace Utility.ViewModels
         public Arrangement ItemsPanelType { get; set; }
 
 
-        public Orientation? ItemsPanelOrientation { get; set; }
+        public Orientation ItemsPanelOrientation { get; set; }
 
         public int? Rows { get; set; }
 
