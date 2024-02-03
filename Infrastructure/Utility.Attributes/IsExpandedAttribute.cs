ec341ae648904545655e73ff2d1a9154496a8614
dtaylor-530 <d.taylor530@gmail.com>
2024-02-03 01:20:48 +0000

    Utility.Attributes Adds IsExpandedAttribute

---
+++ Infrastructure/Utility.Attributes/IsExpandedAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Utility.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class IsExpandedAttribute : Attribute
+    {
+        public IsExpandedAttribute(bool isExpanded)
+        {
+            IsExpanded = isExpanded;
+        }
+
+        public bool IsExpanded { get; }
+    }
+}
