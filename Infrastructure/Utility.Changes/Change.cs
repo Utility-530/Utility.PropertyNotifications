69ed8365a4172e879f184e2a18de315a03d561b0
dtaylor-530 <d.taylor530@gmail.com>
2024-02-05 23:54:05 +0000

    Utility.Changes Adds project

---
+++ Infrastructure/Utility.Changes/Change.cs
@@ -0,0 +1,22 @@
+﻿namespace Utility.Changes
+{
+    public record Change
+    {
+        public Change(object value, Type type, int? count = default)
+        {
+            Value = value;
+            Type = type;
+            Count = count;
+        }
+
+        public object Value { get; }
+
+        public Type Type { get; init; }
+        public int? Count { get; }
+
+        public Set ToChangeSet()
+        {
+            return new Set(this);
+        }
+    }
+}
---
+++ Infrastructure/Utility.Changes/Set.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Utility.Interfaces.NonGeneric;
+
+namespace Utility.Changes
+{
+
+    public class Set<T> : ReadOnlyCollection<Change<T>>
+    {
+        public Set(IList<Change<T>> list) : base(list)
+        {
+        }
+
+        public Set(Change<T> change) : base(new[] { change })
+        {
+        }
+    }
+
+    public class Set : ReadOnlyCollection<Change>
+    {
+        public Set(IEquatable key, IList<Change> list) : base(list)
+        {
+            Key = key;
+        }
+        public virtual IEquatable Key { get; }
+
+        public Set(Change change) : base(new[] { change })
+        {
+        }
+    }
+
+    public record Change<T> : Change
+    {
+        public Change(T? tValue, Type type) : base(tValue, type)
+        {
+            Value = tValue;
+        }
+
+        public new T Value { get; }
+
+        public new Set<T> ToChangeSet()
+        {
+            return new Set<T>(this);
+        }
+
+        public static Change<T> None => new(default, Type.None);
+
+        public Change<TR> As<TR>() where TR : class
+        {
+            return new Change<TR>(Value as TR ?? throw new Exception("sd ssss"), Type);
+        }
+    }
+
+
+}
