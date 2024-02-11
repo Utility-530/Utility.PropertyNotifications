126d6fdbe92ce29ef7b8c8c2c1a74b813d9b4ffb
dtaylor-530 <d.taylor530@gmail.com>
2024-02-11 20:39:26 +0000

    Utility.Infrastructure Removes BaseViewModel Utility.ViewModels.Base Removes preexisting BaseViewModel and replaces with one from Utility.Infrastructure

--- WPF/Utility.ViewModels.Base/BaseViewModel.cs
+++ WPF/Utility.ViewModels.Base/BaseViewModel.cs
@@ -1,131 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Utility.Interfaces.NonGeneric;
 
-namespace Utility.ViewModels.Base
+namespace Utility.Infrastructure
 {
-    public record PropertyAction(object Instance, PropertyInfo PropertyInfo);
-    public record PropertySet(object Instance, PropertyInfo PropertyInfo, object Value) : PropertyAction(Instance, PropertyInfo);
-    public record PropertyGet(object Instance, PropertyInfo PropertyInfo) : PropertyAction(Instance, PropertyInfo);
-    public record PropertyChange(object Instance, PropertyInfo PropertyInfo, object Value) : PropertyAction(Instance, PropertyInfo);
-
-    public class BaseViewModel : NotifyPropertyChanged, IObservable<PropertyAction>, IObserver<PropertyChange>
+    /// <inheritdoc />
+    /// <summary>
+    ///     The observable base class for every object that needs to update subscribers about certain changes using
+    ///     <see cref="T:System.ComponentModel.INotifyPropertyChanged" />
+    ///     <para />
+    ///     (For ViewModels use <see cref="ViewModel" />)
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged
     {
-        //private bool isSelected;
-        //private bool isChecked;
-        //private bool isVisible;
-        //private bool isReadOnly;
-        //private string name;
-        //private int row;
-        //private int column;
-        static readonly Dictionary<string, PropertyInfo> dictionary = new();
+        /// <inheritdoc />
+        /// <summary>
+        ///     The event on property changed
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        static BaseViewModel() => dictionary = typeof(BaseViewModel).GetProperties().ToDictionary(a => a.Name, a => a);
-
-        public ObservableCollection<PropertyChange> Changes { get; } = new();
-
-        public ObservableCollection<IObserver<PropertyAction>> Observers { get; } = new();
-
-        public bool IsSelected
+        /// <summary>
+        ///     Raise the <see cref="PropertyChanged" /> event
+        /// </summary>
+        /// <param name="propertyName">The caller member name of the property (auto-set)</param>
+        //[NotifyPropertyChangedInvocator]
+        public virtual void OnPropertyChanged([CallerMemberName] string? propertyName = default)
         {
-            get => Get<bool>(); set => Set(value);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public bool IsChecked
+        /// <summary>
+        ///     Set a property and raise the <see cref="PropertyChanged" /> event
+        /// </summary>
+        /// <typeparam name="T">The type of the Property</typeparam>
+        /// <param name="field">A reference to the backing field from the property</param>
+        /// <param name="value">The new value being set</param>
+        /// <param name="callerName">The caller member name of the property (auto-set)</param>
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string? callerName = default)
         {
-            get => Get<bool>(); set => Set(value);
-        }
-
-        public bool IsVisible
-        {
-            get => Get<bool>(); set => Set(value);
-        }
-
-        public bool IsReadOnly
-        {
-            get => Get<bool>(); set => Set(value);
-        }
-
-        public string Name
-        {
-            get => Get<string>(); set => Set(value);
-        }
-
-        public int Row
-        {
-            get => Get<int>(); set => Set(value);
-        }
-
-        public int Column
-        {
-            get => Get<int>(); set => Set(value);
-        }
-
-        protected T Get<T>([CallerMemberName] string? calledMemberName = null)
-        {
-            if(Changes.SingleOrDefault(a => a.PropertyInfo == dictionary[calledMemberName]) is T t)
+            if (field?.Equals(value) != true)
             {
-                return t;
+                field = value;
+                OnPropertyChanged(callerName);
+                return true;
             }
-            foreach (var observer in Observers)
-                observer.OnNext(new PropertyGet(this, dictionary[calledMemberName]));
-
-            return default;
-        }
-        
-        protected void Set(object value, [CallerMemberName] string? calledMemberName = null)
-        {
-            foreach (var observer in Observers)
-                observer.OnNext(new PropertySet(this, dictionary[calledMemberName], value));
-        }
-
-        public IDisposable Subscribe(IObserver<PropertyAction> observer)
-        {
-            return new Utility.Observables.Generic.Disposer<PropertyAction>(Observers, observer);
-        }
-
-        public void OnCompleted()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnError(Exception error)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnNext(PropertyChange value)
-        {
-            Changes.Add(value);
-            this.RaisePropertyChanged(value.PropertyInfo.Name);
+            return false;
         }
     }
 
-    public class NotifyPropertyChanged : INotifyPropertyChanged
+    public class ReadOnlyViewModel : BaseViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        ///  raises the PropertyChanged event for a single property
-        ///  'propertyname' can be left null (e.g OnPropertyChanged()), if called from body of property
-        /// </summary>
-        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        /// <summary>
-        ///  raises the PropertyChanged event for a single property
-        ///  'propertyname' can be left null (e.g OnPropertyChanged()), if called from body of property
-        /// </summary>
-        public void RaisePropertyChanged<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
-        {
-            property = value;
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        public bool IsReadOnly { get; init; }
     }
+
+
+    public  class ValueViewModel : ReadOnlyViewModel, IValue
+    {
+        public object? Value { get; set; }
+    }
+
 }
--- WPF/Utility.ViewModels.Base/ViewModel.cs
+++ WPF/Utility.ViewModels.Base/ViewModel.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Utility.Interfaces.NonGeneric;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Utility.Interfaces.NonGeneric;
 
-namespace Utility.ViewModels.Base
-{
-    public class ViewModel: BaseViewModel, IValue
-    {
-        public object Value
-        {
-            //get => Get<object>(); set => Set(value);
-            get;set;
-        }
-    }
-}
+//namespace Utility.ViewModels.Base
+//{
+//    public class ViewModel: BaseViewModel, IValue
+//    {
+//        public object Value
+//        {
+//            //get => Get<object>(); set => Set(value);
+//            get;set;
+//        }
+//    }
+//}
