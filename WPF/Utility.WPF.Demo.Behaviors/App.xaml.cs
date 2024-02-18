719d5c77481d24fe52e0fe4a1379cdf91101506b
dtaylor-530 <d.taylor530@gmail.com>
2024-02-18 23:38:12 +0000

    Utility.WPF.Demo.Behaviors Adds project

---
+++ WPF/Utility.WPF.Demo.Behaviors/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Utility.WPF.Controls.Meta;
+
+namespace Utility.WPF.Demo.Behaviors
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            new Window
+            {
+                WindowState = WindowState.Maximized,
+                Content = new AssemblyViewControl(typeof(App).Assembly)
+            }.Show();
+        }
+    }
+}
