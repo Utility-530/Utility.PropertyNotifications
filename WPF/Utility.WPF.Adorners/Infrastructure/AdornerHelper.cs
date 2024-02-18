3a0ed53877e7124e217013b0f541124962bc9a63
dtaylor-530 <d.taylor530@gmail.com>
2024-02-18 23:30:20 +0000

    Utility.WPF.Adorners Improves AdornerHelper

--- WPF/Utility.WPF.Adorners/Infrastructure/AdornerHelper.cs
+++ WPF/Utility.WPF.Adorners/Infrastructure/AdornerHelper.cs
@@ -5,13 +5,19 @@ namespace Utility.WPF.Adorners.Infrastructure
 {
     public static class AdornerHelper
     {
-        public static void AddIfMissingAdorner(this DependencyObject adornedElement, DependencyObject adorner)
+        public static bool AddIfMissingAdorner(this DependencyObject adornedElement, DependencyObject adorner)
         {
             AdornerCollection? adorners = AdornerEx.GetAdorners(adornedElement);
+            if (adorners.Count > 0)
+            {
+
+            }
             if (adorners.IndexOf(adorner) == -1)
             {
                 adorners.Add(adorner);
+                return true;
             }
+            return false;
         }
 
         public static Adorner[]? Adorners(this UIElement adornedElement)
