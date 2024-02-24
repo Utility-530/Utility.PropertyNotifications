6c4fbb539eef688c2fe7c651002df3aadb02fc3a
dtaylor-530 <d.taylor530@gmail.com>
2024-02-24 09:05:14 +0000

    Utility.Interfaces Adds ICount Adds IMemberDescriptor

---
+++ Utility.Interfaces/NonGeneric/IMemberDescriptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Utility.Interfaces.NonGeneric
+{
+    public interface IMemberDescriptor : IType
+    {
+        Guid Guid { get; }
+        string? Name { get; }
+        System.Type ParentType { get; }
+        IObservable<object> Children { get; }
+        object GetValue();
+        void SetValue(object value);
+
+    }
+}
