2b842b7cbb834d93832f3e41daa764d6d26e6466
dtaylor-530 <d.taylor530@gmail.com>
2024-03-05 10:38:59 +0000

    Renames namespace for Utility.WPF.ResourceDictionarys to http://schemas.utility.com/resourcedictionary

--- WPF/Utility.WPF.ResourceDictionarys/AssemblyInfo.cs
+++ WPF/Utility.WPF.ResourceDictionarys/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Windows.Markup;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: XmlnsPrefix("http://schemas.utility.com/utilitywpf", "utl")]
-[assembly: XmlnsDefinition("http://schemas.utility.com/utilitywpf", "Utility.WPF.ResourceDictionarys")]
+[assembly: XmlnsPrefix("http://schemas.utility.com/resourcedictionary", "res")]
+[assembly: XmlnsDefinition("http://schemas.utility.com/resourcedictionary", "Utility.WPF.ResourceDictionarys")]
