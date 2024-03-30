e125cb14ba35ef889396ba7cda0071ed4cb725c4
dtaylor-530 <d.taylor530@gmail.com>
2024-03-30 10:37:09 +0000

    Utility.WPF.Controls.Buttons Adds PlusGeometryButton Adds MinusGeometryButton

--- WPF/Utility.WPF.Controls.Buttons/XGeometryButton.cs
+++ WPF/Utility.WPF.Controls.Buttons/XGeometryButton.cs
@@ -23,4 +23,37 @@ namespace Utility.WPF.Controls.Buttons
             HoverBackground = new System.Windows.Media.SolidColorBrush(Colors.IndianRed);
         }
     }
+
+    public class PlusGeometryButton : GeometryButton
+    {
+        public PlusGeometryButton()
+        {
+            Data = Geometry.Parse("M0,1 H2 M1,0 V2");
+            {
+
+            };
+            HoverBackground = new System.Windows.Media.SolidColorBrush(Colors.CadetBlue);
+        }
+    }
+
+    public class MinusGeometryButton : GeometryButton
+    {
+        public MinusGeometryButton()
+        {
+            Data = new PathGeometry
+            {
+                Figures =
+                    new PathFigureCollection {
+                    new PathFigure { StartPoint = new System.Windows.Point(0, 0), 
+                        Segments = new PathSegmentCollection
+                    {
+                        new LineSegment { Point = new System.Windows.Point(1, 0) }
+                    }
+                    }
+                }
+            };
+            HoverBackground = new System.Windows.Media.SolidColorBrush(Colors.LightCoral);
+        }
+    }
 }
+
