7706a666f06fa141b0f7b850ce64ec813c4be214
dtaylor-530 <d.taylor530@gmail.com>
2024-04-11 16:59:56 +0100

    Utility.Enums Adds Colour

---
+++ Enums/Utility.Enums/Colour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Enums
+{
+    public enum Colour
+    {
+        Red,Pink, Purple, DeepPurple, Indigo, Blue, LightBlue, Cyan, Teal, Green, LightGreen, Lime, Yellow, Amber, Orange, DeepOrange, Brown, Grey, BlueGrey
+
+    }
+}
