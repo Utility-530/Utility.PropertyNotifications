45f3cc4936ffe629048f080a1ff01a4be1ddc411
dtaylor-530 <d.taylor530@gmail.com>
2024-04-12 08:55:03 +0100

    Utility.Meta Adds SystemAssembly (proxy for System Assembly)

---
+++ Meta/Utility.Meta/SystemAssembly.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Meta
+{
+    public class SystemAssembly : Assembly
+    {
+        const string System = "<System>";
+        public override Type[] GetTypes() => Types;
+
+        public static readonly Type[] Types = new[]{
+                typeof(void),
+                typeof(string),
+                typeof(Enum),
+                typeof(bool),
+                typeof(int),
+                typeof(short),
+                typeof(long),
+                typeof(double),
+                typeof(byte),
+                typeof(Guid),
+                typeof(DateTime),
+                typeof(object),
+        };
+
+        public override string? FullName => System;
+
+        public override AssemblyName GetName() => new (System);
+
+        public override string ToString() => System;
+
+    }
+}
