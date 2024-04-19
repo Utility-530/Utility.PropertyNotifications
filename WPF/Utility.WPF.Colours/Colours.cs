de65bf8b7de14add6d6eff4fd24af7b14c2cb1ae
dtaylor-530 <d.taylor530@gmail.com>
2024-04-19 11:44:56 +0100

    Utility.WPF.Colours Adds method PastelColorForString

--- WPF/Utility.WPF.Colours/Colours.cs
+++ WPF/Utility.WPF.Colours/Colours.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace Utility.WPF.Colours
 {
-    public class Colours
+    public class ColoursHelpers
     {
+        public static string PastelColorForString(string s)
+        {
+            uint hashCode = BitConverter.ToUInt32(SHA1.HashData(Encoding.UTF8.GetBytes(s)), 0);
+
+            return "#" +
+                ((((hashCode >> 24) & 0xFF) + 255) / 2).ToString("X") +
+                ((((hashCode >> 16) & 0xFF) + 255) / 2).ToString("X") +
+                ((((hashCode >> 08) & 0xFF) + 255) / 2).ToString("X");
+        }
+
     }
 }
