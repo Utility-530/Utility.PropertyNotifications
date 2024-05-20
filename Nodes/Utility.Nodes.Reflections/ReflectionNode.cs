1296e010520e190f983d7ab6f711c28ddeb0111d
dtaylor-530 <d.taylor530@gmail.com>
2024-05-20 12:34:06 +0100

    Utility.Nodes.Reflections Changes base class of ReflectionNode to Node{T}

--- Nodes/Utility.Nodes.Reflections/ReflectionNode.cs
+++ Nodes/Utility.Nodes.Reflections/ReflectionNode.cs
@@ -1,19 +1,13 @@
-﻿using System.Reactive.Linq;
-using Utility.Changes;
-using Utility.Collections;
-using Utility.Helpers;
-using Utility.Helpers.NonGeneric;
-using Utility.Interfaces.NonGeneric;
+﻿using Utility.Interfaces.NonGeneric;
 using Utility.Keys;
-using Utility.Models;
 using Utility.Trees.Abstractions;
 
 namespace Utility.Nodes.Reflections
 {
-    public class ReflectionNode : Node
+    public class ReflectionNode : Node<IDescriptor>
     {
         IDescriptor data;
-
+    
         public ReflectionNode(IDescriptor propertyData)
         {
             this.data = propertyData;
@@ -23,99 +17,33 @@ namespace Utility.Nodes.Reflections
         public ReflectionNode()
         {
         }
-
         public override object Data
         {
             get => data;
             set
             {
-                data = value as IDescriptor;
+                data = value as IDescriptor ?? throw new NullReferenceException($"{nameof(Data)} value is not of type {nameof(IDescriptor)}");
                 Key = new GuidKey(data.Guid);
             }
         }
 
-        public override async Task<bool> RefreshChildrenAsync()
-        {
-            if (await HasMoreChildren() == false)
-                return true;
-
-            m_items.Clear();
-
-            _ = GetChildren()
-                .Cast<Change<IDescriptor>>()
-                .Subscribe(async a =>
-                {
-                    switch (a.Type)
-                    {
-                        case Changes.Type.Add:
-                            {
-                                if (a.Value is not { } desc)
-                                {
-                                    throw new Exception(" wrwe334 33");
-                                }
-                                else if (desc.Type == data?.Type)
-                                {
-                                    var node = await ToNode(desc);
-                                    m_items.Add(node);
-                                }
-                                else
-                                {
-                                    //var conversion = ObjectConverter.ToValue(inst, desc);
-                                    ReflectionNode node = (ReflectionNode)await ToNode(desc);
-                                    //await node.RefreshChildrenAsync();
-                                    m_items.Add(node);
-                                }
-                                break;
-                            }
-                        case Changes.Type.Remove:
-                            {
-                                m_items.RemoveOne(e => (e as IReadOnlyTree)?.Data.Equals(a.Value) == true);
-                                break;
-                            }
-                        case Changes.Type.Reset:
-                            {
-                                m_items.Clear();
-                                break;
-                            }
-                    }
-                },
-            e =>
-            {
-            },
-            () =>
-            {
-                //items.Complete();
-            });
-
-            return true;
-        }
-
-
-        public override IObservable<object?> GetChildren()
-        {
-            if (data is IChildren children)
-                return children.Children;
-            return Observable.Empty<object?>();
-        }
-
-
-        public override async Task<bool> HasMoreChildren()
-        {
-            return /*data?.Type.IsValueOrString() == false && */await base.HasMoreChildren();
-        }
+        //public override async Task<bool> HasMoreChildren()
+        //{
+        //    return /*data?.Type.IsValueOrString() == false && */await base.HasMoreChildren();
+        //}
 
         public override string ToString()
         {
-            return data?.Name;
+            return data?.Name ?? "No name";
         }
 
 
-        public override async Task<IReadOnlyTree> ToNode(object value)
+        public override Task<IReadOnlyTree> ToNode(object value)
         {
             if (value is IDescriptor { } descriptor)
             {
 
-                return new ReflectionNode(descriptor) { Parent = this };
+                return Task.FromResult((IReadOnlyTree)new ReflectionNode(descriptor) { Parent = this });
             }
             else
             {
