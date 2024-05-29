c72ac9beff0131691ce4cecf3490308aed988bd0
dtaylor-530 <d.taylor530@gmail.com>
2024-05-29 15:09:19 +0100

    Utility.WPF.Demo.Colours Adds

---
+++ WPF/Utility.WPF.Demo.Colours/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Utility.WPF.Demo.Colours
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
