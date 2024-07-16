df3e090f7e07278b5876b2b2f0fda540cb93fce8
dtaylor-530 <d.taylor530@gmail.com>
2024-07-16 14:53:07 +0100

    Utility.WPF.Packages Adds WpfScreenHelper package Adds ScreenHelper class

---
+++ Packages/Utility.WPF.Packages/ScreenHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfScreenHelper;
+
+namespace Utility.WPF.Packages
+{
+    public static class ScreenHelper
+    {
+        public static void SetOnLastScreen(this Window window)
+        {
+            Screen s = Screen.AllScreens.ToArray()[^1];
+            var r = s.Bounds;
+            window.Top = r.Top;
+            window.Left = r.Left;
+            //window.WindowState = WindowState.Maximized;
+        }
+
+        public static void SetOnFirstScreen(this Window window)
+        {
+            Screen s = Screen.AllScreens.ToArray()[0];
+            var r = s.Bounds;
+            window.Top = r.Top;
+            window.Left = r.Left;
+            //window.WindowState = WindowState.Maximized;
+        }
+    }
+}
