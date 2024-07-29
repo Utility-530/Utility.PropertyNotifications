3cec6605f97ce220a39e4c56112bec03fc3e52ab
dtaylor-530 <d.taylor530@gmail.com>
2024-07-29 10:13:59 +0100

    Utility.WPF.Templates Changes template content for DateTime, Color and String types Adds AutoCompleteConverter for AutoCompleteComboBox of String type template

---
+++ WPF/Utility.WPF.Templates/Infrastructure/AutoCompleteConverter.cs
@@ -0,0 +1,91 @@
+﻿using AutoCompleteTextBox.Editors;
+using MintPlayer.ObservableCollection;
+using System;
+using Type = System.Type;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reactive.Subjects;
+using System.Windows.Data;
+using Utility.Changes;
+using Utility.Helpers;
+
+namespace Utility.WPF.Templates
+{
+
+    public record SuggestionPrompt(string Filter, object Value);
+
+    public class ComboSuggestionProvider : IComboSuggestionProvider, IObservable<SuggestionPrompt>
+    {
+        ReplaySubject<SuggestionPrompt> replaySubject = new();
+
+        private ObservableCollection<string> suggestions = new();
+ 
+        public ComboSuggestionProvider(object value)
+        {
+            Value = value;
+        }
+
+        public object Value { get; }
+
+        public IEnumerable GetFullCollection()
+        {
+            return suggestions;
+        }
+
+        public IEnumerable GetSuggestions(string filter)
+        {
+            replaySubject.OnNext(new(filter, Value));
+            return suggestions;
+        }
+
+        public IDisposable Subscribe(IObserver<SuggestionPrompt> observer)
+        {
+            return replaySubject.Subscribe(observer);
+        }
+    }
+
+    public class AutoCompleteConverter : IValueConverter, IObservable<SuggestionPrompt>, IObserver<Change>
+    {
+        readonly Dictionary<object, ComboSuggestionProvider> dictionary = new();
+        ReplaySubject<SuggestionPrompt> replaySubject = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return dictionary.Get(value, (a) =>
+            {
+                
+                var provider= new ComboSuggestionProvider(a);
+                provider.Subscribe(replaySubject);
+                return provider;
+            });
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static AutoCompleteConverter Instance { get; } = new AutoCompleteConverter();
+
+        public IDisposable Subscribe(IObserver<SuggestionPrompt> observer)
+        {
+            return replaySubject.Subscribe(observer);
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(Change value)
+        {
+          
+        }
+    }
+}
