b03441aa0af410f291e47339f371150d6ee778d2
dtaylor-530 <d.taylor530@gmail.com>
2024-08-14 21:22:51 +0100

    Utility.Trees.Demo.Three Uses Predicate and DecisionTrees for TreeViewItemFactory

--- Trees/Utility.Trees.Demo.Three/Filters/TreeViewItemFactory.cs
+++ Trees/Utility.Trees.Demo.Three/Filters/TreeViewItemFactory.cs
@@ -8,6 +8,7 @@ using Utility.Interfaces;
 using Utility.Interfaces.NonGeneric;
 using Utility.Nodes;
 using Utility.Trees.Abstractions;
+using Utility.Trees.Demo.MVVM.Infrastructure;
 using Utility.Trees.WPF.Abstractions;
 using Utility.WPF.Controls.Trees;
 
@@ -17,39 +18,47 @@ namespace Utility.Trees.Demo.MVVM.MVVM
     public class TreeViewItemFactory : ITreeViewItemFactory
     {
         Random random = new();
-        public HeaderedItemsControl Make(object instance, ItemsControl parent)
-        {
-            HeaderedItemsControl item = null;
-            if (instance is Node node && node.Data as ICollectionDescriptor != null && (node.Data as ICollectionDescriptor).ElementType == typeof(Table))
-            {
 
-                item = new ComboTreeViewItem
-                {
-                    Header = instance,
-                    DataContext = instance,
-                    IsExpanded = true
-                };
-            }
-            else
+        private TreeViewItemFactory()
+        {
+            Predicate = new TreeViewItemDecisionTree(new Decision(item => (item as IReadOnlyTree) != null) { })
             {
-                item = new CustomTreeViewItem
-                {
-                    //AddCommand = new Command(() => { if (instance is ITree { } item) item.Add(new ModelTree(Helpers.Names.Random(random), Guid.NewGuid(), ((GuidKey)item.Key).Value)); }),
-                    //RemoveCommand = new Command(() => { if (instance is IParent<ITree> { Parent: { } parent }) parent.Remove(instance); }),
-                    AddCommand = new Command(() => Add(instance)),
-                    RemoveCommand = new Command(() => Remove(instance)),
-                    Header = instance,
-                    DataContext = instance,
-                    IsExpanded = true
-                };
-            }
-            if(item is TreeViewItem treeViewItem)
-            {
-                treeViewItem.Selected += (s,e)=> { if (parent is ISelection selection) selection.Selection = treeViewItem; };
-            }
-            return item;
+                new TreeViewItemDecisionTree(new Decision<IReadOnlyTree>(item => item.Data as ICollectionDescriptor != null && (item.Data as ICollectionDescriptor).ElementType == typeof(Table)),
+                    instance=>
+                        new ComboTreeViewItem
+                        {
+                            Header = instance,
+                            DataContext = instance,
+                            IsExpanded = true
+                        }),
+                    new TreeViewItemDecisionTree(new Decision<IReadOnlyTree>(item =>true){  }, instance =>
+                    new CustomTreeViewItem
+                    {
+                        //AddCommand = new Command(() => { if (instance is ITree { } item) item.Add(new ModelTree(Helpers.Names.Random(random), Guid.NewGuid(), ((GuidKey)item.Key).Value)); }),
+                        //RemoveCommand = new Command(() => { if (instance is IParent<ITree> { Parent: { } parent }) parent.Remove(instance); }),
+                        AddCommand = new Command(() => Add(instance)),
+                        RemoveCommand = new Command(() => Remove(instance)),
+                        Header = instance,
+                        DataContext = instance,
+                        IsExpanded = true
+                    }),
+            };
         }
 
+        public DecisionTree Predicate { get; set; }
+
+        public HeaderedItemsControl Make(object instance, ItemsControl parent)
+        {
+            Predicate.Reset();
+            Predicate.Input = instance;
+            Predicate.Evaluate();
+            if (Predicate.Backput is TreeViewItem treeViewItem)
+            {
+                treeViewItem.Selected += (s, e) => { if (parent is ISelection selection) selection.Select(treeViewItem.Header); };
+                return treeViewItem;
+            }
+            throw new Exception("DS 3333");
+        }
 
         private void Remove(object instance)
         {
@@ -58,7 +67,7 @@ namespace Utility.Trees.Demo.MVVM.MVVM
 
         private void Add(object instance)
         {
-            if (instance is ITree { Parent: ITree {  } tree, Data: IInstance { Instance: { } _instance } })
+            if (instance is ITree { Parent: ITree { } tree, Data: IInstance { Instance: { } _instance } })
                 if (tree is { Data: IInstance { Instance: IList lst } data } item &&
                     tree is { Data: ICollectionDetailsDescriptor { CollectionItemPropertyType: { } type } })
                 {
