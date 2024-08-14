3b5a3186db6d5733d9b500369166cb2584d852fe
dtaylor-530 <d.taylor530@gmail.com>
2024-08-14 21:23:09 +0100

    Utility.Trees.Demo.Three Adds EventListener

---
+++ Trees/Utility.Trees.Demo.Three/Infrastructure/EventListener.cs
@@ -0,0 +1,16 @@
+﻿using Utility.Interfaces.NonGeneric;
+using Utility.Reactives;
+
+namespace Utility.Trees.Demo.MVVM.Infrastructure
+{
+    public class EventListener : ReplayModel<IEvent>, IEventListener
+    {
+
+        public static EventListener Instance { get; } = new();
+
+        public void Send(IEvent @event)
+        {
+            OnNext(@event);
+        }
+    }
+}
