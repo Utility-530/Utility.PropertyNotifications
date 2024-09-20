f0aee42f18dd9f3499d7659af5f79f02352a9af8
dtaylor-530 <d.taylor530@gmail.com>
2024-09-20 09:09:49 +0100

    Utility.WPF.Demo.Colours Adds RandomRectanglesView

---
+++ WPF/Utility.WPF.Demo.Colours/RandomRectanglesView.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Utility.WPF.Colours;
+
+namespace Utility.WPF.Demo.Colours
+{
+    /// <summary>
+    /// Interaction logic for RandomRectangles.xaml
+    /// </summary>
+    public partial class RandomRectanglesView : UserControl
+    {
+
+
+
+        Random rand = new Random();
+        ulong count = 0;
+        WriteableBitmap bitmap;
+        private FieldInfo[] fields;
+
+        public RandomRectanglesView()
+        {
+            InitializeComponent();
+            CompositionTarget.Rendering += OnRendering;
+            this.SizeChanged += MainView_SizeChanged;
+            fields = typeof(NiceNativeColours.Light).GetFields();
+        }
+
+        private void MainView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            bitmap = BitmapFactory.New((int)e.NewSize.Width, (int)e.NewSize.Height);
+            img.Source = bitmap;
+        }
+
+        void OnRendering(object sender, EventArgs args)
+        {
+            //Button_Click();
+            sdf();
+        }
+
+
+
+        void sdf()
+        {
+            if (bitmap == null)
+                return;
+
+            int width = bitmap.PixelWidth;
+            int height = bitmap.PixelHeight;
+
+            int x1 = rand.Next(width);
+            int x2 = rand.Next(width);
+            int y1 = rand.Next(height);
+            int y2 = rand.Next(height);
+            var clr = (Color)fields[rand.Next(1, fields.Length - 1)].GetValue(default);
+
+
+            using (bitmap.GetBitmapContext())
+            {
+
+                DrawRectangleFilled(x1, x2, y1, y2, clr);
+            }
+        }
+
+
+        void DrawRectangleFilled(int x1, int y1, int x2, int y2, Color clr)
+        {
+
+            if (x2 > x1)
+            {
+
+                for (int i = x1; i < x2 - x1; i++)
+                {
+                    bitmap.DrawLine(i, y1, i, y2, clr);
+                }
+            }
+            else
+            {
+                for (int i = x2; i < x1 - x2; i++)
+                {
+                    bitmap.DrawLine(i, y1, i, y2, clr);
+                }
+            }
+        }
+
+
+    }
+}
+
