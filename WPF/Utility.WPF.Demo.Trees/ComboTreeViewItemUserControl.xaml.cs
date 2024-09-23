b399461bbc5f4f5929a48f0cdc959cbb2af5d6a6
dtaylor-530 <d.taylor530@gmail.com>
2024-09-23 13:52:15 +0100

    Utility.WPF.Demo.Trees Improves ComboTreeViewItemUserControl

--- WPF/Utility.WPF.Demo.Trees/ComboTreeViewItemUserControl.xaml.cs
+++ WPF/Utility.WPF.Demo.Trees/ComboTreeViewItemUserControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections;
+using System.Windows;
 using System.Windows.Controls;
 using Utility.WPF.Controls.Trees;
 
@@ -12,19 +13,30 @@ namespace Utility.WPF.Demo.Trees
         public ComboTreeViewItemUserControl()
         {
             InitializeComponent();
-            var parent = new ComboTreeViewItem() { Width = 200, Height = 30 };
-
-            var treeViewItemA = Make("A");
-            var treeViewItemB = Make("B");
-            var treeViewItemC = Make("C");
-            var treeViewItemD = Make("D");
-            Add(parent, treeViewItemA);
-            Add(parent, treeViewItemB);
-            Add(parent, treeViewItemC);
-            Add(parent, treeViewItemD);
+            var parent = new ComboTreeViewItem() {
+                Width = 200,
+                Height = 30, 
+                //Style= this.Resources["Demo"] as Style, 
+                EditTemplate = this.Resources["EditTemplate"] as DataTemplate,
+                ItemsSource = this.Resources["Characters"] as IEnumerable,
+            };
+            parent.FinishEdit += Parent_Add;
+            //var treeViewItemA = Make("A");
+            //var treeViewItemB = Make("B");
+            //var treeViewItemC = Make("C");
+            //var treeViewItemD = Make("D");
+            //Add(parent, treeViewItemA);
+            //Add(parent, treeViewItemB);
+            //Add(parent, treeViewItemC);
+            //Add(parent, treeViewItemD);
             var treeView = new TreeView();
             treeView.Items.Add(parent);
             this.Content = treeView;
+        }
+
+        private void Parent_Add(object sender, NewObjectRoutedEventArgs e)
+        {
+            MessageBox.Show($"{e.IsAccepted}");
         }
 
         private static void Add(ComboTreeViewItem parent, TreeViewItem child)
