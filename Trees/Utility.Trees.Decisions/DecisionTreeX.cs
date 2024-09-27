dbe24be0ac639f88317b8faeeef5db63ff945b46
dtaylor-530 <d.taylor530@gmail.com>
2024-09-27 14:03:52 +0100

    Utility.Trees.Decisions Adds project

---
+++ Trees/Utility.Trees.Decisions/DecisionTreeX.cs
@@ -0,0 +1,128 @@
+﻿using System.Text;
+using Utility.Helpers.NonGeneric;
+using Utility.Interfaces.NonGeneric;
+using Utility.Pipes;
+
+namespace Utility.Trees.Decisions
+{
+    public class DecisionTreeX : DecisionTree<decimal>
+    {
+        private bool isInvoked;
+
+        public bool IsInvoked
+        {
+            get => isInvoked;
+            set
+            {
+                isInvoked = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        private readonly Func<IEnumerable<object>, object?> combine;
+
+        public DecisionTreeX(IDecision decision, string key, Func<decimal, object>? transform = null, Func<IEnumerable<object>, object?>? combine = null) : base(decision, transform)
+        {
+            Key = key;
+            this.combine = combine ?? new Func<IEnumerable<object>, object?>(a =>
+        {
+            return a.FirstOrDefault(v => v != null);
+        });
+        }
+
+        public void Evaluate(List<string> keys)
+        {
+
+            var result = decision.Evaluate(Input);
+
+            if (result == Match)
+            {
+                Output = Transform(Input);
+                if (Items.Any())
+                {
+                    foreach (DecisionTreeX item in Items)
+                    {
+                        item.Parent = this;
+                        item.Input = Output;
+                        var queueItem = new ForwardItem(item, Output, keys);
+
+                        Pipe.Instance.Queue(queueItem);
+                    }
+                }
+                else if (this.Parent is DecisionTreeX x)
+                {
+                    Backput = this.Output;
+                    Pipe.Instance.Queue(new BackItem(x, Backput, keys));
+                }
+                return;
+            }
+        }
+
+
+        public void BackPropagate(List<string> keys)
+        {
+            List<object> outputs = new();
+
+            foreach (DecisionTree item in Items)
+            {
+                outputs.Add(item.Output);
+            }
+
+            Backput = combine.Invoke(outputs);
+            if (this.Parent is DecisionTreeX x)
+            {
+                var queueItem = new BackItem(x, Backput, keys);
+                Pipe.Instance.Queue(queueItem);
+            }
+        }
+
+    }
+
+    public record ForwardItem(DecisionTreeX Tree, object Value, List<string> Keys) : DecisionQueueItem(Tree, Value, Keys)
+    {
+        public override void Invoke()
+        {
+            Decider.Instance.Current = Tree;
+            Keys.Add(Tree.Key);
+            Tree.Evaluate(Keys);
+        }
+    }
+
+
+
+    public record BackItem(DecisionTreeX Tree, object Value, List<string> Keys) : DecisionQueueItem(Tree, Value, Keys)
+    {
+        public override void Invoke()
+        {
+            Decider.Instance.Current = Tree;
+            Keys.Add(Tree.Key);
+            Tree.BackPropagate(Keys);
+        }
+    }
+
+    public abstract record DecisionQueueItem(DecisionTreeX Tree, object Value, List<string> Keys) : QueueItem
+    {
+
+    }
+
+    public class Decider
+    {
+        private DecisionTreeX current;
+
+        public DecisionTreeX Current
+        {
+            get { return current; }
+            set
+            {
+                if (current != null)
+                    current.IsInvoked = false;
+                current = value;
+                current.IsInvoked = true;
+            }
+        }
+
+        public static Decider Instance { get; } = new();
+    }
+
+}
---
+++ Trees/Utility.Trees.Decisions/DecisionTrees.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utility.Helpers.NonGeneric;
+using Utility.Interfaces.NonGeneric;
+using Utility.Enums;
+
+namespace Utility.Trees.Decisions
+{
+
+
+    public class StringDecisionTree<T> : DecisionTree<T, string>
+    {
+        public StringDecisionTree(IDecision decision, Func<T, object>? transform = null) : base(decision, transform)
+        {
+            Data = decision;
+        }
+    }
+    public class StringDecisionTree : DecisionTree
+    {
+        public StringDecisionTree(IDecision decision, Func<object, object>? transform = null) : base(decision, transform)
+        {
+            Data = decision;
+        }
+
+        protected override object ToBackPut(ICollection<object> backputs)
+        {
+            return backputs.FirstOrDefault(a => a is string);
+        }
+    }
+
+
+
+
+    public class DecisionTree<T, TR> : DecisionTree<T>
+    {
+        public DecisionTree(IDecision decision, Func<T, object>? transform = null) : base(decision, transform)
+        {
+            Data = decision;
+        }
+
+        protected override object ToBackPut(ICollection<object> backputs)
+        {
+            return backputs.FirstOrDefault(a => a is TR);
+        }
+    }
+
+
+    public class BooleanDecisionTree<T> : DecisionTree<T>
+    {
+
+        public BooleanDecisionTree(IDecision decision, Func<T, object>? transform = null) : base(decision, transform)
+        {
+            Data = decision;
+        }
+
+        public AndOr AndOr { get; set; }
+
+
+
+        protected override object ToBackPut(ICollection<object> backputs)
+        {
+            if (AndOr is AndOr.And)
+            {
+                return backputs.All(a => a is bool b ? b : true);
+            }
+            if (AndOr is AndOr.Or)
+            {
+                return backputs.Any(a => a is bool b ? b : true);
+            }
+            throw new Exception(" £3344333");
+        }
+    }
+
+    public class BooleanDecisionTree : DecisionTree
+    {
+        public BooleanDecisionTree(IDecision decision, Func<object, object>? transform = null) : base(decision, transform)
+        {
+            Data = decision;
+        }
+
+        public AndOr AndOr { get; set; }
+
+
+
+        protected override object ToBackPut(ICollection<object> backputs)
+        {
+            if (AndOr is AndOr.And)
+            {
+                return backputs.All(a => a is bool b ? b : true);
+            }
+            if (AndOr is AndOr.Or)
+            {
+                return backputs.Any(a => a is bool b ? b : true);
+            }
+            throw new Exception(" £3344333");
+        }
+    }
+
+    public class DecisionTree<T> : DecisionTree
+    {
+        private readonly Func<T, object>? transform;
+
+        public DecisionTree(IDecision decision, Func<T, object>? transform = null) : base(decision)
+        {
+            Data = decision;
+            this.transform = transform ?? new Func<T, object>(a => a);
+        }
+
+        protected override object Transform(object value)
+        {
+            if (value is not T t)
+            {
+                throw new Exception("fds df33 dffd");
+            }
+
+            return transform.Invoke(t);
+        }
+
+    }
+
+    public class DecisionTree : Tree
+    {
+        protected readonly IDecision decision;
+        private object input;
+        private object output;
+        private object backput;
+        private Func<object, object>? transform;
+        private bool hasEvaluated;
+
+        public DecisionTree(IDecision decision, Func<object, object>? transform = null, bool match = true)
+        {
+            this.decision = decision;
+            Match = match;
+            this.transform = transform ?? new Func<object, object>(a => a);
+        }
+
+
+        public virtual void Evaluate()
+        {
+            List<object> outputs = new();
+
+            //var compile = (Data as IDecision).Predicate.Compile().Invoke(Input);
+
+            var result = decision.Evaluate(Input);
+            HasEvaluated = true;
+            if (result == Match)
+            {
+                Output = Transform(Input);
+                if (Items.Any())
+                {
+                    foreach (DecisionTree item in Items)
+                    {
+                        item.Input = Output;
+                        item.Evaluate();
+                        outputs.Add(item.Backput);
+                    }
+                    Backput = ToBackPut(outputs);
+                }
+                else
+                {
+                    Backput = Output;
+                }
+                return;
+            }
+            Backput = null;
+        }
+
+        public void Reset()
+        {
+            Input = null;
+            Output = null;
+            Backput = null;
+            if (Items.Any())
+            {
+                foreach (DecisionTree x in Items)
+                {
+                    x.Reset();
+                }
+            }
+        }
+
+
+        //public void Eval()
+        //{
+        //    List<object> outputs = new();
+
+        //    //var compile = (Data as IDecision).Predicate.Compile().Invoke(Input);
+
+        //    var result = decision.Evaluate(Input);
+
+        //    if (result == Match)
+        //    {
+        //        Output = Transform(Input);
+        //        if (Items.Any())
+        //        {
+        //            Items.Cast<DecisionTree>()
+        //                .Select(x =>
+        //                {
+        //                    var xe = x.WithChangesTo(a => a.Backput);
+        //                    x.Input = Output;
+        //                    Pipe.Instance.Queue(new DecisionTreeQueueItem(x));
+        //                    return xe;
+        //                })
+        //                .Combine()
+        //                .Subscribe(outputs =>
+        //                {
+        //                    Backput = ToBackPut(outputs);
+        //                });
+        //        }
+        //        else
+        //        {
+        //            Backput = Output;
+        //        }
+
+        //        return;
+        //    }
+        //    Backput = null;
+        //}
+
+        protected virtual object Transform(object value)
+        {
+            if (transform != null)
+                return transform.Invoke(value);
+            throw new Exception(" ew332 324");
+        }
+
+        protected virtual object ToBackPut(ICollection<object> backputs)
+        {
+            return backputs.SingleOrDefault(a => a != null);
+        }
+
+
+        public object Input
+        {
+            get => input; set
+            {
+                input = value;
+                HasEvaluated = false;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool HasEvaluated
+        {
+            get => hasEvaluated; set
+            {
+                hasEvaluated = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public object Output
+        {
+            get => output; set
+            {
+                output = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public object Backput
+        {
+            get => backput; set
+            {
+                backput = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool Match { get; }
+    }
+}
