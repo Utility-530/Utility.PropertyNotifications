5d3f109dabb59a1ad22b37bdf1b5ee1d7abf7e2a
dtaylor-530 <d.taylor530@gmail.com>
2024-10-21 09:37:26 +0100

    Utility.Repos Removes nullable attribute of DateValue type in 'Get' return variable

--- Repos/Utility.Repos/New/ITreeRepository.cs
+++ Repos/Utility.Repos/New/ITreeRepository.cs
@@ -6,7 +6,7 @@
         IObservable<Key> InsertRoot(Guid guid, string name, Type type);
         IEnumerable<Duplication> Duplicate(Guid oldGuid, Guid? newParentGuid = null);
         IObservable<Guid> Find(Guid parentGuid, string name, Type? type = null, int? index = null);
-        IObservable<DateValue?> Get(Guid guid);
+        IObservable<DateValue> Get(Guid guid);
         //int Insert(Guid guid, string name, Type type, Guid parentGuid, int? index );
         IObservable<Guid> InsertByParent(Guid parentGuid, string name, string? table_name = null, int? typeId = null, int? index = null);
         int? MaxIndex(Guid parentGuid, string? name = null);
