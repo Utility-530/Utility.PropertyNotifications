67c7acd7f2c562c55b0859fade3d6bca3d5d9e9d
dtaylor-530 <d.taylor530@gmail.com>
2024-11-03 12:40:47 +0000

    Utility.PropertyDescriptors Fixes issue propogating a change in item of a reference object

--- Meta/Utility.PropertyDescriptors/ReferenceDescriptor.cs
+++ Meta/Utility.PropertyDescriptors/ReferenceDescriptor.cs
@@ -7,32 +7,83 @@ internal record ReferenceDescriptor(Descriptor Descriptor, object Instance) : Va
 {
     private readonly ITreeRepository repo = Locator.Current.GetService<ITreeRepository>();
     private IObservable<Change<IDescriptor>> observable;
+    private PropertiesDescriptor propertiesDescriptor;
 
     public override IObservable<object> Children
     {
         get
         {
-            if (Get() is var inst)
-            {
-                if (inst is Type type)
-                {
-                    return Observable.Empty<Change<IDescriptor>>();
-                }
-                return observable ??= Observable.Create<Change<IDescriptor>>(async observer =>
+            return
+                observable ??= Observable.Create<Change<IDescriptor>>(async observer =>
                 {
                     CompositeDisposable disp = new();
-                    if (inst is null)
+                    this.WhenReceivedFrom(a => a.Value)
+                    .Subscribe(inst =>
                     {
-                        int i = 0;
-
-                        if (Descriptor.PropertyType.IsAssignableTo(typeof(IEnumerable)) && Descriptor.PropertyType.IsAssignableTo(typeof(string)) == false && Descriptor.PropertyType.GetCollectionElementType() is Type _elementType)
+                        if (inst is null)
                         {
+                            int i = 0;
+
+                            if (Descriptor.PropertyType.IsAssignableTo(typeof(IEnumerable)) && Descriptor.PropertyType.IsAssignableTo(typeof(string)) == false && Descriptor.PropertyType.GetCollectionElementType() is Type _elementType)
+                            {
+                                repo
+                                .Find(this.Guid, CollectionDescriptor._Name, Descriptor.PropertyType)
+                                .Subscribe(cguid =>
+                                {
+                                    var enumerable = (IEnumerable)Activator.CreateInstance(Descriptor.PropertyType);
+                                    var collectionDescriptor = new CollectionDescriptor(Descriptor, _elementType, enumerable);
+                                    if (i++ > 0)
+                                    {
+                                        return;
+                                    }
+                                    collectionDescriptor.Subscribe(changes);
+                                    observer.OnNext(new(collectionDescriptor with { Guid = cguid }, Changes.Type.Add));
+                                }).DisposeWith(disp);
+                            }
+                            else
+                                repo
+                                .Find(this.Guid, "Properties", Descriptor.PropertyType)
+                                .Subscribe(pguid =>
+                                {
+                                    if (inst != null)
+                                        return;
+                                    inst = Activator.CreateInstance(Descriptor.PropertyType);
+                                    if (propertiesDescriptor != null)
+                                    {
+                                        observer.OnNext(new(propertiesDescriptor, Changes.Type.Remove));
+                                    }
+                                    propertiesDescriptor = new PropertiesDescriptor(Descriptor, inst) { Guid = Guid };
+                                    propertiesDescriptor.Subscribe(changes);
+                                    observer.OnNext(new(propertiesDescriptor, Changes.Type.Add));
+                                }).DisposeWith(disp);
+                        }
+                        if (inst is object obj)
+                        {
+                            observer.OnNext(new(default, Changes.Type.Reset));
+
+                            repo.Find(this.Guid, "Properties" /*propertiesDescriptor.Name*/, Descriptor.PropertyType)
+                            .Subscribe(pguid =>
+                            {
+                                if (inst == null)
+                                    return;
+                                if (propertiesDescriptor != null)
+                                {
+                                    observer.OnNext(new(propertiesDescriptor, Changes.Type.Remove));
+                                }
+                                propertiesDescriptor = new PropertiesDescriptor(Descriptor, inst);
+                                propertiesDescriptor.Subscribe(changes);
+
+                                observer.OnNext(new(propertiesDescriptor with { Guid = pguid }, Changes.Type.Add));
+                            }).DisposeWith(disp);
+                        }
+                        if (inst is IEnumerable enumerable && inst is not string s && inst.GetType() is Type _type && _type.GetCollectionElementType() is Type elementType)
+                        {
+                            int i = 0;
+                            var collectionDescriptor = new CollectionDescriptor(Descriptor, elementType, enumerable);
                             repo
-                            .Find(this.Guid, CollectionDescriptor._Name, Descriptor.PropertyType)
+                            .Find(this.Guid, collectionDescriptor.Name, Descriptor.PropertyType)
                             .Subscribe(cguid =>
                             {
-                                var enumerable = (IEnumerable)Activator.CreateInstance(Descriptor.PropertyType);
-                                var collectionDescriptor = new CollectionDescriptor(Descriptor, _elementType, enumerable);
                                 if (i++ > 0)
                                 {
                                     return;
@@ -41,52 +92,9 @@ internal record ReferenceDescriptor(Descriptor Descriptor, object Instance) : Va
                                 observer.OnNext(new(collectionDescriptor with { Guid = cguid }, Changes.Type.Add));
                             }).DisposeWith(disp);
                         }
-                        else
-                            repo.Find(this.Guid, "Properties", Descriptor.PropertyType)
-                            .Subscribe(pguid =>
-                            {
-                                inst = Activator.CreateInstance(Descriptor.PropertyType);
-                                var propertiesDescriptor = new PropertiesDescriptor(Descriptor, inst);
-                                var propertiesDescriptor2 = propertiesDescriptor with { Guid = pguid };
-                                propertiesDescriptor2.Subscribe(changes);
-                                observer.OnNext(new(propertiesDescriptor2, Changes.Type.Add));
-                            }).DisposeWith(disp);
-
-                    }
-                    if (inst is object obj)
-                    {
-                        observer.OnNext(new(default, Changes.Type.Reset));
-                        var propertiesDescriptor = new PropertiesDescriptor(Descriptor, inst);
-                        propertiesDescriptor.Subscribe(changes);
-
-                        repo.Find(this.Guid, propertiesDescriptor.Name, Descriptor.PropertyType)
-                        .Subscribe(pguid =>
-                        {
-                            observer.OnNext(new(propertiesDescriptor with { Guid = pguid }, Changes.Type.Add));
-                        }).DisposeWith(disp);
-                    }
-
-                    if (inst is IEnumerable enumerable && inst is not string s && inst.GetType() is Type _type && _type.GetCollectionElementType() is Type elementType)
-                    {
-                        int i = 0;
-                        var collectionDescriptor = new CollectionDescriptor(Descriptor, elementType, enumerable);
-                        repo
-                        .Find(this.Guid, collectionDescriptor.Name, Descriptor.PropertyType)
-                        .Subscribe(cguid =>
-                        {
-                            if (i++ > 0)
-                            {
-                                return;
-                            }
-                            collectionDescriptor.Subscribe(changes);
-                            observer.OnNext(new(collectionDescriptor with { Guid = cguid }, Changes.Type.Add));
-                        }).DisposeWith(disp);
-                    }
-
-                    return Disposable.Empty;
+                    });
+                    return disp;
                 });
-            }
-            return Observable.Empty<Change<IDescriptor>>();
         }
     }
 }
