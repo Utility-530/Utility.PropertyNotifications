71354239ef61450400a3878d00ca6265feac8c82
dtaylor-530 <d.taylor530@gmail.com>
2024-12-31 14:05:53 +0000

    Utility.Changes Adds property, OldValue Adds static constructors to Change

--- Infrastructure/Utility.Changes/Change.cs
+++ Infrastructure/Utility.Changes/Change.cs
@@ -4,14 +4,16 @@ namespace Utility.Changes
 {
     public record Change : IValue
     {
-        public Change(object value, Type type, int? count = default)
+        public Change(object value, object oldValue, Type type, int? count = default)
         {
             Value = value;
+            OldValue = oldValue;
             Type = type;
             Count = count;
         }
 
         public object Value { get; }
+        public object OldValue { get; }
 
         public Type Type { get; init; }
         public int? Count { get; }
--- Infrastructure/Utility.Changes/Change{T}.cs
+++ Infrastructure/Utility.Changes/Change{T}.cs
@@ -4,12 +4,18 @@ namespace Utility.Changes
 {
     public record Change<T> : Change, IValue<T>
     {
-        public Change(T? tValue, Type type) : base(tValue, type)
+        public Change(T? tValue, Type type) : this(tValue, default, type)
+        {
+        }
+
+        public Change(T? tValue, T? oldValue, Type type) : base(tValue, oldValue, type)
         {
             Value = tValue;
+            OldValue = oldValue;
         }
 
         public new T Value { get; }
+        public new T OldValue { get; }
 
         public new Set<T> ToChangeSet()
         {
@@ -22,9 +28,27 @@ namespace Utility.Changes
         {
             return new Change<TR>(Value as TR ?? throw new Exception("sd ssss"), Type);
         }
+
+
+
+        public static Change<T> Add(T NewItem) => new Change<T>(NewItem, default, Changes.Type.Add);
+        public static Change<T> Remove(T NewItem) => new Change<T>(NewItem, default, Changes.Type.Remove);
+        public static Change<T> Update(T NewItem, T OldItem) => new Change<T>(NewItem, OldItem, Changes.Type.Update);
+
+
+
     }
 
-    public record Update<T> :Change<T>
+
+    public readonly record struct TreeChange<T>(T NewItem, T? OldItem, Type Update, int Level)
+    {
+        public static TreeChange<T> Add(T NewItem, int level) => new TreeChange<T>(NewItem, default, Type.Add, level);
+        public static TreeChange<T> Remove(T NewItem, int level) => new TreeChange<T>(NewItem, default, Type.Remove, level);
+        public static TreeChange<T> Replace(T NewItem, T OldItem, int level) => new TreeChange<T>(NewItem, OldItem, Type.Update, level);
+    }
+
+
+    public record Update<T> : Change<T>
     {
         public Update(T? tValue, string property) : base(tValue, Type.Update)
         {
