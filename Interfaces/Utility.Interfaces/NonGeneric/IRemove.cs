76e5a3af97ca504541278b6f34191e52f2b8ed64
dtaylor-530 <d.taylor530@gmail.com>
2024-12-31 14:07:42 +0000

    Utility.Interfaces Adds IBreadcrumb Adds IRemove Adds IRoot Adds to ITree

--- Interfaces/Utility.Interfaces/NonGeneric/IRemove.cs
+++ Interfaces/Utility.Interfaces/NonGeneric/IRemove.cs
@@ -9,5 +9,10 @@ namespace Utility.Interfaces.NonGeneric
     public interface IRemove
     {
         void Remove(object instance);  
+    }  
+    
+    public interface IRemoveAt
+    {
+        void RemoveAt(int index);  
     }
 }
