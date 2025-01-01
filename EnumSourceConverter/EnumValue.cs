397c18ada1b8e87c41d6242dfb3e88e2a2f74ade
dtaylor-530 <d.taylor530@gmail.com>
2025-01-01 20:36:20 +0000

    Init

---
+++ EnumSourceConverter/EnumValue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tonic.UI
+{
+    public class EnumValue
+    {
+        private static Dictionary<object, EnumValue> cache = new Dictionary<object, EnumValue>();
+        private static EnumValue nullValue = new EnumValue(null);
+        public static EnumValue Create(object value)
+        {
+            if (value == null)
+                return nullValue;
+
+            lock (cache)
+            {
+                if (!cache.TryGetValue(value, out EnumValue ret))
+                {
+                    ret = new EnumValue(value);
+                    cache.Add(value, ret);
+                }
+                return ret;
+            }
+        }
+        private EnumValue(object Value)
+        {
+            this.Value = Value;
+            this.Description = EnumConverter.EnumToString(Value);
+        }
+
+        public object Value { get; private set; }
+
+        public string Description { get; private set; }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+
+}
---
+++ EnumSourceConverter/GetTypeEnumConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tonic.UI
+{
+    class GetTypeEnumConverter : IValueConverter
+    {
+        private object cache;
+
+        public BindingExpression Expression { get; set; }
+        /// <summary>
+        /// True to use Friendly names
+        /// </summary>
+        public bool UseDescription { get; set; }
+        /// <summary>
+        /// Null if the type is still unknown
+        /// </summary>
+        private Type Type { get; set; }
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Expression == null) return null;
+
+            if (Type == null)
+                Type = Expression.ResolvedSource.GetType().GetProperty(Expression.ResolvedSourcePropertyName).GetValue(Expression.ResolvedSource)?.GetType();
+
+            if(Type ==null)
+                return DependencyProperty.UnsetValue;
+
+            //El cache no es solo por rendimiento, si no es utilizado, cada vez que se llama a este metodo,
+            //aunque sean los mismos valores devuelve una instancia de un arreglo diferente, lo que ocasiona que
+            //WPF actualize el valor de la propiedad, ocacionando un ciclo infinito
+
+            if (cache == null)
+                if (UseDescription)
+                    cache = EnumSource.GetEnumValues(Type).Select(x => EnumValue.Create(x)).ToArray();
+                else
+                    cache = EnumSource.GetEnumValues(Type);
+            return cache;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
