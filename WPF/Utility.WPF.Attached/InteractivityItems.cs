9ad5aef95ece684e00fc72016583529b5c4782fa
dtaylor-530 <d.taylor530@gmail.com>
2025-01-06 16:24:56 +0000

    Utility.WPF.Attached Adds InteractivityTemplate for adding interactivity Behaviors, Triggers to a Style

---
+++ WPF/Utility.WPF.Attached/InteractivityItems.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriggerCollection = Microsoft.Xaml.Behaviors.TriggerCollection;
+using TriggerBase = Microsoft.Xaml.Behaviors.TriggerBase;
+using System.Windows;
+
+namespace Utility.WPF.Attached
+{
+    /// <summary>
+    /// <see cref="FrameworkTemplate"/> for InteractivityElements instance
+    /// <remarks>Subclassed for forward compatibility, perhaps one day <see cref="FrameworkTemplate"/> </remarks>
+    /// <remarks>will not be partially internal</remarks>
+    /// </summary>
+    public class InteractivityTemplate : DataTemplate
+    {
+
+    }
+
+    /// <summary>
+    /// <a href="https://vspivak.wordpress.com/2011/01/19/using-system-windows-interactivity-behaviors-and-actions-in-wpfsilverlight-styles/"/>
+    /// Holder for interactivity entries
+    /// </summary>
+    public class InteractivityItems : FrameworkElement
+    {
+        private List<Behavior> _behaviors;
+        private List<TriggerBase> _triggers;
+
+        public List<TriggerBase> Triggers => _triggers ??= [];
+
+        public List<Behavior> Behaviors => _behaviors ??= [];
+
+
+
+        public static InteractivityTemplate GetTemplate(DependencyObject obj)
+        {
+            return (InteractivityTemplate)obj.GetValue(TemplateProperty);
+        }
+
+        public static void SetTemplate(DependencyObject obj, InteractivityTemplate value)
+        {
+            obj.SetValue(TemplateProperty, value);
+        }
+
+        public static readonly DependencyProperty TemplateProperty =
+            DependencyProperty.RegisterAttached("Template",
+            typeof(InteractivityTemplate),
+            typeof(InteractivityItems),
+            new PropertyMetadata(default(InteractivityTemplate), OnTemplateChanged));
+
+        private static void OnTemplateChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            InteractivityTemplate dt = (InteractivityTemplate)e.NewValue;
+#if (!SILVERLIGHT)
+            dt.Seal();
+#endif
+            InteractivityItems ih = (InteractivityItems)dt.LoadContent();
+            BehaviorCollection bc = Interaction.GetBehaviors(d);
+            TriggerCollection tc = Interaction.GetTriggers(d);
+
+            foreach (Behavior behavior in ih.Behaviors)
+                bc.Add(behavior);
+
+            foreach (TriggerBase trigger in ih.Triggers)
+                tc.Add(trigger);
+        }
+
+
+    }
+}
