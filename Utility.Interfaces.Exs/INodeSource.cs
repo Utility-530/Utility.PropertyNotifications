2b1e7b227f2b069221bbe212ed745d2673037c66
dtaylor-530 <d.taylor530@gmail.com>
2025-01-21 11:11:27 +0000

    Utility.Interfaces.Exs Adds INode, INodeSource, IValueChanges

---
+++ Utility.Interfaces.Exs/INodeSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Utility.Interfaces.Exs;
+using Utility.Structs.Repos;
+
+namespace Utility.Nodes.Filters
+{
+    public interface INodeSource
+    {
+        string New { get; }
+
+        void Remove(Guid guid);
+        IObservable<INode> ChildrenByGuidAsync(Guid guid);
+        IObservable<INode> SingleByGuidAsync(object currentGuid);
+        int? MaxIndex(Guid guid, string v);
+        void Remove(INode node);
+        IObservable<Key> Find(Guid guid, string name, System.Type type, object localIndex);
+        IObservable<GuidValue> Get(Guid guid);
+        void Add(INode node);
+    }
+}
