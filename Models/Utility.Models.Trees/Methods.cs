ce31de0c025599fcd464684fffb3b2118cd27246
dtaylor-530 <d.taylor530@gmail.com>
2025-01-21 12:03:59 +0000

    Utility.Models.Trees Adds project

---
+++ Models/Utility.Models.Trees/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Models.Trees
+{
+    public class Methods
+    {
+        public static bool FirstMethod(bool value)
+        {
+            return true;
+        }
+    }
+}
