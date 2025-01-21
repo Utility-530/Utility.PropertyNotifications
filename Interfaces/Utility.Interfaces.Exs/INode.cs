370932526648a9d5087038202e300e269f6afecf
dtaylor-530 <d.taylor530@gmail.com>
2025-01-21 11:11:27 +0000

    Utility.Interfaces.Exs Adds INode, INodeSource, IValueChanges

---
+++ Interfaces/Utility.Interfaces.Exs/INode.cs
@@ -0,0 +1,37 @@
+﻿using Utility.Trees.Abstractions;
+using Utility.Interfaces.NonGeneric;
+using Utility.Enums;
+using System.Collections;
+
+namespace Utility.Interfaces.Exs
+{
+    public interface INode : ITree, IIsExpanded, ICurrent<INode>, IAdd, IKey, ILocalIndex, IIsPersistable, IIsEditable, IOrientation
+    {
+    }
+
+    public interface ICurrent<T>
+    {
+        T Current { get; set; }
+    }
+
+    public interface ILocalIndex
+    {
+        int? LocalIndex { get; set; }
+    }
+
+    public interface IOrientation
+    {
+        Orientation Orientation { get; set; }
+    }
+        
+    public interface IProliferation
+    {
+        IEnumerable Proliferation();
+    }
+
+    public interface ISetNode
+    {
+        void SetNode(INode node);
+
+    }
+}
---
+++ Interfaces/Utility.Interfaces.Exs/IValueChanges.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utility.Interfaces.NonGeneric;
+using Utility.Structs;
+
+namespace Utility.Interfaces.Exs
+{
+
+    public interface IValueChanges : IObservable<ValueChange>
+    {
+    }
+
+}
