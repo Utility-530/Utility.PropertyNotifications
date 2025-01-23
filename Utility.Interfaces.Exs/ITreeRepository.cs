b49da2e5b0edd0f0f6504885c0af5a649badeff2
dtaylor-530 <d.taylor530@gmail.com>
2025-01-23 11:02:03 +0000

    Utility.Interfaces.Ex Adds IRemoved to INode Updates INodeSource and ITreeRepository with addition of string variable to Get and Set methods

--- Utility.Interfaces.Exs/ITreeRepository.cs
+++ Utility.Interfaces.Exs/ITreeRepository.cs
@@ -9,16 +9,12 @@ namespace Utility.Repos
         void Copy(Guid guid, Guid newGuid);
         IObservable<Key> InsertRoot(Guid guid, string name, Type type);
         IEnumerable<Duplication> Duplicate(Guid oldGuid, Guid? newParentGuid = null);
-        IObservable<Key> Find(Guid parentGuid, string name, Type? type = null, int? index = null);
-        IObservable<DateValue> Get(Guid guid);
-        //int Insert(Guid guid, string name, Type type, Guid parentGuid, int? index );
+        IObservable<Key> Find(Guid parentGuid, string? name = null, Type? type = null, int? index = null);
+        IObservable<DateValue> Get(Guid guid, string? name = null);
         IObservable<Guid> InsertByParent(Guid parentGuid, string name, string? table_name = null, int? typeId = null, int? index = null);
         int? MaxIndex(Guid parentGuid, string? name = null);
-        //Task<IReadOnlyCollection<Key>> Keys();
-        //void Register(Guid guid, INotifyPropertyCalled propertyCalled);
-        //void Register(Guid guid, INotifyPropertyReceived propertyReceived);
         void Remove(Guid guid);
         IObservable<IReadOnlyCollection<Key>> SelectKeys(Guid? parentGuid = null, string? name = null, string? table_name = null);
-        void Set(Guid guid, object value, DateTime dateTime);
+        void Set(Guid guid, string name, object value, DateTime dateTime);
     }
 }
