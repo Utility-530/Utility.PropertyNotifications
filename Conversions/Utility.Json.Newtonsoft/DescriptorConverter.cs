550c17d96c2a505cf29f3ec869b0412e5f4f90e4
dtaylor-530 <d.taylor530@gmail.com>
2025-01-23 10:39:45 +0000

    Utility.Json.Newtonsoft Adds DescriptorConverter

---
+++ Conversions/Utility.Json.Newtonsoft/DescriptorConverter.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+
+namespace Utility.Conversions.Json.Newtonsoft
+{
+    public class DescriptorConverter : JsonConverter<PropertyDescriptor>
+    {
+
+        public override void WriteJson(JsonWriter writer, PropertyDescriptor value, JsonSerializer serializer)
+        {
+            // Begin object
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("$type");
+            writer.WriteValue("PropertyDescriptor, System.ComponentModel");
+
+            writer.WritePropertyName("ComponentType");
+            writer.WriteValue(value.ComponentType?.AssemblyQualifiedName);
+
+            writer.WritePropertyName("PropertyType");
+            writer.WriteValue(value.PropertyType.AssemblyQualifiedName);
+
+            writer.WritePropertyName("Name");
+            writer.WriteValue(value.Name ?? string.Empty);
+
+            // End object
+            writer.WriteEndObject();
+        }
+
+        public override PropertyDescriptor ReadJson(JsonReader reader, Type objectType, PropertyDescriptor existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return null;
+            }
+            // Read the JSON object
+            var jObject = JObject.Load(reader);
+
+
+            var propertyName = jObject["Name"]?.ToString();
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new JsonSerializationException("Property name is missing or invalid.");
+            }
+
+            var typeName = jObject["ComponentType"]?.ToString();
+            if (string.IsNullOrEmpty(typeName))
+            {
+                var propertyTypeName = jObject["PropertyType"]?.ToString();
+                var propertyType = Type.GetType(propertyTypeName);
+                return new Utility.Meta.RootDescriptor(propertyType, name: propertyName);
+            }
+
+            var type = Type.GetType(typeName);
+            if (type == null)
+            {
+                throw new JsonSerializationException($"Unable to resolve type: {typeName}");
+            }            
+ 
+            return TypeDescriptor.GetProperties(type)[propertyName];    
+        }
+    }
+}
