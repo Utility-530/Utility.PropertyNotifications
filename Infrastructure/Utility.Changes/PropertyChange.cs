d3570fd29465ae92aa51140ae4cfd0796aff8bd4
dtaylor-530 <d.taylor530@gmail.com>
2025-01-23 10:40:35 +0000

    Utility.Changes Adds PropertyChange

---
+++ Infrastructure/Utility.Changes/PropertyChange.cs
@@ -0,0 +1,5 @@
+﻿namespace Utility.Changes
+{
+    public record PropertyChange(object Source, string PropertyName, object NewValue, object? PreviousValue = null) : Change(NewValue, PreviousValue, Type.Update);
+
+}
