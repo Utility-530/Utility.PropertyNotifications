b1b0ffa58ba7dd6b90e8a5ca4a9fb96e22271460
dtaylor-530 <d.taylor530@gmail.com>
2025-01-23 11:06:04 +0000

    Utility.Structs.Repos Adds Name variable to DataValue

--- Structs/Utility.Structs.Repos/DateValue.cs
+++ Structs/Utility.Structs.Repos/DateValue.cs
@@ -2,7 +2,7 @@
 
 namespace Utility.Structs.Repos
 {
-    public readonly record struct DateValue(DateTime DateTime, object Value);
+    public readonly record struct DateValue(Guid Guid, string Name, DateTime DateTime, object Value);
 
 
 
