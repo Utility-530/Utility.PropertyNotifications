1869be0ff86d54e88d5f2c38bf785e6e2397a1ca
dtaylor-530 <d.taylor530@gmail.com>
2025-01-23 10:59:19 +0000

    Utility.Interfaces Adds IAsyncClone Adds IGetData and ISetData Adds IRaiseExPropertyChanged Adds PropertyChangedExEventArgs Adds IAsyncClone to IReadOnlyTree

--- Interfaces/Utility.Interfaces/Properties/INotifyPropertyCalled.cs
+++ Interfaces/Utility.Interfaces/Properties/INotifyPropertyCalled.cs
@@ -18,6 +18,6 @@ namespace Utility.Interfaces
         }
 
         public object? Value { get; }
-    }
+    }    
 
 }
--- Interfaces/Utility.Interfaces/Properties/IRaisePropertyChanged.cs
+++ Interfaces/Utility.Interfaces/Properties/IRaisePropertyChanged.cs
@@ -1,9 +1,17 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 namespace Utility.Interfaces
 {
     public interface IRaisePropertyChanged
     {
-        public void RaisePropertyChanged(object value, string? propertyName = null);
+        public void RaisePropertyChanged([CallerMemberName] string? propertyName = null);
+
+    }
+
+    public interface IRaiseExPropertyChanged
+    {
+        public void RaisePropertyChanged<T>(ref T previousValue, T value, [CallerMemberName] string? propertyName = null);
 
     }
 }
