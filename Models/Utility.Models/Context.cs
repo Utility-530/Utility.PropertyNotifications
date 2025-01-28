e304b1a6ef7bdc2b687913de490eb3afb3933dac
dtaylor-530 <d.taylor530@gmail.com>
2025-01-28 11:52:17 +0000

    Utility.Models Adds Context

---
+++ Models/Utility.Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using Utility.Interfaces.NonGeneric;
+
+namespace Utility.Models
+{
+    public class Context : IContext
+    {
+        private readonly SynchronizationContext ui;
+
+        public Context()
+        {
+            ui = SynchronizationContext.Current ?? throw new Exception($"Expected {nameof(SynchronizationContext)}!");
+        }
+
+
+        public SynchronizationContext UI => ui;
+
+        public static Context Instance { get; } = new();
+
+    }
+}
