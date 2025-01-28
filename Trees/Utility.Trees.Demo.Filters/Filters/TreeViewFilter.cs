31667f1509ba0dfb6a675bddf44eb3e1d90f891e
dtaylor-530 <d.taylor530@gmail.com>
2025-01-28 12:54:46 +0000

    Utility.Trees.Demo.Filters Updates TreeViewFilter

--- Trees/Utility.Trees.Demo.Filters/Filters/TreeViewFilter.cs
+++ Trees/Utility.Trees.Demo.Filters/Filters/TreeViewFilter.cs
@@ -18,75 +18,75 @@ namespace Utility.Trees.Demo.Filters
         public TreeViewFilter()
         {
             Predicate =
-                 new BooleanDecisionTree<IReadOnlyTree>(new Decision(item => true))
+                 new TrueDecisionTree<IReadOnlyTree>(new Decision(item => true))
                  {
                      // prevents child items of a parent Descriptor that is being shown as a Treeviewer from also being shown
-                     new BooleanDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Data as IPropertiesDescriptor != null))
+                     new TrueDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Data as IPropertiesDescriptor != null))
                         {
-                            new BooleanDecisionTree<IReadOnlyTree>(new Decision(item => (((IReadOnlyTree)item).Data as IPropertiesDescriptor).Type == typeof(Table)))
+                            new TrueDecisionTree<IReadOnlyTree>(new Decision(item => (((IReadOnlyTree)item).Data as IPropertiesDescriptor).Type == typeof(Table)))
                             {
-                                new BooleanDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Parent == null), a => false)
+                                new TrueDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Parent == null), a => false)
                                 {
 
                                 },
-                                new BooleanDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Parent != null), a => a.Parent)
+                                new TrueDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Parent != null), a => a.Parent)
                                 {
-                                    new BooleanDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Parent == null), a => false)
+                                    new TrueDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Parent == null), a => false)
                                 {
 
                                 },
-                                    new BooleanDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Parent != null),a=>a.Parent)
+                                    new TrueDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Parent != null),a=>a.Parent)
                                     {
-                                        new BooleanDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Data as ICollectionDescriptor ==null), a => false),
+                                        new TrueDecisionTree<IReadOnlyTree>(new Decision(item => ((IReadOnlyTree)item).Data as ICollectionDescriptor ==null), a => false),
                                     }
                                 }
                             }
                         },
 
-                      new BooleanDecisionTree<IReadOnlyTree>(new Decision(item => true) { }, a => a.Data)
+                      new TrueDecisionTree<IReadOnlyTree>(new Decision(item => true) { }, a => a.Data)
                     {
-                        new BooleanDecisionTree(new Decision(item => item as IMethodDescriptor!=null){  })
+                        new TrueDecisionTree(new Decision(item => item as IMethodDescriptor!=null){  })
                         {
-                            new BooleanDecisionTree(new Decision<IMethodDescriptor>(md=>md.Type.IsArray==false),  a=> true)
+                            new TrueDecisionTree(new Decision<IMethodDescriptor>(md=>md.Type.IsArray==false),  a=> true)
                         },
-                        new BooleanDecisionTree(new Decision(item => item as IDescriptor != null && (item as IDescriptor).ParentType!=null))
+                        new TrueDecisionTree(new Decision(item => item as IDescriptor != null && (item as IDescriptor).ParentType!=null))
                         {
-                            new BooleanDecisionTree(new Decision<IDescriptor>(item => item as IPropertiesDescriptor!=null))
+                            new TrueDecisionTree(new Decision<IDescriptor>(item => item as IPropertiesDescriptor!=null))
                             {
-                                new BooleanDecisionTree(new Decision<IDescriptor>(item => item.Type.IsAssignableTo(typeof(IEnumerable))),  a=> false)
+                                new TrueDecisionTree(new Decision<IDescriptor>(item => item.Type.IsAssignableTo(typeof(IEnumerable))),  a=> false)
                             },
-                            new BooleanDecisionTree(new Decision<IDescriptor>(item => item as ICollectionItemDescriptor!=null))
+                            new TrueDecisionTree(new Decision<IDescriptor>(item => item as ICollectionItemDescriptor!=null))
                             {
-                                new BooleanDecisionTree(new Decision<IDescriptor>(item => (item as ICollectionItemDescriptor).Removed != null ),  a=> false)
+                                new TrueDecisionTree(new Decision<IDescriptor>(item => (item as ICollectionItemDescriptor).Removed != null ),  a=> false)
                             },
-                            new BooleanDecisionTree<IDescriptor>(new Decision<IDescriptor>(item => item.ParentType.Name == "Array"), item => item.Name)
+                            new TrueDecisionTree<IDescriptor>(new Decision<IDescriptor>(item => item.ParentType.Name == "Array"), item => item.Name)
                             {
-                                new BooleanDecisionTree( new Decision<string>(item => item == "IsFixedSize"),  a=> false) { },
-                                new BooleanDecisionTree( new Decision<string>(item => item == "IsReadOnly"),  a=> false) { },
-                                new BooleanDecisionTree( new Decision<string>(item => item == "IsSynchronized"),  a=> false) { },
-                                new BooleanDecisionTree(new Decision<string>(item => item == "LongLength"),  a=> false) { },
-                                new BooleanDecisionTree(new Decision<string>(item => item == "Length"), a=> false) { },
-                                new BooleanDecisionTree(new Decision<string>(item => item == "SyncRoot"),  a=> false) { },
+                                new TrueDecisionTree( new Decision<string>(item => item == "IsFixedSize"),  a=> false) { },
+                                new TrueDecisionTree( new Decision<string>(item => item == "IsReadOnly"),  a=> false) { },
+                                new TrueDecisionTree( new Decision<string>(item => item == "IsSynchronized"),  a=> false) { },
+                                new TrueDecisionTree(new Decision<string>(item => item == "LongLength"),  a=> false) { },
+                                new TrueDecisionTree(new Decision<string>(item => item == "Length"), a=> false) { },
+                                new TrueDecisionTree(new Decision<string>(item => item == "SyncRoot"),  a=> false) { },
                             },
-                            new BooleanDecisionTree<IDescriptor>(new Decision<IDescriptor>(item => item.ParentType.Name == "String"),item =>item.Name)
+                            new TrueDecisionTree<IDescriptor>(new Decision<IDescriptor>(item => item.ParentType.Name == "String"),item =>item.Name)
                             {
-                                new BooleanDecisionTree(new Decision<string>(item => item == "Length"),  a=> true) { }
+                                new TrueDecisionTree(new Decision<string>(item => item == "Length"),  a=> true) { }
                             },
-                            new BooleanDecisionTree<IDescriptor>(new Decision<IDescriptor>(item => item.ParentType.IsAssignableTo(typeof(IList))), item =>item.Name)
+                            new TrueDecisionTree<IDescriptor>(new Decision<IDescriptor>(item => item.ParentType.IsAssignableTo(typeof(IList))), item =>item.Name)
                             {
-                            new BooleanDecisionTree(new Decision<string>(item => item == nameof(IList.Remove)),  a=> false) { },
-                            new BooleanDecisionTree(  new Decision<string>(item => item == nameof(IList.GetEnumerator)),  a=> false) { },
-                            new BooleanDecisionTree(  new Decision<string>(item => item == nameof(IList.CopyTo)),  a=> false) { },
-                            new BooleanDecisionTree(  new Decision<string>(item => item == nameof(IList.IndexOf)),  a=> false) { },
-                            new BooleanDecisionTree(   new Decision<string>(item => item == nameof(IList.Contains)),  a=> false) { },
-                            new BooleanDecisionTree(   new Decision<string>(item => item == nameof(IList.Add)),  a=> false) { },
-                            new BooleanDecisionTree(   new Decision<string>(item => item == nameof(IList.RemoveAt)),  a=> false) { },
-                            new BooleanDecisionTree(  new Decision<string>(item => item == nameof(ObservableCollection<object>.Move))) { },
-                            new BooleanDecisionTree(   new Decision<string>(item => item == nameof(IList.Remove)),  a=> false) { },
-                            new BooleanDecisionTree(  new Decision<string>(item => item == nameof(IList.Insert)),  a=> false) { },
-                            new BooleanDecisionTree(  new Decision<string>(item => item == nameof(IList.Add)), a=> false),
-                            new BooleanDecisionTree(  new Decision<string>(item => item == nameof(IList.Count)), a=> false),
-                            new BooleanDecisionTree(  new Decision<string>(item => item == nameof(List<object>.Capacity)), a=> false)
+                            new TrueDecisionTree(new Decision<string>(item => item == nameof(IList.Remove)),  a=> false) { },
+                            new TrueDecisionTree(  new Decision<string>(item => item == nameof(IList.GetEnumerator)),  a=> false) { },
+                            new TrueDecisionTree(  new Decision<string>(item => item == nameof(IList.CopyTo)),  a=> false) { },
+                            new TrueDecisionTree(  new Decision<string>(item => item == nameof(IList.IndexOf)),  a=> false) { },
+                            new TrueDecisionTree(   new Decision<string>(item => item == nameof(IList.Contains)),  a=> false) { },
+                            new TrueDecisionTree(   new Decision<string>(item => item == nameof(IList.Add)),  a=> false) { },
+                            new TrueDecisionTree(   new Decision<string>(item => item == nameof(IList.RemoveAt)),  a=> false) { },
+                            new TrueDecisionTree(  new Decision<string>(item => item == nameof(ObservableCollection<object>.Move))) { },
+                            new TrueDecisionTree(   new Decision<string>(item => item == nameof(IList.Remove)),  a=> false) { },
+                            new TrueDecisionTree(  new Decision<string>(item => item == nameof(IList.Insert)),  a=> false) { },
+                            new TrueDecisionTree(  new Decision<string>(item => item == nameof(IList.Add)), a=> false),
+                            new TrueDecisionTree(  new Decision<string>(item => item == nameof(IList.Count)), a=> false),
+                            new TrueDecisionTree(  new Decision<string>(item => item == nameof(List<object>.Capacity)), a=> false)
                         }
 
                         }
@@ -100,7 +100,7 @@ namespace Utility.Trees.Demo.Filters
             Predicate.Reset();
             Predicate.Input = item;
             Predicate.Evaluate();
-            return (bool)Predicate.Backput;
+            return (bool)(Predicate.Backput ?? true);
         }
 
 
