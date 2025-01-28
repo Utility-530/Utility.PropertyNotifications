086dd109afacaa04db2491f4d054b7f2128a4685
dtaylor-530 <d.taylor530@gmail.com>
2025-01-28 11:41:55 +0000

    Utility.Trees.Decisions Divides BooleanDecisionTrees into True and False types depending on the default value Removes exception throwing and returns null from Transform method for generic DecisionTree if unexpected type encountered

--- Trees/Utility.Trees.Decisions/DecisionTrees.cs
+++ Trees/Utility.Trees.Decisions/DecisionTrees.cs
@@ -23,7 +23,7 @@ namespace Utility.Trees.Decisions
             Data = decision;
         }
 
-        protected override object ToBackPut(ICollection<object> backputs)
+        protected override object? ToBackPut(ICollection<object?> backputs)
         {
             return backputs.FirstOrDefault(a => a is string);
         }
@@ -39,59 +39,93 @@ namespace Utility.Trees.Decisions
             Data = decision;
         }
 
-        protected override object ToBackPut(ICollection<object> backputs)
+        protected override object? ToBackPut(ICollection<object?> backputs)
         {
             return backputs.FirstOrDefault(a => a is TR);
         }
     }
 
 
-    public class BooleanDecisionTree<T> : DecisionTree<T>
+    public class TrueDecisionTree<T> : BooleanDecisionTree<T>
     {
-
-        public BooleanDecisionTree(IDecision decision, Func<T, object>? transform = null) : base(decision, transform)
+        public TrueDecisionTree(IDecision decision, Func<T, object>? transform = null) : base(decision, true, transform)
         {
             Data = decision;
         }
+    }
+
+    public class FalseDecisionTree<T> : BooleanDecisionTree<T>
+    {
+        public FalseDecisionTree(IDecision decision, Func<T, object>? transform = null) : base(decision, false, transform)
+        {
+            Data = decision;
+        }
+    }
+     
+    public class BooleanDecisionTree<T> : DecisionTree<T>
+    {
+
+        public BooleanDecisionTree(IDecision decision, bool defaultValue, Func<T, object>? transform = null) : base(decision, transform)
+        {
+            Data = decision;
+            DefaultValue = defaultValue;
+        }
 
         public AndOr AndOr { get; set; }
+        public bool DefaultValue { get; }
 
-
-
-        protected override object ToBackPut(ICollection<object> backputs)
+        protected override object? ToBackPut(ICollection<object?> backputs)
         {
             if (AndOr is AndOr.And)
             {
-                return backputs.All(a => a is bool b ? b : true);
+                return backputs.All(a => a is bool b ? b : DefaultValue);
             }
             if (AndOr is AndOr.Or)
             {
-                return backputs.Any(a => a is bool b ? b : true);
+                return backputs.Any(a => a is bool b ? b : DefaultValue);
             }
             throw new Exception(" £3344333");
         }
     }
 
-    public class BooleanDecisionTree : DecisionTree
+    public class TrueDecisionTree : BooleanDecisionTree
     {
-        public BooleanDecisionTree(IDecision decision, Func<object, object>? transform = null) : base(decision, transform)
+        public TrueDecisionTree(IDecision decision, Func<object, object>? transform = null) : base(decision, true, transform)
         {
             Data = decision;
         }
 
+    }
+    public class FalseDecisionTree : BooleanDecisionTree
+    {
+        public FalseDecisionTree(IDecision decision, Func<object, object>? transform = null) : base(decision, false, transform)
+        {
+            Data = decision;
+        }
+
+    }
+
+    public class BooleanDecisionTree : DecisionTree
+    {
+        public BooleanDecisionTree(IDecision decision, bool defaultValue, Func<object, object?>? transform = null) : base(decision, transform)
+        {
+            Data = decision;
+            DefaultValue = defaultValue;
+        }
+
         public AndOr AndOr { get; set; }
 
+        public bool DefaultValue { get; }
 
-
-        protected override object ToBackPut(ICollection<object> backputs)
+        protected override object? ToBackPut(ICollection<object?> backputs)
         {
             if (AndOr is AndOr.And)
             {
-                return backputs.All(a => a is bool b ? b : true);
+                return backputs.All(a => a is bool b ? b : DefaultValue);
             }
             if (AndOr is AndOr.Or)
             {
-                return backputs.Any(a => a is bool b ? b : true);
+                return backputs.Any(a => a is bool b ? b : DefaultValue);
             }
             throw new Exception(" £3344333");
         }
@@ -99,19 +133,20 @@ namespace Utility.Trees.Decisions
 
     public class DecisionTree<T> : DecisionTree
     {
-        private readonly Func<T, object>? transform;
+        private readonly Func<T, object?> transform;
 
-        public DecisionTree(IDecision decision, Func<T, object>? transform = null) : base(decision)
+        public DecisionTree(IDecision decision, Func<T, object?>? transform = null) : base(decision)
         {
             Data = decision;
-            this.transform = transform ?? new Func<T, object>(a => a);
+            this.transform = transform ?? new Func<T, object?>(a => a);
         }
 
-        protected override object Transform(object value)
+        protected override object? Transform(object? value)
         {
             if (value is not T t)
             {
-                throw new Exception("fds df33 dffd");
+                //throw new Exception("fds df33 dffd");
+                return null; 
             }
 
             return transform.Invoke(t);
@@ -122,23 +157,23 @@ namespace Utility.Trees.Decisions
     public class DecisionTree : Tree
     {
         protected readonly IDecision decision;
-        private object input;
-        private object output;
-        private object backput;
-        private Func<object, object>? transform;
+        private object? input;
+        private object? output;
+        private object? backput;
+        private readonly Func<object, object?> transform;
         private bool hasEvaluated;
 
-        public DecisionTree(IDecision decision, Func<object, object>? transform = null, bool match = true)
+        public DecisionTree(IDecision decision, Func<object, object?>? transform = null, bool match = true)
         {
             this.decision = decision;
             Match = match;
-            this.transform = transform ?? new Func<object, object>(a => a);
+            this.transform = transform ?? new Func<object, object?>(a => a);
         }
 
 
         public virtual void Evaluate()
         {
-            List<object> outputs = new();
+            List<object?> outputs = new();
 
             //var compile = (Data as IDecision).Predicate.Compile().Invoke(Input);
 
@@ -147,6 +182,8 @@ namespace Utility.Trees.Decisions
             if (result == Match)
             {
                 Output = Transform(Input);
+                if (Output == null)
+                    return;
                 if (Items.Any())
                 {
                     foreach (DecisionTree item in Items)
@@ -218,20 +255,20 @@ namespace Utility.Trees.Decisions
         //    Backput = null;
         //}
 
-        protected virtual object Transform(object value)
+        protected virtual object? Transform(object? value)
         {
             if (transform != null)
                 return transform.Invoke(value);
             throw new Exception(" ew332 324");
         }
 
-        protected virtual object ToBackPut(ICollection<object> backputs)
+        protected virtual object? ToBackPut(ICollection<object?> backputs)
         {
             return backputs.SingleOrDefault(a => a != null);
         }
 
 
-        public object Input
+        public object? Input
         {
             get => input; set
             {
@@ -250,7 +287,7 @@ namespace Utility.Trees.Decisions
             }
         }
 
-        public object Output
+        public object? Output
         {
             get => output; set
             {
@@ -259,7 +296,7 @@ namespace Utility.Trees.Decisions
             }
         }
 
-        public object Backput
+        public object? Backput
         {
             get => backput; set
             {
