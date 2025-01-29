4dd10a0b9cff9e1ede22fc6d7b2ca3487a3eb4c5
dtaylor-530 <d.taylor530@gmail.com>
2025-01-29 16:07:36 +0000

    Utility.Json.Newtonsoft Adds SettingsFactory

---
+++ Conversions/Utility.Json.Newtonsoft/SettingsFactory.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Conversions.Json.Newtonsoft
+{
+    public static class SettingsFactory
+    {
+        static JsonSerializerSettings combined;
+
+        public static JsonSerializerSettings Combined
+        {
+            get
+            {
+                if (combined != null)
+                    return combined;
+
+                var _settings = new JsonSerializerSettings
+                { 
+                    TypeNameHandling = TypeNameHandling.All, 
+                    CheckAdditionalContent = false
+                };
+
+                foreach (var setting in converters())
+                {
+                    _settings.Converters.Add(setting);
+                }
+                return combined = _settings;
+
+                IEnumerable<JsonConverter> converters()
+                {
+                    yield return new AssemblyJsonConverter();
+                    yield return (new PropertyInfoJsonConverter());
+                    yield return (new MethodInfoJsonConverter());
+                    yield return (new ParameterInfoJsonConverter());
+                    yield return (new AttributeCollectionConverter());
+                    yield return (new Utility.Conversions.Json.Newtonsoft.DescriptorConverter());
+                }
+            }
+        }
+
+    }
+}
