cd910e841c90fe57aa5c6baad7cc661dd8b47949
dtaylor-530 <d.taylor530@gmail.com>
2025-02-09 14:11:36 +0000

    EnumSourceConverter GetTypeEnumConverter Fixes issue using cache withe GetTypeEnumConverter

--- EnumSourceConverter/GetTypeEnumConverter.cs
+++ EnumSourceConverter/GetTypeEnumConverter.cs
@@ -11,7 +11,7 @@ namespace Tonic.UI
 {
     class GetTypeEnumConverter : IValueConverter
     {
-        private object cache;
+        private Dictionary<object, object> cache = new Dictionary<object, object>();
 
         public BindingExpression Expression { get; set; }
         /// <summary>
@@ -21,29 +21,34 @@ namespace Tonic.UI
         /// <summary>
         /// Null if the type is still unknown
         /// </summary>
-        private Type Type { get; set; }
+        //private Type Type { get; set; }
 
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (Expression == null) return null;
 
-            if (Type == null)
-                Type = Expression.ResolvedSource.GetType().GetProperty(Expression.ResolvedSourcePropertyName).GetValue(Expression.ResolvedSource)?.GetType();
-
-            if(Type ==null)
+            if (value == null)
                 return DependencyProperty.UnsetValue;
+            if (cache.ContainsKey(value) == false)
+            {
+                //if (Type == null)
+                var Type = Expression.ResolvedSource.GetType().GetProperty(Expression.ResolvedSourcePropertyName).GetValue(Expression.ResolvedSource)?.GetType();
 
-            //El cache no es solo por rendimiento, si no es utilizado, cada vez que se llama a este metodo,
-            //aunque sean los mismos valores devuelve una instancia de un arreglo diferente, lo que ocasiona que
-            //WPF actualize el valor de la propiedad, ocacionando un ciclo infinito
+                if (Type == null)
+                    return DependencyProperty.UnsetValue;
 
-            if (cache == null)
+                //El cache no es solo por rendimiento, si no es utilizado, cada vez que se llama a este metodo,
+                //aunque sean los mismos valores devuelve una instancia de un arreglo diferente, lo que ocasiona que
+                //WPF actualize el valor de la propiedad, ocacionando un ciclo infinito
+
+
                 if (UseDescription)
-                    cache = EnumSource.GetEnumValues(Type).Select(x => EnumValue.Create(x)).ToArray();
+                    cache[value] = EnumSource.GetEnumValues(Type).Select(x => EnumValue.Create(x)).ToArray();
                 else
-                    cache = EnumSource.GetEnumValues(Type);
-            return cache;
+                    cache[value] = EnumSource.GetEnumValues(Type);
+            }
+            return cache[value];
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
