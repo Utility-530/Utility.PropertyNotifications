42843b382a001f7e135a0f513328df700757e668
dtaylor-530 <d.taylor530@gmail.com>
2025-02-11 22:58:55 +0000

    Utility.WPF.Converters Adds TypeHierarchyConverter

---
+++ WPF/Utility.WPF.Converters/TypeHierarchyConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Utility.WPF.Converters
+{
+    public class TypeHierarchyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string s && Type.GetType(s) is Type _type)
+            {
+                return new string[] { _type.Assembly.GetName().Name, _type.Namespace, _type.Name };
+            }
+            if(value is Type type)
+            {
+                return new string[] { type.Assembly.GetName().Name, type.Namespace, type.Name };
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static TypeHierarchyConverter Instance { get; } = new();
+    }
+}
