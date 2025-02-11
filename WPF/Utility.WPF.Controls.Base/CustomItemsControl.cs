827467cf2a5f4a44023cf94e167660e025fc8757
dtaylor-530 <d.taylor530@gmail.com>
2025-02-11 22:58:01 +0000

    Utility.WPF.Controls.Base Adds CustomItemsControl

---
+++ WPF/Utility.WPF.Controls.Base/CustomItemsControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Utility.WPF.Controls.Base
+{
+    /// <summary>
+    /// Makes it possible to configure ItemContainerStyle 
+    /// </summary>
+    public class CustomItemsControl : ItemsControl
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new ContentControl();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            // Even wrap other ContentControls
+            return false;
+        }
+    }
+}
