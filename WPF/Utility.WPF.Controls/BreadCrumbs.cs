80b9355c66ae5a5bfc928a0faf7f68d8415e363a
dtaylor-530 <d.taylor530@gmail.com>
2025-02-11 22:57:45 +0000

    Utility.WPF.Controls Adds BreadCrumbs

---
+++ WPF/Utility.WPF.Controls/BreadCrumbs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Utility.WPF.Controls.Base;
+
+namespace Utility.WPF.Controls
+{
+    public class BreadCrumbs : CustomItemsControl
+    {
+
+        static BreadCrumbs()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BreadCrumbs), new FrameworkPropertyMetadata(typeof(BreadCrumbs)));
+        }
+
+
+
+    }
+}
