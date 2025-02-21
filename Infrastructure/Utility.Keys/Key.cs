c8bba3c65aef4c3062fbefd63aebc84029cd7708
dtaylor-530 <d.taylor530@gmail.com>
2025-02-21 18:26:46 +0000

    Utility.Keys Adds TypeNameKey

--- Infrastructure/Utility.Keys/Key.cs
+++ Infrastructure/Utility.Keys/Key.cs
@@ -1,10 +1,4 @@
-﻿using System.Formats.Asn1;
-using System.Globalization;
-using System.Linq;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using Utility.Interfaces.NonGeneric;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿
 
 namespace Utility.Keys
 {
---
+++ Infrastructure/Utility.Keys/TypeNameKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Keys
+{
+    public record TypeNameKey(Type Type, string Name) : Key
+    {
+    }
+}
