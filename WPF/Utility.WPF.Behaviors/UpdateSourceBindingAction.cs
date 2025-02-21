617cc9081eb3a848e18334b5fdf1da94d1b931f6
dtaylor-530 <d.taylor530@gmail.com>
2025-02-21 18:39:54 +0000

    Utility.WPF.Behaviors Adds GuidEditBehavior Adds UpdateSourceBindingAction

---
+++ WPF/Utility.WPF.Behaviors/UpdateSourceBindingAction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Utility.WPF.Behaviors
+{
+    public class UpdateSourceBindingAction : TriggerAction<FrameworkElement>
+    {
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register("Source", typeof(FrameworkElement), typeof(UpdateSourceBindingAction), new PropertyMetadata());
+
+        public FrameworkElement Source
+        {
+            get { return (FrameworkElement)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public DependencyProperty Property { get; set; }
+
+        protected override void Invoke(object parameter)
+        {
+            (Source ?? AssociatedObject).GetBindingExpression(Property).UpdateSource();
+        }
+
+    }
+}
