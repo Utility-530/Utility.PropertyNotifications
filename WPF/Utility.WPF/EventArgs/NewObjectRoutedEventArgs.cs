2afb144bf697f244344d64102ae5d39f945444c8
dtaylor-530 <d.taylor530@gmail.com>
2025-02-21 18:36:12 +0000

    Utility.WPF\EventArgs Adds NewObjectRoutedEventArgs

---
+++ WPF/Utility.WPF/EventArgs/NewObjectRoutedEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Utility.WPF
+{
+
+        public class NewObjectRoutedEventArgs : RoutedEventArgs
+        {
+            public NewObjectRoutedEventArgs(bool isAccepted, object newObject, RoutedEvent routedEvent, object source) : base(routedEvent, source)
+            {
+                NewObject = newObject;
+                IsAccepted = isAccepted;
+            }
+
+            public object NewObject { get; }
+            public bool IsAccepted { get; }
+        }
+
+        public delegate void FinishEditRoutedEventHandler(object sender, NewObjectRoutedEventArgs e);
+
+
+    
+}
