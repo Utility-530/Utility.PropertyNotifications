47541d41cd2ea083d669e2f35693d97a3f5b49ad
dtaylor-530 <d.taylor530@gmail.com>
2025-02-26 13:13:27 +0000

    Utility.WPF.Converters Adds AssemblyNameConverter Adds CheckedEventArgsConverter Adds TypeHierarchyConverter

---
+++ WPF/Utility.WPF.Converters/AssemblyNameConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Utility.WPF.Converters
+{
+    public class AssemblyNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Assembly assembly)
+            {
+                return assembly.GetName().Name;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static AssemblyNameConverter Instance { get; } = new AssemblyNameConverter();
+    }
+}
