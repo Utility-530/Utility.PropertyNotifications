2bb1189d20344f54cc7935be0c57ee4c966e26ba
dtaylor-530 <d.taylor530@gmail.com>
2025-02-26 12:30:15 +0000

    Utility.WPF.Controls.PropertyTrees PropertyTree: Separates NodeEngine into a separate file Changes TreeView to CustomTreeView Adds Schema property Adds default Edit template

---
+++ WPF/Utility.WPF.Controls.PropertyTrees/NodeEngine.cs
@@ -0,0 +1,241 @@
+﻿using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Reflection;
+using Splat;
+using Utility.Reactives;
+using Utility.Keys;
+using Utility.Structs.Repos;
+using Utility.Interfaces.Exs;
+using Utility.Changes;
+using Utility.Interfaces;
+using Utility.Helpers;
+using NetFabric.Hyperlinq;
+using Utility.Helpers.Generic;
+using Utility.Interfaces.NonGeneric;
+using Utility.PropertyNotifications;
+using Utility.PropertyDescriptors;
+
+namespace Utility.WPF.Controls.PropertyTrees
+{
+    public readonly record struct DirtyNode(string Property, INode Node);
+    public class NodeEngine : INodeSource
+    {
+        public static string New = "new";
+        public static readonly string Key = nameof(NodeEngine);
+
+        private Dictionary<string, MethodValue> dictionary;
+        private Dictionary<string, string> dictionaryMethodNameKeys = [];
+        private readonly ObservableCollection<KeyValuePair<string, PropertyChange>> dirtyNodes = [];
+
+        Lazy<IFilter> filter = new(() => Locator.Current.GetService<IFilter>());
+        Lazy<IExpander> expander = new(() => Locator.Current.GetService<IExpander>());
+        Lazy<IContext> context = new(() => Locator.Current.GetService<IContext>());
+
+        private readonly ObservableCollection<INode> nodes = [];
+
+        private NodeEngine()
+        {
+        }
+
+        public IReadOnlyCollection<INode> Nodes => nodes;
+        public ObservableCollection<KeyValuePair<string, PropertyChange>> DirtyNodes => dirtyNodes;
+        public static NodeEngine Instance { get; } = new();
+        string INodeSource.New => New;
+
+        public IObservable<INode?> Single(string key)
+        {
+            throw new NotImplementedException();
+            //return Many(key).Take(1);
+        }
+
+        public IObservable<INode> SingleByNameAsync(string name)
+        {
+            return nodes.SelfAndAdditions().Where(a => a.Data.ToString() == name).Take(1);
+        }
+
+        public IObservable<INode> ChildrenByGuidAsync(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime Remove(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int? MaxIndex(Guid guid, string v)
+        {
+            //return repository.MaxIndex(guid, v);
+            throw new NotImplementedException();
+
+        }
+
+        public void Remove(INode node)
+        {
+            nodes.Remove(node);
+        }
+
+        public IObservable<Structs.Repos.Key?> Find(Guid parentGuid, string name, Guid? guid = null, System.Type? type = null, int? localIndex = null)
+        {
+            throw new NotImplementedException();
+
+            // return repository.Find(parentGuid, name, guid, type, localIndex);
+        }
+
+        public IObservable<DateValue> Get(Guid guid, string name)
+        {
+            throw new NotImplementedException();
+            //return repository.Get(guid, name);
+        }
+
+        public void Set(Guid guid, string name, object value, DateTime dateTime)
+        {
+            // repository.Set(guid, name, value, dateTime);
+        }
+
+        public void Add(INode node)
+        {
+            if (node.Data is not IDescriptor descriptor)
+                throw new Exception("Vd 222");
+            if (node.Key is null)
+                node.Key = new GuidKey(Guid.NewGuid());
+            if (Nodes.Any(a => a.Key == node.Key) == false)
+            {
+                nodes.Add(node);
+                { 
+
+                    if (filter.Value?.Filter(node) == false)
+                    {
+                        node.IsVisible = false;
+                        return;
+                    }
+                    if (expander.Value?.Expand(node) == true)
+                    {
+                        node.IsExpanded = true;
+                    }
+
+                    node.PropertyChanged += Node_PropertyChanged;
+                } //);
+
+                configure(node);
+
+            }
+
+            void Node_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+            {
+                if (sender is INode node)
+                {
+                    if (e is PropertyChangedExEventArgs { PreviousValue: var previousValue, PropertyName: string name, Value: var value })
+                    {
+                        //context.Value.UI(() => dirtyNodes.Add(new(node.Key, new PropertyChange(sender, name, value, previousValue))));
+                    }
+                    else
+                        throw new Exception("ss FGre333333333");
+                }
+                else
+                    throw new Exception("dfd 4222243");
+            }
+
+            void configure(INode node)
+            {
+                if (node.Data is ICollectionItemReferenceDescriptor d)
+                {
+
+                }
+                node.WithChangesTo(a => a.Data)
+                .Where(a => a is not string)
+                .Take(1)
+                .Subscribe(data =>
+                {
+                    if (node.Data is ICollectionItemReferenceDescriptor d)
+                    {
+
+                    }
+                    if (data is ISetNode iSetNode)
+                    {
+                        iSetNode.SetNode(node);
+                    }
+                    if (data is IGetGuid guid && node.Key == null)
+                    {
+                        node.Key = new GuidKey(guid.Guid);
+                    }
+
+
+                    if (data is IYieldChildren ychildren)
+                        ychildren.Children.Cast<IDescriptor>().ForEach(async d =>
+                        {
+                            var newNode = await node.ToTree(d);
+                            node.Add(newNode);
+                            if (newNode.Data is IGetName { Name: "BaseType" })
+                            {
+
+                            }
+                            Add(newNode as INode);
+                        });
+                    else
+                    {
+
+                    }
+
+                });
+
+                async void change(Change a)
+                {
+                    if (a is Change { Type: Changes.Type.Add, Value: { } value })
+                    {
+                        if (value is INode _node)
+                            node.Add(_node);
+                        else
+                        {
+                            node.Add(await node.ToTree(value));
+                        }
+                    }
+                    else if (a is Change { Type: Changes.Type.Remove, Value: { } _value })
+                    {
+                        node.RemoveBy(c =>
+
+                        {
+                            if (c is IKey key)
+                            {
+                                if (_value is IKey _key)
+                                {
+                                    return key.Key.Equals(_key.Key);
+                                }
+                                else if (_value is IGetGuid guid)
+                                {
+                                    return key.Key.Equals(new GuidKey(guid.Guid));
+                                }
+                            }
+                            throw new Exception("44c dd");
+
+                        });
+                    }
+                }
+            }
+        }
+
+        public async void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<INode> Many(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MethodValue
+    {
+        public MethodInfo Method { get; set; }
+
+        public Task Task { get; set; }
+
+        public IList<INode> Nodes { get; set; } = [];
+    }
+}
--- WPF/Utility.WPF.Controls.PropertyTrees/PropertyTree.cs
+++ WPF/Utility.WPF.Controls.PropertyTrees/PropertyTree.cs
@@ -1,32 +1,26 @@
-
 using Splat;
-using System.Collections.ObjectModel;
-using System.Reactive.Disposables;
-using System.Reactive.Linq;
+using System.Collections;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using Utility.Changes;
-using Utility.Descriptors;
+using System.Windows.Data;
 using Utility.Interfaces.Exs;
 using Utility.Interfaces.NonGeneric;
-using Utility.Keys;
 using Utility.Models;
 using Utility.Nodes;
-using Utility.Nodes.Demo;
 using Utility.Nodes.WPF;
-using Utility.Reactives;
-using Utility.Structs.Repos;
-using Utility.Trees.Abstractions;
-using Type = System.Type;
+using Utility.PropertyDescriptors;
+using Utility.WPF.Controls.Trees;
+using Utility.WPF.Templates;
 
 namespace Utility.WPF.Controls.PropertyTrees
 {
-    public class PropertyTree : TreeView
+    public class PropertyTree : CustomTreeView
     {
-
+        static HashSet<string> keys = new();
+        private string _key;
         public static readonly DependencyProperty ObjectProperty = DependencyProperty.Register(nameof(Object), typeof(object), typeof(PropertyTree), new PropertyMetadata(null, changed));
+        public static readonly DependencyProperty SchemaProperty = DependencyProperty.Register("Schema", typeof(Schema), typeof(PropertyTree), new PropertyMetadata());
 
         private static void changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -35,34 +29,52 @@ namespace Utility.WPF.Controls.PropertyTrees
                 tree.ItemsSource = create(value).Items;
             }
 
-            static Node create(object obj)
+            Node create(object obj)
             {
-                Locator.Current.GetService<INodeSource>().Reset();
-                var root = DescriptorFactory.CreateRoot(obj, Guid.NewGuid(), "obj").Take(1).Wait();
+                var root = DescriptorFactory.CreateRoot(obj, tree._key);
                 var reflectionNode = new Node(root) { IsExpanded = true };
-  
-                Locator.Current.GetService<INodeSource>().Add(reflectionNode);
+                NodeEngine.Instance.Add(reflectionNode);
                 return reflectionNode;
             }
         }
 
         static PropertyTree()
         {
-
-            Style dynamicStyle = new(typeof(TreeView)) { };
+            Style dynamicStyle = new(typeof(CustomTreeView)) { };
             dynamicStyle.Setters.Add(new Setter(TreeView.ItemTemplateSelectorProperty, Utility.Nodes.WPF.DataTemplateSelector.Instance));
             dynamicStyle.Setters.Add(new Setter(TreeView.ItemContainerStyleSelectorProperty, Utility.Nodes.WPF.StyleSelector.Instance));
+        
+
             StyleProperty.OverrideMetadata(typeof(PropertyTree), new FrameworkPropertyMetadata(dynamicStyle));
+        }
+
+        static DataTemplate createEditTemplate()
+        {
+            return Factorys.TemplateGenerator.CreateDataTemplate(() =>
+            {
+                var propertyTree = new PropertyTree() { };
+                BindingOperations.SetBinding(
+                   propertyTree,
+                   PropertyTree.ObjectProperty,
+                   new Binding { Path = new PropertyPath(".") });
+                return propertyTree;
+            });
         }
 
 
         public PropertyTree()
         {
-            Locator.CurrentMutable.RegisterLazySingleton<ITreeRepository>(() => new InMemoryTreeRepository());
-            Locator.CurrentMutable.RegisterLazySingleton<INodeSource>(() => new NodeSource());
+            string key = "obj";
+            while (keys.Add(key) == false)
+            {
+                key = Utility.Randoms.Next.Instance.Word();
+            }
+            this._key = key;
+       
             Locator.CurrentMutable.RegisterConstant<IContext>(new Context());
             Locator.CurrentMutable.RegisterConstant<IFilter>(TreeViewFilter.Instance);
             Locator.CurrentMutable.RegisterConstant<IExpander>(Utility.Nodes.WPF.Expander.Instance);
+            Locator.CurrentMutable.RegisterConstant<System.Windows.Controls.DataTemplateSelector>(CustomDataTemplateSelector.Instance);
         }
 
 
@@ -73,144 +85,34 @@ namespace Utility.WPF.Controls.PropertyTrees
             set { SetValue(ObjectProperty, value); }
         }
 
+        public Schema Schema
+        {
+            get { return (Schema)GetValue(SchemaProperty); }
+            set { SetValue(SchemaProperty, value); }
+        }
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            if(element is CustomTreeViewItem treeViewItem && item is Node{ Data: ICollectionDescriptor collectionDescriptor })
+            {
+                var innerType = collectionDescriptor.ElementType;
+                treeViewItem.NewObject = ActivateAnything.Activate.New(innerType);
+            }
+
+            base.PrepareContainerForItemOverride(element, item);
+        }
         protected override DependencyObject GetContainerForItemOverride()
         {
             var item = base.GetContainerForItemOverride();
-            //if(item is TreeViewItem tvi)
-            //{
-            //    tvi.IsExpanded = true;
-            //}
+
+            if(item is CustomTreeViewItem customTreeViewItem)
+            {
+                customTreeViewItem.EditTemplate = createEditTemplate();
+   
+            }
             return item;
         }
     }
-
-
-    public class NodeSource : INodeSource
-    {
-        ObservableCollection<INode> nodes = [];
-        public string New { get; }
-
-        public void Add(INode node)
-        {
-            if (Locator.Current.GetService<IFilter>().Filter(node) == false)
-            {
-                node.IsVisible = false;
-            }
-            else
-            {
-                Locator.Current.GetService<IContext>().UI.Post((a) => nodes.Add(node), null);
-            }
-        }
-
-        public void Reset()
-        {
-            Locator.Current.GetService<ITreeRepository>().Reset();
-        }
-
-
-        public IObservable<INode> ChildrenByGuidAsync(Guid guid)
-        {
-            bool b = false;
-            return Observable.Create<INode>(observer =>
-            {
-                CompositeDisposable composite = new();
-                nodes
-                .AndAdditions<INode>()
-                .Subscribe(node =>
-                {
-                    if (node.Key == new GuidKey(guid))
-                    {
-                        if (node.Data is IChildren children )
-                        {
-                            if (b == false)
-                            {
-                                b = true;
-                                children.Children.Subscribe(async child =>
-                                {
-                                    if (child is Change<IDescriptor> { Type: Changes.Type.Add } change)
-                                    {
-                                        var tree = await (node as Node).ToTree(change.Value);
-
-                                        if (tree is INode _node)
-                                        {
-                                            _node.IsExpanded = true;
-                                            observer.OnNext(_node);
-                                            this.Add(_node);
-                                        }
-                                        else
-                                            throw new Exception(" $43333");
-
-                                    }
-                                }, () => observer.OnCompleted())
-                                .DisposeWith(composite);
-                            }
-                            else
-                            {
-
-             //                   children.Children.Subscribe(async child =>
-             //                   {
-             //                       if (child is Change<IDescriptor> { Type: Changes.Type.Add } change)
-             //                       {
-             //                           var tree = await (node as Node).ToTree(change.Value);
-
-             //                           if (tree is INode _node)
-             //                           {
-             //                               _node.IsExpanded = true;
-             //                               observer.OnNext(_node);
-             //                               this.Add(_node);
-             //                           }
-             //                           else
-             //                               throw new Exception(" $43333");
-
-             //                       }
-             //                   }, () => observer.OnCompleted())
-             //.DisposeWith(composite);
-
-                            }
-                            return;
-                        }
-
-                        throw new Exception("sdfg 324333");
-                    }
-                }).DisposeWith(composite);
-                return composite;
-            });
-        }
-
-        public IObservable<Structs.Repos.Key?> Find(Guid parentGuid, string name, Guid? guid = null, Type? type = null, int? localIndex = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IObservable<DateValue> Get(Guid guid, string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int? MaxIndex(Guid guid, string v)
-        {
-            throw new NotImplementedException();
-        }
-
-        public DateTime Remove(Guid guid)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(INode node)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public void Set(Guid guid, string name, object value, DateTime dateTime)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IObservable<INode?> Single(string v)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
+
+
