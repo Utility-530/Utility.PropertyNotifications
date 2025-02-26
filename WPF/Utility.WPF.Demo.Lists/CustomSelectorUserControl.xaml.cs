b1062c8803201fe10524d4c37adb69aac4455040
dtaylor-530 <d.taylor530@gmail.com>
2025-02-26 13:14:21 +0000

    Utility.WPF.Demo.Lists Adds CustomSelectorUserControl

---
+++ WPF/Utility.WPF.Demo.Lists/CustomSelectorUserControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Utility.Commands;
+using Utility.WPF.Demo.Data.Model;
+
+namespace Utility.WPF.Demo.Lists
+{
+    /// <summary>
+    /// Interaction logic for CustomSelectorUserControl.xaml
+    /// </summary>
+    public partial class CustomSelectorUserControl : UserControl
+    {
+        public CustomSelectorUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class CustomSelectorViewModel
+    {
+        private Character newObject = new Character() { Color = Colors.Aqua };
+
+        public CustomSelectorViewModel()
+        {
+            FinishEdit = new Command<object>(o =>
+            {
+                if (o is NewObjectRoutedEventArgs args)
+                {
+                    Items.Add(args.NewObject as Character);
+                }
+            });
+
+            var res = new Utility.WPF.Demo.Data.Resources();
+            Items = new(res["Characters"] as Character[]);
+
+        }
+
+        public ICommand FinishEdit { get; }
+
+        public ObservableCollection<Character> Items { get; }
+        public Character NewObject
+        {
+            get => newObject; set
+            {
+                newObject = value;
+            }
+        }
+    }
+}
