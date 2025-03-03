3e104677c2bb99ee78ac2861942bfe156ccd963d
dtaylor-530 <d.taylor530@gmail.com>
2025-03-03 09:50:42 +0000

    Utility.WPF.ResourceDictionarys Adds GeometryResourceDictionary

---
+++ WPF/Utility.WPF.ResourceDictionarys/GeometryResourceDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.WPF.ResourceDictionarys
+{
+    public class GeometryResourceDictionary : SharedResourceDictionary
+    {
+        public GeometryResourceDictionary()
+        {
+            Source = new Uri($"/Utility.WPF.Geometries;component/Resources.xaml", UriKind.RelativeOrAbsolute);
+        }
+    }
+}
