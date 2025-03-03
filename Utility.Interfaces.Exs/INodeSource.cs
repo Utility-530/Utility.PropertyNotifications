7cd127b51940e46c030be18665fad8f8c2d1fad9
dtaylor-530 <d.taylor530@gmail.com>
2025-03-03 09:18:53 +0000

    Utility.Interfaces.Ex INodeSource Removes ChildrenByGuidAsync Adds Save

--- Utility.Interfaces.Exs/INodeSource.cs
+++ Utility.Interfaces.Exs/INodeSource.cs
@@ -6,9 +6,7 @@ namespace Utility.Interfaces.Exs
     public interface INodeSource
     {
         string New { get; }
-
         DateTime Remove(Guid guid);
-        IObservable<INode> ChildrenByGuidAsync(Guid guid);
         int? MaxIndex(Guid guid, string v);
         void Remove(INode node);
         IObservable<Key?> Find(Guid parentGuid, string name, Guid? guid = null, Type? type = null, int? localIndex = null);
@@ -16,7 +14,7 @@ namespace Utility.Interfaces.Exs
         void Set(Guid guid, string name, object value, DateTime dateTime);
         void Add(INode node);
         IObservable<INode?> Single(string v);
-
         void Reset();
+        void Save();
     }
 }
