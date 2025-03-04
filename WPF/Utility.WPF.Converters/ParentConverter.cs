f282f12b27971f42774ec7a28e351aacd06ca684
dtaylor-530 <d.taylor530@gmail.com>
2025-03-04 22:55:05 +0000

    Utility.WPF.Converters Adds ParentConverter Adds PropertyNamesConverter

---
+++ WPF/Utility.WPF.Converters/ParentConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using Utility.WPF.Helpers;
+
+namespace Utility.WPF.Converters
+{
+    public class ParentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is FrameworkElement child)
+            {
+                if (parameter is int i )
+                {
+                    if (i == 0)
+                    {
+                        return VisualTreeHelper.GetParent(child);
+                    }   
+           
+                    else
+                    {
+                        return Convert(VisualTreeHelper.GetParent(child), targetType, i - 1, culture);
+                    }
+                }           
+                if (int.TryParse(parameter.ToString(), out var i2))
+                {
+                    if (i2 == 0)
+                    {
+                        return VisualTreeHelper.GetParent(child);
+                    }   
+           
+                    else
+                    {
+                        return Convert(VisualTreeHelper.GetParent(child), targetType, i2 - 1, culture);
+                    }
+                }
+                else
+                {
+                    return VisualTreeHelper.GetParent(child);
+                }
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static ParentConverter Instance { get; } = new();
+    }
+}
---
+++ WPF/Utility.WPF.Converters/PropertyNamesConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using Utility.Helpers;
+
+namespace Utility.WPF.Converters
+{
+    public class PropertyNamesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return DependencyProperty.UnsetValue;
+
+            Type type = value.GetType();
+            if (parameter is PropertyType propertyType)
+            {
+                var flags = EnumHelper.GetFlags(propertyType);
+                if (flags.Contains(PropertyType.GenericArgument1))
+                    type = type.GetGenericArguments().First();
+
+                else if (flags.Contains(PropertyType.GenericArgument2))
+                    type = type.GetGenericArguments().Skip(1).First();
+
+
+                if (flags.Contains(PropertyType.TopLevel))
+                    return Utility.Helpers.PropertyHelper.TopLevelPublicInstanceProperties(type).Select(a => a.Name).ToArray();
+                else
+                    return Utility.Helpers.PropertyHelper.PublicInstanceProperties(type).Select(a => a.Name);
+            }
+            return Utility.Helpers.PropertyHelper.PublicInstanceProperties(type).Select(a => a.Name);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum PropertyType
+    {
+        Default,
+        GenericArgument1,
+        GenericArgument2,
+        TopLevel,
+        TopLevelAndGenericArgument1 = GenericArgument1 | TopLevel,
+        TopLevelAndGenericArgument2 = GenericArgument2 | TopLevel,
+
+    }
+}
