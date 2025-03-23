cf6a8a3895797dca8add3eede08f1d86596ea3de
dtaylor-530 <d.taylor530@gmail.com>
2025-03-23 20:07:58 +0000

    Utility.Keys Adds null condition for Key string implicit operator

--- Infrastructure/Utility.Keys/Key.cs
+++ Infrastructure/Utility.Keys/Key.cs
@@ -4,9 +4,6 @@ namespace Utility.Keys
 {
     public record Key 
     {
-        public static implicit operator string(Key d) => d.ToString();
-        //public static explicit operator Digit(byte b) => new Digit(b);
-
-
+        public static implicit operator string?(Key d) => d?.ToString();
     }
 }
