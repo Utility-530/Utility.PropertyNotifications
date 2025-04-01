5bbfb1b16955709f5bbb3673a79ea771f3fd5394
dtaylor-530 <d.taylor530@gmail.com>
2025-04-01 13:34:28 +0100

    Utility.WPF.Demo.Html Adds

---
+++ WPF/Utility.WPF.Demo.Html/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using TheArtOfDev.HtmlRenderer.Demo.Common;
+using TheArtOfDev.HtmlRenderer.Demo.WPF;
+using TheArtOfDev.HtmlRenderer.WPF;
+using Utility.WPF.Controls.Html;
+
+namespace Utility.WPF.Demo.Html
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public const string PerformanceSamplesTreeNodeName = "Performance Samples";
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SamplesLoader.Init("WPF", typeof(HtmlRender).Assembly.GetName().Version.ToString());
+
+            var grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(160) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            var treeSel = new TreeSelectionView();
+            //var mainView = new HtmlView();
+            //var mainView = new Controls.Html.HtmlEditorControl();
+            var mainView = new Controls.Html.TinyHtmlEditorControl();
+            Grid.SetColumn(mainView, 1);
+            treeSel.LoadSamples();
+            treeSel.SelectionChanged += (s, e) =>
+            {
+                if (e.Data.Item.Parent is TreeViewItem tvi)
+                {
+                    if (!Equals(tvi.Header, PerformanceSamplesTreeNodeName))
+                    {
+                        //mainView.SetColoredText(e.Data.Sample.Html);
+                    }
+                }
+                else
+                {
+                    return;
+                }
+
+
+
+                mainView.Html = e.Data.Sample.Html;
+            };
+            grid.Children.Add(treeSel);
+            grid.Children.Add(mainView);
+            var window = new Window { Content = grid };
+            window.Show();
+            base.OnStartup(e);
+        }
+    }
+
+}
---
+++ WPF/Utility.WPF.Demo.Html/Common/DemoUtils.cs
@@ -0,0 +1,132 @@
+﻿// "Therefore those skilled at the unorthodox
+// are infinite as heaven and earth,
+// inexhaustible as the great rivers.
+// When they come to an end,
+// they begin again,
+// like the days and months;
+// they die and are reborn,
+// like the four seasons."
+// 
+// - Sun Tsu,
+// "The Art of War"
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using TheArtOfDev.HtmlRenderer.Core.Entities;
+
+namespace TheArtOfDev.HtmlRenderer.Demo.Common
+{
+    public class DemoUtils
+    {
+        private const int Iterations = 20;
+
+        /// <summary>
+        /// The HTML text used in sample form for HtmlLabel.
+        /// </summary>
+        public static String SampleHtmlLabelText
+        {
+            get
+            {
+                return "This is an <b>HtmlLabel</b> on transparent background with <span style=\"color: red\">colors</span> and links: " +
+                       "<a href=\"http://htmlrenderer.codeplex.com/\">HTML Renderer</a>";
+            }
+        }
+
+        /// <summary>
+        /// The HTML text used in sample form for HtmlPanel.
+        /// </summary>
+        public static String SampleHtmlPanelText
+        {
+            get
+            {
+                return "This is an <b>HtmlPanel</b> with <span style=\"color: red\">colors</span> and links: <a href=\"http://htmlrenderer.codeplex.com/\">HTML Renderer</a>" +
+                       "<div style=\"font-size: 1.2em; padding-top: 10px;\" >If there is more text than the size of the control scrollbars will appear.</div>" +
+                       "<br/>Click me to change my <code>Text</code> property.";
+            }
+        }
+
+
+
+
+        public static string RunSamplesPerformanceTest(Action<String> setHtmlDelegate)
+        {
+            GC.Collect();
+
+            double baseMemory;
+            var baseStopwatch = RunTest(setHtmlDelegate, false, out baseMemory);
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            double runMemory;
+            var runStopwatch = RunTest(setHtmlDelegate, true, out runMemory);
+
+            double memory = runMemory - baseMemory;
+            var elapsedMilliseconds = runStopwatch.ElapsedMilliseconds - baseStopwatch.ElapsedMilliseconds;
+
+            float htmlSize = 0;
+            foreach (var sample in SamplesLoader.ShowcaseSamples)
+                htmlSize += sample.Html.Length * 2;
+            htmlSize = htmlSize / 1024f;
+
+            var sampleCount = SamplesLoader.ShowcaseSamples.Count;
+            var msg = string.Format("{0} HTMLs ({1:N0} KB)\r\n{2} Iterations", sampleCount, htmlSize, Iterations);
+            msg += "\r\n\r\n";
+            msg += string.Format("CPU:\r\nTotal: {0} msec\r\nIterationAvg: {1:N2} msec\r\nSingleAvg: {2:N2} msec",
+                elapsedMilliseconds, elapsedMilliseconds / (double)Iterations, elapsedMilliseconds / (double)Iterations / sampleCount);
+
+            if (Environment.Version.Major >= 4)
+            {
+                msg += "\r\n\r\n";
+                msg += string.Format("Memory:\r\nTotal: {0:N0} KB\r\nIterationAvg: {1:N0} KB\r\nSingleAvg: {2:N0} KB\r\nOverhead: {3:N0}%",
+                    memory, memory / Iterations, memory / Iterations / sampleCount, 100 * (memory / Iterations) / htmlSize);
+            }
+
+            msg += "\r\n\r\n\r\n";
+            msg += string.Format("Full CPU:\r\nTotal: {0} msec\r\nIterationAvg: {1:N2} msec\r\nSingleAvg: {2:N2} msec",
+                runStopwatch.ElapsedMilliseconds, runStopwatch.ElapsedMilliseconds / (double)Iterations, runStopwatch.ElapsedMilliseconds / (double)Iterations / sampleCount);
+
+            if (Environment.Version.Major >= 4)
+            {
+                msg += "\r\n\r\n";
+                msg += string.Format("Full Memory:\r\nTotal: {0:N0} KB\r\nIterationAvg: {1:N0} KB\r\nSingleAvg: {2:N0} KB\r\nOverhead: {3:N0}%",
+                    runMemory, runMemory / Iterations, runMemory / Iterations / sampleCount, 100 * (runMemory / Iterations) / htmlSize);
+            }
+
+            return msg;
+        }
+
+        private static Stopwatch RunTest(Action<String> setHtmlDelegate, bool real, out double totalMem)
+        {
+            totalMem = 0;
+            long startMemory = 0;
+            if (Environment.Version.Major >= 4)
+            {
+                typeof(AppDomain).GetProperty("MonitoringIsEnabled").SetValue(null, true, null);
+                startMemory = (long)AppDomain.CurrentDomain.GetType().GetProperty("MonitoringTotalAllocatedMemorySize").GetValue(AppDomain.CurrentDomain, null);
+            }
+
+            var sw = Stopwatch.StartNew();
+
+            for (int i = 0; i < Iterations; i++)
+            {
+                foreach (var sample in SamplesLoader.ShowcaseSamples)
+                {
+                    setHtmlDelegate(real ? sample.Html : string.Empty);
+                }
+            }
+
+            sw.Stop();
+
+            if (Environment.Version.Major >= 4)
+            {
+                var endMemory = (long)AppDomain.CurrentDomain.GetType().GetProperty("MonitoringTotalAllocatedMemorySize").GetValue(AppDomain.CurrentDomain, null);
+                totalMem = (endMemory - startMemory) / 1024f;
+            }
+
+            return sw;
+        }
+    }
+}
