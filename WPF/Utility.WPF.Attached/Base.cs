6b5561d9f52a6be227abaeec1748ddf504c5ebb4
dtaylor-530 <d.taylor530@gmail.com>
2025-04-01 13:08:16 +0100

    Utility.WPF.Attached Adds Base class for general attached properties

---
+++ WPF/Utility.WPF.Attached/Base.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Utility.WPF.Attached
+{
+    public class Base
+    {
+        public static readonly DependencyProperty IntegerProperty = DependencyProperty.RegisterAttached(
+        "Integer",
+        typeof(int),
+        typeof(Base),
+        new FrameworkPropertyMetadata(int.MinValue, changed));
+
+        private static void changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            
+        }
+
+        public static void SetInteger(DependencyObject dep, int value)
+        {
+            dep.SetValue(IntegerProperty, value);
+        }
+
+        public static int GetInteger(DependencyObject dep)
+        {
+            return (int)dep.GetValue(IntegerProperty);
+        }       
+        
+        
+        
+        public static readonly DependencyProperty StringProperty = DependencyProperty.RegisterAttached(
+        "String",
+        typeof(string),
+        typeof(Base),
+        new FrameworkPropertyMetadata(null));
+
+        public static void SetString(DependencyObject dep, string value)
+        {
+            dep.SetValue(StringProperty, value);
+        }
+
+        public static string GetString(DependencyObject dep)
+        {
+            return (string)dep.GetValue(StringProperty);
+        }        
+        
+
+
+
+        public static readonly DependencyProperty DoubleProperty = DependencyProperty.RegisterAttached(
+        "Double",
+        typeof(double),
+        typeof(Base),
+        new FrameworkPropertyMetadata(double.MinValue));
+
+        public static void SetDouble(DependencyObject dep, double value)
+        {
+            dep.SetValue(DoubleProperty, value);
+        }
+
+        public static double GetDouble(DependencyObject dep) => (double)dep.GetValue(DoubleProperty);
+
+    }
+}
