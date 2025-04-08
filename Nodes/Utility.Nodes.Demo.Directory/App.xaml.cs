03672f8e8dfaff436aedc2bdddfa29569f9ef5e0
dtaylor-530 <d.taylor530@gmail.com>
2025-04-08 11:57:01 +0100

    Utility.Nodes.Demo.Directory Adds

---
+++ Nodes/Utility.Nodes.Demo.Directory/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Splat;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using Utility.Conversions.Json.Newtonsoft;
+using Utility.Interfaces.Exs;
+using Utility.Interfaces.NonGeneric;
+using Utility.Models;
+using Utility.Nodes.Filters;
+using Utility.Repos;
+
+namespace Utility.Nodes.Demo.Directory
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SQLitePCL.Batteries.Init();
+
+            Locator.CurrentMutable.RegisterConstant<ITreeRepository>(TreeRepository.Instance);
+            //Locator.CurrentMutable.RegisterConstant<INodeSource>(NodeSource.Instance);
+            Locator.CurrentMutable.RegisterConstant<INodeSource>(NodeEngine.Instance);
+            Locator.CurrentMutable.RegisterConstant<MethodCache>(new MethodCache());
+            //Locator.CurrentMutable.RegisterConstant<IFilter>(TreeViewFilter.Instance);
+            //Locator.CurrentMutable.RegisterConstant<IExpander>(Expander.Instance);
+            Locator.CurrentMutable.RegisterConstant<IContext>(Context.Instance);
+            Splat.Locator.CurrentMutable.RegisterLazySingleton<MainViewModel>(() => new MainViewModel());
+            //Splat.Locator.CurrentMutable.RegisterLazySingleton<System.Windows.Controls.DataTemplateSelector>(() => CustomDataTemplateSelector.Instance);
+
+
+            //var x = DataTemplateSelector.Instance;
+
+            JsonConvert.DefaultSettings = () => SettingsFactory.Combined;
+
+            //TransformerService service = new();
+            //ControlsService _service = ControlsService.Instance;
+            //ParserService parserService = new();
+
+
+            var mainViewModel = Locator.Current.GetService<MainViewModel>();
+
+            //var window = new Window() { Content = mainViewModel };
+
+            //window.Show();
+
+
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Nodes/Utility.Nodes.Demo.Directory/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Splat;
+using Utility.Interfaces.Exs;
+using Utility.Interfaces.NonGeneric;
+using Utility.Nodes.Filters;
+using Utility.PropertyNotifications;
+using Utility.Trees;
+using Utility.Trees.Abstractions;
+
+namespace Utility.Nodes.Demo.Directory
+{
+    public class MainViewModel : NotifyPropertyClass
+    {
+        Lazy<INodeSource> source = new(() => Locator.Current.GetService<INodeSource>());
+
+        private ITree selection;
+
+        public ITree Selection
+        {
+            get
+            {
+                if (selection == null)
+                    source.Value.Single(nameof(Factory.BuildComboRoot))
+                        .Subscribe(a => { selection = a;
+                            RaisePropertyChanged(nameof(Selection)); });
+                return selection ?? new Tree();
+            }
+        }
+
+    }
+}
