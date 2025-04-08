d35bedaba0185fb84f138164c47c7c8630e6688c
dtaylor-530 <d.taylor530@gmail.com>
2025-04-08 11:18:11 +0100

    Utility.Interfaces Adds generic IGet and ISet Cleans INodeSource Adds IIsSelected to INode

--- Interfaces/Utility.Interfaces.Exs/INodeSource.cs
+++ Interfaces/Utility.Interfaces.Exs/INodeSource.cs
@@ -3,15 +3,12 @@ using Utility.Structs.Repos;
 
 namespace Utility.Interfaces.Exs
 {
-    public interface INodeSource
+    public interface INodeSource: IDisposable
     {
         string New { get; }
-        DateTime Remove(Guid guid);
-        int? MaxIndex(Guid guid, string v);
+        
         void Remove(INode node);
-        IObservable<Key?> Find(Guid parentGuid, string name, Guid? guid = null, Type? type = null, int? localIndex = null);
-        IObservable<DateValue> Get(Guid guid, string name);
-        void Set(Guid guid, string name, object value, DateTime dateTime);
+
         void Add(INode node);
         IObservable<INode?> Single(string v);
 
---
+++ Interfaces/Utility.Interfaces/Generic/ISet.cs
@@ -0,0 +1,7 @@
+﻿namespace Utility.Interfaces.Generic
+{
+    public interface ISet<T>
+    {
+        public void Set(T value);
+    }
+}
