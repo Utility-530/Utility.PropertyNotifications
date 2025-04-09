84d3be9c619d0d04fabe0940eea7b906ab4444c3
dtaylor-530 <d.taylor530@gmail.com>
2025-04-09 14:28:53 +0100

    Interfaces Adds IIsRemovable Adds IIsReplicable Adds IIsRemovable and IIsReplicable to INode

---
+++ Utility.Interfaces/NonGeneric/IIsReplicable.cs
@@ -0,0 +1,17 @@
+﻿namespace Utility.Interfaces.NonGeneric
+{
+    public interface IIsReplicable
+    {
+        bool IsReplicable { get; set; }
+    }   
+    
+    public interface IGetIsReplicable
+    {
+        bool IsReplicable { get; }
+    } 
+    
+    public interface ISetIsReplicable
+    {
+        bool IsSetReplicable { get; }
+    }
+}
