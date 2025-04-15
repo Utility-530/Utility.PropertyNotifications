a247bd6ff69381723734dc58b650406aac1f58cc
dtaylor-530 <d.taylor530@gmail.com>
2025-04-15 10:34:22 +0100

    Utility.WPF.Converters Adds OrientationConverter

---
+++ WPF/Utility.WPF.Converters/OrientationConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using Utility.Enums;
+using O = System.Windows.Controls.Orientation;
+
+namespace Utility.WPF.Converters
+{
+    public class OrientationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Orientation o)
+            {
+                switch (o)
+                {
+                    case Orientation.Horizontal:
+                        return O.Horizontal;
+                    case Orientation.Vertical:
+                        return O.Vertical;
+                }
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
