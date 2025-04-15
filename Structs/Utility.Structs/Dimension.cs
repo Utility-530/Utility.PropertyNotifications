6931037a5bd3867deed44a861e6a95978d307b17
dtaylor-530 <d.taylor530@gmail.com>
2025-04-15 10:31:59 +0100

    Utility.Structs Adds Dimension

---
+++ Structs/Utility.Structs/Dimension.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+
+namespace Utility.Structs
+{
+    /// <summary>
+    /// Defines the valid units for a <see cref="Dimension"/>.
+    /// </summary>
+#if !BUILDTASK
+    public
+#endif
+    enum DimensionUnitType
+    {
+        /// <summary>
+        /// The row or column is auto-sized to fit its content.
+        /// </summary>
+        Auto = 0,
+
+        /// <summary>
+        /// The row or column is sized in device independent pixels.
+        /// </summary>
+        Pixel = 1,
+
+        /// <summary>
+        /// The row or column is sized as a weighted proportion of available space.
+        /// </summary>
+        Star = 2,
+    }
+
+    /// <summary>
+    /// Holds the width or height of a <see cref="Grid"/>'s column and row definitions.
+    /// </summary>
+#if !BUILDTASK
+    public
+#endif
+    struct Dimension : IEquatable<Dimension>
+    {
+        private readonly DimensionUnitType _type;
+
+        private readonly double _value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Dimension"/> struct.
+        /// </summary>
+        /// <param name="value">The size of the Dimension in device independent pixels.</param>
+        public Dimension(double value)
+            : this(value, DimensionUnitType.Pixel)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Dimension"/> struct.
+        /// </summary>
+        /// <param name="value">The size of the Dimension.</param>
+        /// <param name="type">The unit of the Dimension.</param>
+        public Dimension(double value, DimensionUnitType type)
+        {
+            if (value < 0 || double.IsNaN(value) || double.IsInfinity(value))
+            {
+                throw new ArgumentException("Invalid value", nameof(value));
+            }
+
+            if (type < DimensionUnitType.Auto || type > DimensionUnitType.Star)
+            {
+                throw new ArgumentException("Invalid value", nameof(type));
+            }
+
+            _type = type;
+            _value = value;
+        }
+
+        /// <summary>
+        /// Gets an instance of <see cref="Dimension"/> that indicates that a row or column should
+        /// auto-size to fit its content.
+        /// </summary>
+        public static Dimension Auto => new Dimension(0, DimensionUnitType.Auto);
+
+        /// <summary>
+        /// Gets an instance of <see cref="Dimension"/> that indicates that a row or column should
+        /// fill its content.
+        /// </summary>
+        public static Dimension Star => new Dimension(1, DimensionUnitType.Star);
+
+        /// <summary>
+        /// Gets the unit of the <see cref="Dimension"/>.
+        /// </summary>
+        public DimensionUnitType GridUnitType => _type;
+
+        /// <summary>
+        /// Gets a value that indicates whether the <see cref="Dimension"/> has a <see cref="GridUnitType"/> of Pixel.
+        /// </summary>
+        public bool IsAbsolute => _type == DimensionUnitType.Pixel;
+
+        /// <summary>
+        /// Gets a value that indicates whether the <see cref="Dimension"/> has a <see cref="GridUnitType"/> of Auto.
+        /// </summary>
+        public bool IsAuto => _type == DimensionUnitType.Auto;
+
+        /// <summary>
+        /// Gets a value that indicates whether the <see cref="Dimension"/> has a <see cref="GridUnitType"/> of Star.
+        /// </summary>
+        public bool IsStar => _type == DimensionUnitType.Star;
+
+        /// <summary>
+        /// Gets the length.
+        /// </summary>
+        public double Value => _value;
+
+        /// <summary>
+        /// Compares two Dimension structures for equality.
+        /// </summary>
+        /// <param name="a">The first Dimension.</param>
+        /// <param name="b">The second Dimension.</param>
+        /// <returns>True if the structures are equal, otherwise false.</returns>
+        public static bool operator ==(Dimension a, Dimension b)
+        {
+            return a.IsAuto && b.IsAuto || a._value == b._value && a._type == b._type;
+        }
+
+        /// <summary>
+        /// Compares two Dimension structures for inequality.
+        /// </summary>
+        /// <param name="gl1">The first Dimension.</param>
+        /// <param name="gl2">The first Dimension.</param>
+        /// <returns>True if the structures are unequal, otherwise false.</returns>
+        public static bool operator !=(Dimension gl1, Dimension gl2)
+        {
+            return !(gl1 == gl2);
+        }
+
+        /// <summary>
+        /// Determines whether the <see cref="Dimension"/> is equal to the specified object.
+        /// </summary>
+        /// <param name="o">The object with which to test equality.</param>
+        /// <returns>True if the objects are equal, otherwise false.</returns>
+        public override bool Equals(object o)
+        {
+            if (o == null)
+            {
+                return false;
+            }
+
+            if (!(o is Dimension))
+            {
+                return false;
+            }
+
+            return this == (Dimension)o;
+        }
+
+        /// <summary>
+        /// Compares two Dimension structures for equality.
+        /// </summary>
+        /// <param name="Dimension">The structure with which to test equality.</param>
+        /// <returns>True if the structures are equal, otherwise false.</returns>
+        public bool Equals(Dimension Dimension)
+        {
+            return this == Dimension;
+        }
+
+        /// <summary>
+        /// Gets a hash code for the Dimension.
+        /// </summary>
+        /// <returns>The hash code.</returns>
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode() ^ _type.GetHashCode();
+        }
+
+        /// <summary>
+        /// Gets a string representation of the <see cref="Dimension"/>.
+        /// </summary>
+        /// <returns>The string representation.</returns>
+        public override string ToString()
+        {
+            if (IsAuto)
+            {
+                return "Auto";
+            }
+
+            string s = _value.ToString();
+            return IsStar ? s + "*" : s;
+        }
+
+        /// <summary>
+        /// Parses a string to return a <see cref="Dimension"/>.
+        /// </summary>
+        /// <param name="s">The string.</param>
+        /// <returns>The <see cref="Dimension"/>.</returns>
+        public static Dimension Parse(string s)
+        {
+            s = s.ToUpperInvariant();
+
+            if (s == "AUTO")
+            {
+                return Auto;
+            }
+            else if (s.EndsWith("*"))
+            {
+                var valueString = s.Substring(0, s.Length - 1).Trim();
+                var value = valueString.Length > 0 ? double.Parse(valueString, CultureInfo.InvariantCulture) : 1;
+                return new Dimension(value, DimensionUnitType.Star);
+            }
+            else
+            {
+                var value = double.Parse(s, CultureInfo.InvariantCulture);
+                return new Dimension(value, DimensionUnitType.Pixel);
+            }
+        }
+
+        /// <summary>
+        /// Parses a string to return a collection of <see cref="Dimension"/>s.
+        /// </summary>
+        /// <param name="s">The string.</param>
+        /// <returns>The <see cref="Dimension"/>.</returns>
+        public static IEnumerable<Dimension> ParseLengths(string s)
+        {
+            return s.Split(',').Select(s=> Parse(s));
+        }
+    }
+}
