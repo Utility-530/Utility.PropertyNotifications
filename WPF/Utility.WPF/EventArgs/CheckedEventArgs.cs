c9797b50a3402c2adce71707ef4f978b8c56abb8
dtaylor-530 <d.taylor530@gmail.com>
2025-04-23 14:48:03 +0100

    Utility.WPF Adds CheckedEventArgs Adds HierarchyMouseUpEventArgs

---
+++ WPF/Utility.WPF/EventArgs/CheckedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Utility.WPF
+{
+    public class CheckedEventArgs : RoutedEventArgs
+    {
+        public CheckedEventArgs(bool isChecked, RoutedEvent routedEvent, object source) : base(routedEvent, source)
+        {
+            IsChecked = isChecked;
+        }
+
+        public bool IsChecked { get; }
+    }
+
+    public delegate void CheckedRoutedEventHandler(object sender, CheckedEventArgs e);
+
+
+}
