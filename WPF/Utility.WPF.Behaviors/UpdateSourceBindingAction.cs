0a7302d9fe75bcec580624ffcabc6ccf0aa3046b
dtaylor-530 <d.taylor530@gmail.com>
2025-04-23 14:56:20 +0100

    Utility.WPF.Behaviors Cleans

--- WPF/Utility.WPF.Behaviors/UpdateSourceBindingAction.cs
+++ WPF/Utility.WPF.Behaviors/UpdateSourceBindingAction.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.Xaml.Behaviors;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Utility.WPF.Behaviors
@@ -21,7 +16,7 @@ namespace Utility.WPF.Behaviors
         public DependencyProperty Property { get; set; }
 
         protected override void Invoke(object parameter)
-        {
+        { 
             (Source ?? AssociatedObject).GetBindingExpression(Property).UpdateSource();
         }
 
