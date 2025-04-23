aaf1ade9a72ef43fbf70d85da57be34612f326e0
dtaylor-530 <d.taylor530@gmail.com>
2025-04-23 15:16:54 +0100

    Utility.Json.Newtonsoft Adds MetadataConverter for adding metadata to serialisation

---
+++ Conversions/Utility.Json.Newtonsoft/MetadataConverter.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Utility.Helpers;
+
+namespace Utility.Conversions.Json.Newtonsoft
+{
+    public class MetadataConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return Utility.Helpers.PropertyHelper.ValueTypes.Contains(objectType) == false && objectType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(objectType) == false;
+        }
+
+
+        public override bool CanRead => false;
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteStartObject();
+
+            var type = value.GetType();
+
+            //if (typeof(IEnumerable).IsAssignableFrom(type))
+            //{
+            //    if (value is IList { IsFixedSize: { } isFixedSize })
+            //    {
+
+            //    }
+            //}
+            var properties = value.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+
+            writer.WritePropertyName("$type");
+            writer.WriteValue(type.AsString());
+            List<string> names = new List<string>();
+            List<string> eNames = new List<string>();
+
+            foreach (var prop in properties)
+            {
+                var propName = prop.Name;
+                var propValue = prop.GetValue(value);
+
+                writer.WritePropertyName(propName);
+                if(prop.PropertyType.IsEnum)
+                {
+                    serializer.Serialize(writer, propValue);
+                    eNames.Add(prop.Name);
+                }
+                else if (prop.PropertyType.IsValueType || prop.PropertyType == typeof(string))
+                    writer.WriteValue(propValue);
+                else
+                    serializer.Serialize(writer, propValue);
+
+
+                // Write metadata about whether the property is readonly (no setter or private setter)
+                bool isReadOnly = !prop.CanWrite || prop.SetMethod == null || !prop.SetMethod.IsPublic;
+                if (isReadOnly)
+                {
+                    names.Add(prop.Name);
+                }
+            }
+
+            writer.WritePropertyName($"$isreadonly");
+            serializer.Serialize(writer, names);        
+            
+            writer.WritePropertyName($"$isenum");
+            serializer.Serialize(writer, eNames);
+
+            writer.WriteEndObject();
+        }
+    }
+}
