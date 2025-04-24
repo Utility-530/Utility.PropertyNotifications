3d8ffc23e2d7cc7826512f3bc88bc35a2b1c2bb8
dtaylor-530 <d.taylor530@gmail.com>
2025-04-24 15:17:52 +0100

    Utility.WPF.Panels Adds GridEx for setting row-widths and column-heights via binding for Grid

---
+++ WPF/Utility.WPF.Panels/GridEx.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using Utility.Structs;
+using System.Collections;
+
+namespace Utility.WPF.Panels
+{
+    /// <summary>
+    /// ALlows setting the row heights/ column-widths via binding 
+    /// </summary>
+    public class GridEx : Grid
+    {
+        public static readonly DependencyProperty RowHeightsProperty =
+            DependencyProperty.Register("RowHeights", typeof(IEnumerable), typeof(GridEx), new PropertyMetadata(rowsChanged));
+
+
+        public static readonly DependencyProperty ColumnWidthsProperty =
+            DependencyProperty.Register("ColumnWidths", typeof(IEnumerable), typeof(GridEx), new PropertyMetadata(columnsChanged));
+
+
+        public IEnumerable RowHeights
+        {
+            get { return (IEnumerable)GetValue(RowHeightsProperty); }
+            set { SetValue(RowHeightsProperty, value); }
+        }
+
+
+        private static void rowsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is GridEx g && e.NewValue is IEnumerable<Dimension> dimensions)
+            {
+                foreach (var dimension in dimensions)
+                {
+                    g.RowDefinitions.Add(new RowDefinition { Height = new GridLength(dimension.Value, (GridUnitType)dimension.GridUnitType) });
+                }
+            }
+        }
+
+        public IEnumerable ColumnWidths
+        {
+            get { return (IEnumerable)GetValue(ColumnWidthsProperty); }
+            set { SetValue(ColumnWidthsProperty, value); }
+        }
+
+
+        private static void columnsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is GridEx g && e.NewValue is IEnumerable<Dimension> dimensions)
+            {
+                foreach (var dimension in dimensions)
+                {
+                    g.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(dimension.Value, (GridUnitType)dimension.GridUnitType) });
+                }
+            }
+        }
+    }
+}
+
