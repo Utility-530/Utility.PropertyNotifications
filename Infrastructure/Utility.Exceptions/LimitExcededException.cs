1cdf24145e1cb09a47b0a86c5866630f3959784f
dtaylor-530 <d.taylor530@gmail.com>
2025-05-01 07:17:19 +0100

    Utility.Exceptions Adds project

---
+++ Infrastructure/Utility.Exceptions/LimitExcededException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Utility.Exceptions
+{
+    public class LimitExceededException : Exception
+    {
+        public LimitExceededException(int limit, int excess):base($"Number of items exceeds limit ({limit}) by {excess}")
+        {
+            Limit = limit;
+            Excess = excess;
+            Data.Add("Limit", limit);
+            Data.Add("Excess", excess);
+        }
+
+        public int Limit { get; set; }
+
+        public int Excess { get; set; }
+    }
+}
