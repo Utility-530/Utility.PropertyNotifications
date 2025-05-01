7dc529ffef929ce776ae6c665d786ff3aa678504
dtaylor-530 <d.taylor530@gmail.com>
2025-05-01 08:10:51 +0100

    Utility.Interfaces.Ex Adds IAutoList Adds IFactory Adds more to INode and INodeSource

--- Interfaces/Utility.Interfaces.Exs/INode.cs
+++ Interfaces/Utility.Interfaces.Exs/INode.cs
@@ -3,13 +3,32 @@ using Utility.Interfaces.NonGeneric;
 using Utility.Enums;
 using System.Collections;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
+using Utility.Structs;
+using System;
 
 namespace Utility.Interfaces.Exs
 {
-    public interface INode : ITree, IIsExpanded, ICurrent<INode>, IAdd, IKey, ILocalIndex, IIsPersistable, IIsEditable, IOrientation, IRemoved, IIsVisible, IAddCommand, IRemoveCommand, IIsSelected, ISetIsSelected, 
+    public interface INode : ITree, IIsExpanded, ICurrent<INode>, IAdd, IKey, ILocalIndex, IIsPersistable, IIsEditable, IOrientation, IRemoved, IIsVisible, IAddCommand, IRemoveCommand, IIsSelected, ISetIsSelected,
         IIsRemovable, IIsReplicable, ISort, ISortOrder,
-        IArrangement
+        IArrangement, IRows, IColumns, IIsAugmentable, IErrors
     {
+
+    }
+
+    public interface IErrors
+    {
+        ObservableCollection<Exception> Errors { get; }
+    }
+
+    public interface IColumns
+    {
+        ObservableCollection<Dimension> Columns { get; }
+    }
+     
+    public interface IRows
+    {
+        ObservableCollection<Dimension> Rows { get; }
     }
 
     public interface IRemoveCommand
@@ -50,6 +69,12 @@ namespace Utility.Interfaces.Exs
     public interface ISetNode
     {
         void SetNode(INode node);
+
+    }
+
+    public interface IIsAugmentable
+    {
+        bool IsAugmentable { get; set; }
 
     }
 }
--- Interfaces/Utility.Interfaces.Exs/INodeSource.cs
+++ Interfaces/Utility.Interfaces.Exs/INodeSource.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using Utility.Structs.Repos;
+using Utility.Trees.Abstractions;
 
 namespace Utility.Interfaces.Exs
 {
     public interface INodeSource: IDisposable
     {
         string New { get; }
-        
+        IReadOnlyCollection<INode> Nodes { get; }
+
         void Remove(INode node);
 
         void Add(INode node);
@@ -14,5 +17,7 @@ namespace Utility.Interfaces.Exs
 
         void Save();
         IObservable<INode> Create(string name, Guid guid, Func<string, INode> nodeFactory, Func<string, object> modelFactory);
+        IObservable<INode> FindChild(INode node, Guid guid);
+        IObservable<INode> Selections { get; }
     }
 }
