515c61e3af6df0e1372f4ff371c6cd73f24b571c
dtaylor-530 <d.taylor530@gmail.com>
2025-05-01 08:10:51 +0100

    Utility.Interfaces.Ex Adds IAutoList Adds IFactory Adds more to INode and INodeSource

---
+++ Utility.Interfaces.Exs/IAutoList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Interfaces.Exs
+{
+    public interface IAutoList
+    {
+        public IEnumerable AutoList { get; set; }
+    }
+}
