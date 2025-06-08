ee3ede96e727b0211404c67afeaefb7005481ae5
dtaylor-530 <d.taylor530@gmail.com>
2025-06-08 12:57:28 +0100

    Utility.WPF.Demo.Lists Adds fixes for CheckBoxesUserControl Adds toggle for switching between IsGrid states to CustomSelectorUserControl

---
+++ WPF/Utility.WPF.Demo.Lists/Infrastructure/CommandConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using Utility.WPF.Controls.Lists;
+using Utility.WPF.Models;
+
+namespace Utility.WPF.Demo.Lists.Infrastructure
+{
+    internal class CommandConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is CheckedRoutedEventArgs args)
+            {
+                if(args.OriginalSource is CheckBoxesListControl c)
+                {
+                    foreach(var item in c.ItemsSource)
+                    {
+
+                    }
+                }
+            }
+            return new Dictionary<object, bool?>();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WPF/Utility.WPF.Demo.Lists/Infrastructure/MethodsViewModel.cs
+++ WPF/Utility.WPF.Demo.Lists/Infrastructure/MethodsViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using ReactiveUI;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
-using Utility.Helpers.Ex;
-using Utility.WPF.Demo.Buttons.Infrastructure;
 using Utility.WPF.Demo.Common.ViewModels;
+using Utility.Helpers.Reflection;
+using _Model = Utility.WPF.Demo.Buttons.Infrastructure.Model;
 
 namespace Utility.WPF.Demo.Lists.Infrastructure
 {
@@ -11,11 +12,13 @@ namespace Utility.WPF.Demo.Lists.Infrastructure
     {
         public MethodsViewModel()
         {
+            var instance = new _Model();
             Data = new ObservableCollection<ButtonViewModel>(
-                Utility.Helpers.ReflectionHelper.GetMethods(new Utility.WPF.Demo.Buttons.Infrastructure.Model())
+            
+                TypeHelper.GetInstanceMethods(instance.GetType())
+                .Select(m => (m.GetDescription(), new Func<object?>(() => m.Invoke(instance, Array.Empty<object>()))))
                 .Select(a => new ButtonViewModel(a.Item1, ReactiveCommand.Create(() => { _ = a.Item2(); }))));
         }
-
         public ObservableCollection<ButtonViewModel> Data { get; }
     }
 }
