046520541cd186ae2cf5a400a84290e10f2bcdb0
dtaylor-530 <d.taylor530@gmail.com>
2025-06-13 17:45:00 +0100

    Adds Html Behaviors

---
+++ WPF/Utility.WPF.Behaviors/WebBrowserNavigationBehavior.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Utility.WPF.Behaviors
+{
+    public class WebBrowserNavigationBehavior : Behavior<WebBrowser>
+    {
+        public WebBrowserNavigationBehavior()
+        {
+
+        }
+
+        protected override void OnAttached()
+        {
+            if (Html is { } html)
+            {
+                AssociatedObject.NavigateToString(html);
+            }
+            base.OnAttached();
+        }
+
+        public string Html
+        {
+            get { return (string)GetValue(HtmlProperty); }
+            set { SetValue(HtmlProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Html.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HtmlProperty =
+            DependencyProperty.Register("Html", typeof(string), typeof(WebBrowserNavigationBehavior), new PropertyMetadata(changed));
+
+        private static void changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is WebBrowserNavigationBehavior { AssociatedObject: { } obj } && e.NewValue is string newValue)
+            {
+                obj.NavigateToString(newValue);
+            }
+        }
+    }
+}
---
+++ WPF/Utility.WPF.Controls.Html/Infrastructure/TextEditorTextBehavior.cs
@@ -0,0 +1,44 @@
+﻿using AvalonEditB;
+using ColorCode.Compilation.Languages;
+using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Utility.WPF.Controls.Html
+{
+    public class TextEditorTextBehavior : Behavior<TextEditor>
+    {
+
+
+        public string Html
+        {
+            get { return (string)GetValue(HtmlProperty); }
+            set { SetValue(HtmlProperty, value); }
+        }
+
+        public static readonly DependencyProperty HtmlProperty =
+            DependencyProperty.Register("Html", typeof(string), typeof(TextEditorTextBehavior), new PropertyMetadata(changed));
+
+        protected override void OnAttached()
+        {
+            if(Html is { } html)
+            {
+                AssociatedObject.Text = html;   
+            }
+            base.OnAttached();
+        }
+
+        private static void changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextEditorTextBehavior { AssociatedObject: { } associatedObject } && e.NewValue is string html)
+            {
+                associatedObject.Text = html;
+            }
+        }
+    }
+}
