43fa9fa6415c0d86cd194ec4300e38f512454a1f
dtaylor-530 <d.taylor530@gmail.com>
2025-06-13 17:15:44 +0100

    Utility.Services Adds Razor service

---
+++ Infrastructure/Utility.Services/Razor.cs
@@ -0,0 +1,82 @@
+﻿using RazorEngine.Templating;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reactive.Threading.Tasks;
+using Utility.Entities;
+
+namespace Utility.Services
+{ 
+    public class RazorService : IObserver<FilePath>, IObserver<Instance>, IObservable<RazorEngineOutput>
+    {
+        //public record ArticleDTO(string Title, string Description);
+        public record ObjectDTO(object Value, string TemplateName, string Directory);
+        public record RazorFileDTO(string Content, object Value, string Text);
+
+        private readonly Subject<FilePath> filePaths = new();
+        private readonly Subject<Instance> objects = new();
+        private readonly Subject<RazorEngineOutput> razorFiles = new();
+        //private readonly EngineFactory factory = new();
+
+        public RazorService()
+        {
+            CancellationTokenSource? source = null;
+
+            string? dir = null;
+
+            int i = 0;
+            var obs = filePaths.CombineLatest(objects)
+                .Subscribe(async a =>
+                {                    
+                    Utility.Globals.Logs.OnNext(new Log { Date = DateTime.Now, Message = "Compiling model", Source = nameof(RazorService) });
+                    source?.Cancel(false);
+                    source = new();
+                    var (filePath, instance) = a;
+                    var map = await Task.Run<RazorFileDTO>(() =>
+                    {
+                        try
+                        {
+                            var service = RazorEngineService.Create();
+                            //var x = Path.Combine(Directory, TemplateName);
+                            var text = File.ReadAllText(filePath.Full);
+                            string textIndex = service.RunCompile(text, filePath.Full, null, instance.Value);
+                            Globals.Logs.OnNext(new Log { Date = DateTime.Now, Message = "Compiled model", Source = nameof(RazorService) });
+                            return new RazorFileDTO(textIndex, instance.Value, text);
+                        }
+                        catch (Exception ex)
+                        {
+                            Globals.Logs.OnNext(new Log { Date = DateTime.Now, Message = $"Failed to Compiled model : {ex.Message}", Source = nameof(RazorService) });
+                        }
+                        return null;
+                    }, source.Token);
+
+                    razorFiles.OnNext(new(map.Content, map.Text, map.Value));
+                });
+
+   
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(FilePath value)
+        {
+            filePaths.OnNext(value);
+        }
+        public void OnNext(Instance value)
+        {
+            objects.OnNext(value);
+        }
+
+        public IDisposable Subscribe(IObserver<RazorEngineOutput> observer)
+        {
+            return razorFiles.Subscribe(observer);
+        }
+    }
+}
