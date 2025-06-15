3a6dcb09afebd762e0ec2eb7e3d58b69dcf555b5
dtaylor-530 <d.taylor530@gmail.com>
2025-06-15 16:17:29 +0100

    Utility.WPF.Behaviors Adds GuidContextBehavior for adding a contextmenu to Guid to textbox via ContextMenu

---
+++ WPF/Utility.WPF.Behaviors/GuidContextBehavior.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Utility.Commands;
+
+namespace Utility.WPF.Behaviors
+{
+    public class GuidContextBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            var menuItem = new MenuItem
+            {
+                Header = "Generate",
+                Command = new Command(() =>
+                {
+                    AssociatedObject.Text = Guid.NewGuid().ToString();
+
+                })
+            };
+            var menuItem2 = new MenuItem
+            {
+                Header = "Copy",
+                Command = new Command(() =>
+                {
+                    Clipboard.SetText(AssociatedObject.Text);
+                })
+            };
+
+            AssociatedObject.ContextMenu = new ContextMenu { };  
+            AssociatedObject.ContextMenu.Items.Add(menuItem);
+            AssociatedObject.ContextMenu.Items.Add(menuItem2);
+            AssociatedObject.ContextMenu.Items.Add(new Separator());
+            AssociatedObject.ContextMenu.Items.Add(new MenuItem { Header = "Cut", Command = ApplicationCommands.Cut });
+            AssociatedObject.ContextMenu.Items.Add(new MenuItem { Header = "Copy", Command = ApplicationCommands.Copy });
+            AssociatedObject.ContextMenu.Items.Add(new MenuItem { Header = "Paste", Command = ApplicationCommands.Paste });
+
+            base.OnAttached();
+        }
+    }
+}
+
--- WPF/Utility.WPF.Behaviors/GuidEditBehavior.cs
+++ WPF/Utility.WPF.Behaviors/GuidEditBehavior.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Utility.WPF.Behaviors
 {
