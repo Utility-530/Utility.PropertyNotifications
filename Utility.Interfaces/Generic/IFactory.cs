e43e7c896a37a497e6c4db91e98fa7a8472f1f31
dtaylor-530 <d.taylor530@gmail.com>
2025-06-17 14:21:42 +0100

    Utility.Interfaces Adds IFactory

---
+++ Utility.Interfaces/Generic/IFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Interfaces.Generic
+{
+    public interface IFactory<T>
+    {
+        Task<T> Create(object config);
+    }
+}
