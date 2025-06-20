dd83096340f3550765b3b2c3bbff21d732fe85e7
dtaylor-530 <d.taylor530@gmail.com>
2025-06-20 17:27:31 +0100

    Utility.Attributes Adds guid and transformmethod parameters to ModelAttribute

--- Infrastructure/Utility.Attributes/ModelAttribute.cs
+++ Infrastructure/Utility.Attributes/ModelAttribute.cs
@@ -1,7 +1,9 @@
 ﻿namespace Utility.Attributes
 {
     [AttributeUsage(AttributeTargets.Class)]
-    public class ModelAttribute : Attribute
+    public class ModelAttribute(string guid, string? transformMethod = default) : Attribute
     {
+        public Guid Guid { get; } = Guid.Parse(guid);
+        public string? TransformMethod { get; } = transformMethod;
     }
 }
