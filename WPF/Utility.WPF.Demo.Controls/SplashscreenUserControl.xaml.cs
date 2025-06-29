f093ec060c09b80532a9ccd5e9d7dc5278606023
dtaylor-530 <d.taylor530@gmail.com>
2025-06-29 22:57:27 +0100

    Utility.WPF.Demo.Controls Adds SplashscreenUserControl

---
+++ WPF/Utility.WPF.Demo.Controls/SplashscreenUserControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Utility.WPF.Demo.Controls
+{
+    /// <summary>
+    /// Interaction logic for SplashscreenUSerControl.xaml
+    /// </summary>
+    public partial class SplashscreenUserControl : UserControl
+    {
+        public SplashscreenUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public void Finished()
+        {
+            MessageBox.Show("Finished");
+        }
+    }
+}
