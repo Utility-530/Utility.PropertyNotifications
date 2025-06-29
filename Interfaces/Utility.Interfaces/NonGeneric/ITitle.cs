ce07c4f8ad13a63603667d72e6ed6cfb736b63b1
dtaylor-530 <d.taylor530@gmail.com>
2025-06-29 22:38:49 +0100

    Utility.Interfaces Adds IFactory Adds ITitle

---
+++ Interfaces/Utility.Interfaces/NonGeneric/ITitle.cs
@@ -0,0 +1,18 @@
+﻿namespace Utility.Enums
+{
+    public interface ITitle
+    {
+        string Title { get; set; }
+    }
+
+
+    public interface IGetTitle
+    {
+        string Title { get; }
+    }
+
+    public interface ISetTitle
+    {
+        string Title { set; }
+    }
+}
