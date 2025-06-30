7c851c5dc324fea70c3c16885a95ba37f74ae6f6
dtaylor-530 <d.taylor530@gmail.com>
2025-06-30 22:03:48 +0100

    Utility.Interfaces Adds IIndex Removes IObservableService

---
+++ Utility.Interfaces/Generic/IIndex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Interfaces.Generic
+{
+    public interface IObservableIndex<T>
+    {
+        System.IObservable<T> this[string key] { get; }
+    }
+    public interface IIndex<T>
+    {
+        T this[string key] { get; }
+    }
+
+    public interface ITaskIndex<T>
+    {
+        Task<T> this[string key] { get; }
+    }
+}
