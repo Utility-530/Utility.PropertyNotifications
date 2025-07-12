881e59e235a33bc458e391db00920bd0bf2b2c6d
dtaylor-530 <d.taylor530@gmail.com>
2025-07-12 17:12:22 +0100

    Interfaces Utility.Interfaces Adds IFileSystemInfo Utility.Interfaces.Ex Adds IValueModel

---
+++ Utility.Interfaces.Exs/IValueModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using Utility.Interfaces;
+using Utility.Interfaces.NonGeneric;
+
+namespace Utility.Models
+{
+    public interface IValueModel : IValue, ISetValue, INotifyPropertyCalled, INotifyPropertyReceived, INotifyPropertyChanged
+    {
+
+    }
+}
