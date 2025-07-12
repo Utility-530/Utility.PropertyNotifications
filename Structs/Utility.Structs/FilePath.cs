28f690e3ab0946168057a994c8b95675ed7501a4
dtaylor-530 <d.taylor530@gmail.com>
2025-07-12 17:08:27 +0100

    Utility.Structs Adds FilePath

---
+++ Structs/Utility.Structs/FilePath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Utility.Structs
+{
+    public readonly record struct FilePath(string Directory, string FileName)
+    {
+        public string Full => Path.Combine(Directory, FileName);
+        public static FilePath FromFilePath(string filePath) => new(Path.GetDirectoryName(filePath), Path.GetFileName(filePath));
+    }
+}
