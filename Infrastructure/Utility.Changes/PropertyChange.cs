3d52705c86f0b4aa4bf39b3f2f19d38704d60ce6
dtaylor-530 <d.taylor530@gmail.com>
2025-07-12 17:10:24 +0100

    Utility.Changes Removes PropertyChange

--- Infrastructure/Utility.Changes/PropertyChange.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Utility.Changes
-{
-    //public record PropertyChange(object Source, string PropertyName, object NewValue, object? PreviousValue = null) : Change(NewValue, PreviousValue, Type.Update);
-
-}
