66f2911adfad3f104bf8fdbf9d319951fff2daf2
dtaylor-530 <d.taylor530@gmail.com>
2025-07-22 09:50:55 +0100

    Utility.WPF.ResourceDictionarys Adds Exclude property to AutoResourceDictionary

--- WPF/Utility.WPF.ResourceDictionarys/AutoResourceDictionary.cs
+++ WPF/Utility.WPF.ResourceDictionarys/AutoResourceDictionary.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Utility.Helpers;
 
 namespace Utility.WPF.ResourceDictionarys
 {
     public class AutoResourceDictionary : SharedResourceDictionary
     {
         private Type? type;
+
+        public string Exclude { get; set; }
 
         public Type Type
         {
@@ -13,11 +20,37 @@ namespace Utility.WPF.ResourceDictionarys
                 if (type == value)
                     return;
                 type = value;
-                foreach (var (_, resourceDictionary) in type.Assembly.SelectResourceDictionaries())
+                string[] exclusions = null;
+                if (Exclude != null)
+                {
+                    exclusions = [.. this.Exclude.Split(',').Select(a => a)];
+                }
+
+                foreach (var (_, resourceDictionary) in type.Assembly.SelectResourceDictionaries(a => !predicate(a, exclusions)))
                 {
                     AddToMergedDictionaries(resourceDictionary);
                 }
             }
         }
+
+        private static bool predicate(System.Collections.DictionaryEntry a, string[] exclusions)
+        {
+            if (exclusions is { } ex &&  a.Key is string key)
+                return StringHelper.Contains(ex, key, StringComparison.InvariantCultureIgnoreCase);
+            return false;
+        }
     }
 }
+
+//foreach (var item in keys)
+//{
+//    try
+//    {
+
+//        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri(item, UriKind.Relative) });
+//    }
+//    catch(IOException ex) when (ex.Message.Contains("Cannot locate resource"))
+//    {
+
+//    }
+//}
