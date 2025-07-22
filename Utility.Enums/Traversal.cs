8b9dfb3911edf43ae5ce8b9b1d94051002b96e29
dtaylor-530 <d.taylor530@gmail.com>
2025-07-22 09:25:37 +0100

    Utility.Collections
    Updates
    Utility.Infrastructure
    Updates
    Utility.Interfaces
    Moves IObservable and IObserver to new sub-folders in new folder, Reactive
    Utility.Nodes.Directorys
    Updates
    Utility.Observables
    Updates

---
+++ Utility.Enums/Traversal.cs
@@ -0,0 +1,75 @@
+﻿namespace Utility.Enums
+{
+    public enum Traversal : byte
+    {
+        None,
+        InToOut = 1,
+        OutToIn = 2
+    }
+
+    public enum XTraversal : byte
+    {
+        None,
+        LeftToRight = 1,
+        RightToLeft = 2
+    }  
+    
+    public enum YTraversal : byte
+    {
+        None,
+        TopToBottom = 1,
+        BottomToTop = 2
+    }   
+    
+    public enum ZTraversal : byte
+    {
+        None,
+        FrontToBack = 1,
+        BackToFront = 2
+    }
+
+
+    public enum XYTraversal : byte
+    {
+        None,
+        LeftToRight = 1,
+        RightToLeft = 2,
+
+        TopToBottom = 4,
+        BottomToTop = 8,
+
+
+    }
+
+    public enum XZTraversal : byte
+    {
+        None,
+
+        LeftToRight = 1,
+        RightToLeft = 2,
+
+        FrontToBack = 16,
+        BackToFront = 32,
+    }
+    public enum YZTraversal : byte
+    {
+        None,
+
+
+        TopToBottom = 4,
+        BottomToTop = 8,
+        FrontToBack = 16,
+        BackToFront = 32,
+    }
+    public enum XYZTraversal : byte
+    {
+        None,
+
+        LeftToRight = 1,
+        RightToLeft = 2,
+        TopToBottom = 4,
+        BottomToTop = 8,
+        FrontToBack = 16,
+        BackToFront = 32,
+    }
+}
