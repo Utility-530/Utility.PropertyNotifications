a00cbf0e02241104ac6ab98926a85db691822cbb
dtaylor-530 <d.taylor530@gmail.com>
2025-07-22 09:25:37 +0100

    Utility.Collections
    Updates
    Utility.Infrastructure
    Updates
    Utility.Interfaces
    Moves IObservable and IObserver to new sub-folders in new folder, Reactive
    Utility.Nodes.Directorys
    Updates
    Utility.Observables
    Updates

--- Utility.Interfaces/Reactive/NonGeneric/IObservable.cs
+++ Utility.Interfaces/Reactive/NonGeneric/IObservable.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Utility.Interfaces.NonGeneric
+namespace Utility.Interfaces.Reactive.NonGeneric
 {
     public interface IObservable : IEnumerable
     {
