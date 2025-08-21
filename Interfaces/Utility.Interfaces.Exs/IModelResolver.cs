111521380614536135b0045d46b74e5c1adc1835
dtaylor-530 <d.taylor530@gmail.com>
2025-08-21 14:35:28 +0100

    Utility.Interfaces.Ex Adds IModelResolver

---
+++ Interfaces/Utility.Interfaces.Exs/IModelResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utility.Interfaces.NonGeneric;
+
+namespace Utility.Interfaces.Exs
+{
+    public interface IModelResolver: IObservable<IModel>
+    {
+        void Register(string key, Func<IModel> factory);
+
+        IModel this[string key] {get;}
+    }
+}
