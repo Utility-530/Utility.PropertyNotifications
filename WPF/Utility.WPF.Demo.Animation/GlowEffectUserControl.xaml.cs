ab34050390c964baba9c4126d1ec05daba0f43ce
dtaylor-530 <d.taylor530@gmail.com>
2025-08-21 15:26:35 +0100

    Utility.WPF.Demo.Animation Adds GlowEffectUserControl

---
+++ WPF/Utility.WPF.Demo.Animation/GlowEffectUserControl.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Utility.WPF.Demo.Animations
+{
+    /// <summary>
+    /// Interaction logic for GlowEffectUserControl.xaml
+    /// </summary>
+    public partial class GlowEffectUserControl 
+    {
+        public GlowEffectUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDemo_Click(object sender, RoutedEventArgs e)
+        {
+            HighlightControl(sender as Control);
+        }
+
+        private void TxtDemo_GotFocus(object sender, RoutedEventArgs e)
+        {
+            HighlightControl(sender as Control);
+        }
+
+        private void TxtDemo_LostFocus(object sender, RoutedEventArgs e)
+        {
+            StopHighlight(sender as Control);
+        }
+
+        private void BorderDemo_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (sender is Border border)
+                border.Tag = "Pulse";
+        }
+
+        private void BorderDemo_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (sender is Border border)
+                border.Tag = null;
+        }
+
+        private void CmbDemo_DropDownOpened(object sender, EventArgs e)
+        {
+            HighlightControl(sender as Control);
+        }
+
+        private void HighlightAll_Click(object sender, RoutedEventArgs e)
+        {
+            HighlightControl(btnDemo);
+            HighlightControl(txtDemo);
+            HighlightControl(cmbDemo);
+            borderDemo.Tag = "Pulse";
+        }
+
+        private void StopHighlights_Click(object sender, RoutedEventArgs e)
+        {
+            StopHighlight(btnDemo);
+            StopHighlight(txtDemo);
+            StopHighlight(cmbDemo);
+            borderDemo.Tag = null;
+        }
+
+        private void HighlightControl(Control control)
+        {
+            if (control != null)
+                control.Tag = "Highlight";
+        }
+
+        private void StopHighlight(Control control)
+        {
+            if (control != null)
+                control.Tag = null;
+        }
+    }
+}
+
