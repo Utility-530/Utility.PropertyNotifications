2ef66d577b82e829661c4652964ed86a56ebc880
dtaylor-530 <d.taylor530@gmail.com>
2025-09-10 11:22:22 +0100

    Utility.Models Improves TreeResolver with method FindRecursive

--- Models/Utility.Models/TreeResolver.cs
+++ Models/Utility.Models/TreeResolver.cs
@@ -15,6 +15,15 @@ using ITreeRepository = Utility.Interfaces.Exs.ITreeRepository;
 
 namespace Utility.Models
 {
+    public class NamedTree : Tree, IName
+    {
+        public NamedTree()
+        {
+            Data = Name;
+        }
+        public string Name { get; set; } = "root";
+    }
+
     public class TreeResolver : IResolver<IReadOnlyTree>, IObservable<IReadOnlyTree>
     {
         private CompositeDisposable compositeDisposable = new CompositeDisposable();
@@ -23,7 +32,7 @@ namespace Utility.Models
 
         public TreeResolver()
         {
-            Initialise();
+            //Initialise();
         }
 
         public void Register(string key, Func<IReadOnlyTree> factory)
@@ -54,76 +63,92 @@ namespace Utility.Models
                 });
         }
 
-        public class NamedTree : Tree, IName
-        {
-            public NamedTree()
-            {
-                Data = Name;
-            }
-            public string Name { get; set; } = "root";
-        }
-
         public IReadOnlyTree this[string key] => _factories[key].Value;
 
-        public void Initialise()
-        {
-            var tree = new NamedTree() { Key = "004cf888-a762-4149-a3b9-7a0911cdf1a9" };
-            _factories["004cf888-a762-4149-a3b9-7a0911cdf1a9"] = new(() => tree);
-            children(tree)
-                .Subscribe(a =>
-                {
-                    _factories[(a as IGetKey).Key.ToString()] = new(() => a);
-                })
-                .DisposeWith(compositeDisposable);
-        }
+        //public void Initialise()
+        //{
+        //    var tree = new NamedTree() { Key = "004cf888-a762-4149-a3b9-7a0911cdf1a9" };
+        //    _factories["004cf888-a762-4149-a3b9-7a0911cdf1a9"] = new(() => tree);
+        //    children2(tree)
+        //        .Subscribe(a =>
+        //        {
+        //            _factories[(a as IGetKey).Key.ToString()] = new(() => a);
+        //        })
+        //        .DisposeWith(compositeDisposable);
+        //}
 
-        IObservable<IReadOnlyTree> children(ITree node)
+        //IObservable<IReadOnlyTree> children(ITree node)
+        //{
+        //    return Observable.Create<IReadOnlyTree>(observer =>
+        //    {
+        //        int i = 0, j = 0;
+        //        bool flag = false;
+        //        return repository.Find((GuidKey)(node as IGetKey).Key)
+        //            .Subscribe(async key =>
+        //            {
+
+        //                i++;
+        //                //}
+        //                if (key.HasValue)
+        //                {
+        //                    flag = true;
+        //                    FindChild(node, key.Value.Guid).Subscribe(a =>
+        //                    {
+        //                        observer.OnNext(a);
+        //                        children((ITree)a).Subscribe(observer.OnNext);
+        //                    }, () => { });
+        //                }
+        //            }
+        //            , () =>
+        //            {
+
+        //            });
+        //    });
+        //}
+
+        public IObservable<IReadOnlyTree> Children(ITree node)
         {
             return Observable.Create<IReadOnlyTree>(observer =>
             {
                 int i = 0, j = 0;
                 bool flag = false;
-                return repository.Find((GuidKey)(node as IGetKey).Key)
+                List<IReadOnlyTree> nodes = new();
+                nodes.Add(node);
+                observer.OnNext(node);
+                return repository.FindRecursive((GuidKey)(node as IGetKey).Key)
                     .Subscribe(async key =>
                     {
-
-                        i++;
-                        //}
-                        if (key.HasValue)
+                        var _node = nodes.Single(a => (a as IGetKey).Key.ToString() == key.ParentGuid.ToString());
+                        activate(_node as ITree, key).Subscribe(a =>
                         {
-                            flag = true;
-                            FindChild(node, key.Value.Guid).Subscribe(a =>
-                            {
-                                observer.OnNext(a);
-                                children((ITree)a).Subscribe(observer.OnNext);
-                            }, () => { });
-                        }
+                            observer.OnNext(a);
+                            nodes.Add(a);
+                        });
                     }
                     , () =>
                     {
-
-                    });
-            });
-        }
-
-        public IObservable<IReadOnlyTree> FindChild(ITree node, Guid guid)
-        {
-            return Observable.Create<IReadOnlyTree>(observer =>
-            {
-                return repository
-                .Find((GuidKey)(node as IGetKey).Key, guid: guid)
-                .Subscribe(_key =>
-                {
-                    activate(node, _key).Subscribe(a =>
-                    {
-                        node.Add(a);
-                        observer.OnNext(a);
                         observer.OnCompleted();
                     });
-                });
             });
         }
 
+        //public IObservable<IReadOnlyTree> FindChild(ITree node, Guid guid)
+        //{
+        //    return Observable.Create<IReadOnlyTree>(observer =>
+        //    {
+        //        return repository
+        //        .Find((GuidKey)(node as IGetKey).Key, guid: guid)
+        //        .Subscribe(_key =>
+        //        {
+        //            activate(node, _key).Subscribe(a =>
+        //            {
+        //                node.Add(a);
+        //                observer.OnNext(a);
+        //                observer.OnCompleted();
+        //            });
+        //        });
+        //    });
+        //}
 
         IObservable<IReadOnlyTree> activate(ITree node, Structs.Repos.Key? _key)
         {
@@ -134,12 +159,6 @@ namespace Utility.Models
                     .Subscribe(_new =>
                     {
                         (_new as ISetKey).Key = new GuidKey(_key.Value.Guid);
-                        //_new.Removed = _key.Value.Removed;
-
-                        //if (d is IIsReadOnly readOnly)
-                        //{
-                        //    (_new as ISetIsReadOnly).IsReadOnly = readOnly.IsReadOnly;
-                        //}
 
                         if (_key.HasValue == false)
                         {
