1bd99a5116fe74841cbdbe3afcbafe2eec184d88
dtaylor-530 <d.taylor530@gmail.com>
2025-09-10 11:23:53 +0100

    Utility.Models.Diagrams Adds ObjectConnector

---
+++ Models/Utility.Models.Diagrams/ObjectConnector.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using Utility.Interfaces.NonGeneric;
+using Utility.PropertyNotifications;
+using Utility.Reactives;
+
+namespace Utility.Models.Diagrams
+{
+    public class ObjectConnector : ReactiveProperty<object>, IKey
+    {
+        public string Key { get; set; }
+
+        public ObjectConnector(INotifyPropertyChanged instance, PropertyInfo propertyInfo) : base(default)
+        {
+            instance.WithChangesTo(propertyInfo)
+                .Subscribe(a =>
+                {
+                    IsActive = true;
+                    OnPropertyChanged(nameof(IsActive));
+                });
+        
+        }
+
+        public bool IsActive { get; set; }
+
+        public PropertyInfo Parameter { get; set; }
+    }
+
+}
