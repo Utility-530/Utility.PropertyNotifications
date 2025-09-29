57795e66fdd804b7afca61341973da06d7f0196a
dtaylor-530 <d.taylor530@gmail.com>
2025-09-29 13:30:05 +0100

    Utility.Interfaces Adds IProcess

---
+++ Interfaces/Utility.Interfaces/Generic/IProcess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Interfaces.Generic
+{
+    public interface IProcess<T>
+    {
+        void Process(T value);
+    }
+}
---
+++ Interfaces/Utility.Interfaces/NonGeneric/IProcess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Interfaces.NonGeneric
+{
+    public interface IProcess
+    {
+        void Process(object value);
+    }
+}
