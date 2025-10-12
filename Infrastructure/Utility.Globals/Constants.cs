a48f800c17f2cf4ca27f78465fd92deba2a56169
dtaylor-530 <d.taylor530@gmail.com>
2025-10-12 22:34:18 +0100

    Utility.Globals Adds Constants for data paths

---
+++ Infrastructure/Utility.Globals/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class Constants
+    {
+        public const string DefaultDataPath = "O:\\Users\\rytal\\Data";
+        public const string DefaultModelsFileName = "models.sqlite";
+        public static readonly string DefaultModelsFilePath = Path.Combine(DefaultDataPath, DefaultModelsFileName);
+    }
+}
