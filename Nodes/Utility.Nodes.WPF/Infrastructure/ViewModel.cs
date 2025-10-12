08555d26620d190d24c122c1fead5a956004396a
dtaylor-530 <d.taylor530@gmail.com>
2025-10-12 23:37:30 +0100

    Utility.Nodes.WPF Removes ViewModel

--- Nodes/Utility.Nodes.WPF/Infrastructure/ViewModel.cs
+++ Nodes/Utility.Nodes.WPF/Infrastructure/ViewModel.cs
@@ -1,78 +1,79 @@
-﻿using Splat;
-using System.Collections.ObjectModel;
-using System.Runtime.CompilerServices;
-using Utility.Interfaces.Exs;
-using Utility.PropertyNotifications;
-using Utility.Trees.Abstractions;
-using Utility.Helpers;
-using Utility.Interfaces.Generic;
-using Utility.Interfaces.Exs.Diagrams;
+﻿//using Splat;
+//using System.Collections.ObjectModel;
+//using System.Runtime.CompilerServices;
+//using Utility.Interfaces.Exs;
+//using Utility.PropertyNotifications;
+//using Utility.Trees.Abstractions;
+//using Utility.Helpers;
+//using Utility.Interfaces.Generic;
+//using Utility.Interfaces.Exs.Diagrams;
 
-namespace Utility.Nodes.WPF
-{
-    public class ViewModel : NotifyPropertyClass, IDisposable
-    {
-        private bool _disposed;
-        private bool isSelected;
-        protected Lazy<IObservableIndex<INodeViewModel>> source = new(() => Locator.Current.GetService<IObservableIndex<INodeViewModel>>());
-        private readonly Dictionary<string, ObservableCollection<IReadOnlyTree>> dictionary = [];
+//namespace Utility.Nodes.WPF
+//{
+//    public class ViewModel : NotifyPropertyClass, IDisposable
+//    {
+//        private bool _disposed;
+//        private bool isSelected;
+//        protected Lazy<IObservableIndex<INodeViewModel>> source = new(() => Locator.Current.GetService<IObservableIndex<INodeViewModel>>());
+//        private readonly Dictionary<string, ObservableCollection<IReadOnlyTree>> dictionary = [];
 
-        public virtual string Name => GetType().Name;
-        protected void Subscribe(string key, Action<IReadOnlyTree> field,[CallerMemberName] string? callerName = null) => 
+//        public virtual string Name => GetType().Name;
+//        protected void Subscribe(string key, Action<IReadOnlyTree> field,[CallerMemberName] string? callerName = null) => 
             
-            source.Value[key].Subscribe(a => { field(a); RaisePropertyChanged(callerName); }).DisposeWith(disposables);
+//            source.Value[key].Subscribe(a => { field(a); RaisePropertyChanged(callerName); }).DisposeWith(disposables);
 
-        public bool IsSelected { get => isSelected; set => RaisePropertyChanged(ref isSelected, value); }
-        protected IEnumerable<IReadOnlyTree> get(string name, [CallerMemberName] string? propertyName = default)
-        {
-            if (propertyName == null)
-                throw new Exception("sd78__22");
-            if (dictionary.TryGetValue(propertyName, out var value) == false)
-            {
-                source
-                    .Value
-                    [name]
-                    .Subscribe(a =>
-                    {
-                        get(propertyName).Add(a);
-                    }).DisposeWith(disposables);
-            }
-            return get(propertyName);
-        }
+//        public bool IsSelected { get => isSelected; set => RaisePropertyChanged(ref isSelected, value); }
 
-        #region dispose
+//        protected IEnumerable<IReadOnlyTree> get(string name, [CallerMemberName] string? propertyName = default)
+//        {
+//            if (propertyName == null)
+//                throw new Exception("sd78__22");
+//            if (dictionary.TryGetValue(propertyName, out var value) == false)
+//            {
+//                source
+//                    .Value
+//                    [name]
+//                    .Subscribe(a =>
+//                    {
+//                        get(propertyName).Add(a);
+//                    }).DisposeWith(disposables);
+//            }
+//            return get(propertyName);
+//        }
 
-        protected System.Reactive.Disposables.CompositeDisposable disposables = new();
+//        #region dispose
 
-        private ObservableCollection<IReadOnlyTree> get(string key) => dictionary.Get(key);
+//        protected System.Reactive.Disposables.CompositeDisposable disposables = new();
 
-        public void Dispose()
-        {
-            // Dispose of unmanaged resources.
-            Dispose(true);
-            // Suppress finalization.
-            GC.SuppressFinalize(this);
-        }
+//        private ObservableCollection<IReadOnlyTree> get(string key) => dictionary.Get(key);
 
-        //The Dispose method performs all object cleanup, so the garbage collector no longer needs to call the objects' Object.Finalize override. Therefore, the call to the SuppressFinalize method prevents the garbage collector from running the finalizer. If the type has no finalizer, the call to GC.SuppressFinalize has no effect. The actual cleanup is performed by the Dispose(bool) method overload.
-        //In the overload, the disposing parameter is a Boolean that indicates whether the method call comes from a Dispose method(its value is true) or from a finalizer(its value is false).
+//        public void Dispose()
+//        {
+//            // Dispose of unmanaged resources.
+//            Dispose(true);
+//            // Suppress finalization.
+//            GC.SuppressFinalize(this);
+//        }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (_disposed)
-            {
-                return;
-            }
+//        //The Dispose method performs all object cleanup, so the garbage collector no longer needs to call the objects' Object.Finalize override. Therefore, the call to the SuppressFinalize method prevents the garbage collector from running the finalizer. If the type has no finalizer, the call to GC.SuppressFinalize has no effect. The actual cleanup is performed by the Dispose(bool) method overload.
+//        //In the overload, the disposing parameter is a Boolean that indicates whether the method call comes from a Dispose method(its value is true) or from a finalizer(its value is false).
 
-            if (disposing)
-            {
-                // Dispose managed state (managed objects).
-                disposables.Dispose();
-            }
+//        protected virtual void Dispose(bool disposing)
+//        {
+//            if (_disposed)
+//            {
+//                return;
+//            }
 
-            // Free unmanaged resources.
-            _disposed = true;
-        }
-        #endregion
-    }
-}
+//            if (disposing)
+//            {
+//                // Dispose managed state (managed objects).
+//                disposables.Dispose();
+//            }
+
+//            // Free unmanaged resources.
+//            _disposed = true;
+//        }
+//        #endregion
+//    }
+//}
