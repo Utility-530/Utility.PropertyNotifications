b4e9a7255ee010c1749a21fcd4d221842bdf6f00
dtaylor-530 <d.taylor530@gmail.com>
2025-10-19 22:13:06 +0100

    Utility.Common Moves RepositoryHelper to Utility.Extensions Moves Query to Utility.Persists

--- Common/Utility.Common/DataAccess/Queries.cs
+++ Common/Utility.Common/DataAccess/Queries.cs
@@ -1,63 +1,63 @@
-﻿using Utility.Enums;
-using Utility.Interfaces.NonGeneric.Data;
+﻿//using Utility.Enums;
+//using Utility.Interfaces.NonGeneric.Data;
 
-namespace Utility.Common
-{
-    public class FirstQuery : IQuery
-    {
-    }
+//namespace Utility.Common
+//{
+//    public class FirstQuery : IQuery
+//    {
+//    }
 
-    public class FirstOrDefaultQuery : IQuery
-    {
-    }
+//    public class FirstOrDefaultQuery : IQuery
+//    {
+//    }
 
-    public class AllQuery : IQuery
-    {
-    }
+//    public class AllQuery : IQuery
+//    {
+//    }
 
-    public class ContainsStringQuery : IQuery
-    {
-        public ContainsStringQuery(string text, string property)
-        {
-            Text = text;
-            Property = property;
-        }
+//    public class ContainsStringQuery : IQuery
+//    {
+//        public ContainsStringQuery(string text, string property)
+//        {
+//            Text = text;
+//            Property = property;
+//        }
 
-        public string Text { get; }
-        public string Property { get; }
-    }
+//        public string Text { get; }
+//        public string Property { get; }
+//    }
 
-    public class MatchesStringQuery : IQuery
-    {
-        public MatchesStringQuery(string text, string property, AbsoluteOrder absoluteOrder)
-        {
-            Text = text;
-            Property = property;
-            AbsoluteOrder = absoluteOrder;
-        }
+//    public class MatchesStringQuery : IQuery
+//    {
+//        public MatchesStringQuery(string text, string property, AbsoluteOrder absoluteOrder)
+//        {
+//            Text = text;
+//            Property = property;
+//            AbsoluteOrder = absoluteOrder;
+//        }
 
-        public string Text { get; }
-        public string Property { get; }
+//        public string Text { get; }
+//        public string Property { get; }
 
-        public AbsoluteOrder AbsoluteOrder { get; }
-    }
+//        public AbsoluteOrder AbsoluteOrder { get; }
+//    }
 
-    public class MatchesStringOrderQuery : IQuery
-    {
-        public MatchesStringOrderQuery(string text, string property, AbsoluteOrder absoluteOrder)
-        {
-            Text = text;
-            Property = property;
-            AbsoluteOrder = absoluteOrder;
-        }
+//    public class MatchesStringOrderQuery : IQuery
+//    {
+//        public MatchesStringOrderQuery(string text, string property, AbsoluteOrder absoluteOrder)
+//        {
+//            Text = text;
+//            Property = property;
+//            AbsoluteOrder = absoluteOrder;
+//        }
 
-        public string Text { get; }
-        public string Property { get; }
+//        public string Text { get; }
+//        public string Property { get; }
 
-        public AbsoluteOrder AbsoluteOrder { get; }
-    }
+//        public AbsoluteOrder AbsoluteOrder { get; }
+//    }
 
-    public class CountQuery : IQuery
-    {
-    }
-}
+//    public class CountQuery : IQuery
+//    {
+//    }
+//}
--- Persists/Utility.Persists/Infrastructure/Query.cs
+++ Persists/Utility.Persists/Infrastructure/Query.cs
@@ -1,6 +1,7 @@
-﻿using Utility.Interfaces.NonGeneric.Data;
+﻿using Utility.Enums;
+using Utility.Interfaces.NonGeneric.Data;
 
-namespace Utility.Persists.Infrastructure
+namespace Utility.Persists
 {
     public class MaxRowId : IQuery
     {
@@ -19,4 +20,61 @@ namespace Utility.Persists.Infrastructure
         public bool IsSuccess { get; }
     }
 
+    public class FirstQuery : IQuery
+    {
+    }
+
+    public class FirstOrDefaultQuery : IQuery
+    {
+    }
+
+    public class AllQuery : IQuery
+    {
+    }
+
+    public class ContainsStringQuery : IQuery
+    {
+        public ContainsStringQuery(string text, string property)
+        {
+            Text = text;
+            Property = property;
+        }
+
+        public string Text { get; }
+        public string Property { get; }
+    }
+
+    public class MatchesStringQuery : IQuery
+    {
+        public MatchesStringQuery(string text, string property, AbsoluteOrder absoluteOrder)
+        {
+            Text = text;
+            Property = property;
+            AbsoluteOrder = absoluteOrder;
+        }
+
+        public string Text { get; }
+        public string Property { get; }
+
+        public AbsoluteOrder AbsoluteOrder { get; }
+    }
+
+    public class MatchesStringOrderQuery : IQuery
+    {
+        public MatchesStringOrderQuery(string text, string property, AbsoluteOrder absoluteOrder)
+        {
+            Text = text;
+            Property = property;
+            AbsoluteOrder = absoluteOrder;
+        }
+
+        public string Text { get; }
+        public string Property { get; }
+
+        public AbsoluteOrder AbsoluteOrder { get; }
+    }
+
+    public class CountQuery : IQuery
+    {
+    }
 }
