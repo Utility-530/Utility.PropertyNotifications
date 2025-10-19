d0429bdf9b65ccfade470d9bc6eba7fe021d2427
dtaylor-530 <d.taylor530@gmail.com>
2025-10-19 22:34:03 +0100

    Utility.Attributes Adds ParamAttribute Adds SymbolAttribute

---
+++ Infrastructure/Utility.Attributes/ParamAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Attributes
+{
+    public class ParamAttribute(bool listenToCollectionChanges = false, int ratePerMinute = 60) : Attribute
+    {
+        public bool ListenToCollectionChanges { get; } = listenToCollectionChanges;
+        public int RatePerMinute { get; } = ratePerMinute;
+    }
+}
