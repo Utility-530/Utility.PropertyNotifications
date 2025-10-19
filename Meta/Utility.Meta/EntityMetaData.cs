2211a8b12b0a896657dd495e5e2646b476c10136
dtaylor-530 <d.taylor530@gmail.com>
2025-10-19 22:59:44 +0100

    Meta Adds EntityMetaData to Utility.Meta Updates

---
+++ Meta/Utility.Meta/EntityMetaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utility.Enums;
+
+namespace Utility.Meta
+{
+    public class EntityMetaData
+    {
+        public Type Type { get; set; }
+
+        public string TransformationMethod { get; set; }
+
+        public Guid Guid { get; set; }
+
+        public PropertyMetaData[] Properties { get; set; } 
+        public int Index { get; set; }
+    }
+
+    public class PropertyMetaData()
+    {
+
+        public string Name { get; set; }
+
+        public DataType DataType { get; set; }
+        public int ColumnWidth { get; set; }
+        public bool IsReadOnly { get; set; }
+    }
+}
