b4d147f9d62959cfe22180a28c430c7ca8a1b15d
dtaylor-530 <d.taylor530@gmail.com>
2025-10-19 22:36:01 +0100

    Utility.API.Entities Adds project Utility.API.Services Adds project

---
+++ Infrastructure/Utility.API.Services/Coinbase.cs
@@ -0,0 +1,184 @@
+﻿using Coinbase;
+using CsvHelper;
+using CsvHelper.Configuration;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Utility.API.Entities;
+using Utility.Attributes;
+using Utility.Entities;
+using Utility.Enums;
+using Utility.Helpers;
+using Utility.Services.Meta;
+
+namespace Utility.API.Services
+{
+    public class Coinbase
+    {
+        public record InputParam() : Param<Coinbase>(nameof(Coinbase.Import));
+        public record ReturnParam() : Param<Coinbase>(nameof(Coinbase.Import));
+
+        private const string Name = "Coinbase_CSV_Importer";
+        private const string CoinbaseTransactionImport = "Coinbase_Transaction_Import";
+        private static CoinbaseClient client;
+
+        public enum TransactionType
+        {
+            [Description("Reward Income")]
+            RewardIncome,
+            [Description("Advanced Trade Sell")]
+            AdvancedTradeSell,
+            [Description("Advanced Trade Buy")]
+            AdvancedTradeBuy,
+            [Description("Retail Unstaking Transfer")]
+            RetailUnstakingTransfer,
+            [Description("Withdrawal")]
+            Withdrawal,
+            [Description("Deposit")]
+            Deposit,
+            [Description("Pro Deposit")]
+            ProDeposit,
+            [Description("Pro Withdrawal")]
+            ProWithdrawal,
+        }
+
+        public class CoinbaseTransaction
+        {
+            [PrimaryKey]
+            public Guid ID { get; set; }
+            public DateTime Timestamp { get; set; }
+            public TransactionType TransactionType { get; set; }
+            public CryptoCoin Asset { get; set; }
+            public decimal QuantityTransacted { get; set; }
+            public Currency PriceCurrency { get; set; }
+            public decimal PriceAtTransaction { get; set; }
+            public decimal Subtotal { get; set; }
+            public decimal Total { get; set; }
+            public decimal FeesSpread { get; set; }
+            public string Notes { get; set; }
+        }
+
+        public sealed class TransactionMap : ClassMap<CoinbaseTransaction>
+        {
+            public TransactionMap()
+            {
+                Map(m => m.ID).Convert(args => parseID(args.Row));
+                Map(m => m.Timestamp).Convert(args => DateTime.Parse(args.Row.GetField("Timestamp").Replace(" UTC", ""), CultureInfo.InvariantCulture));
+                Map(m => m.TransactionType).Convert(args => parseTransactionType(args.Row));
+                Map(m => m.Asset).Convert(args => parseCryptoCoin(args.Row));
+                Map(m => m.QuantityTransacted).Name("Quantity Transacted");
+                Map(m => m.PriceCurrency).Convert(args => Utility.Helpers.EnumHelper.Parse<Currency>(args.Row["Price Currency"]));
+                Map(m => m.PriceAtTransaction).Convert(args => decimal.Parse(args.Row.GetField("Price at Transaction").Replace("£", ""), CultureInfo.InvariantCulture));
+                Map(m => m.Subtotal).Convert(args =>
+                    decimal.Parse(args.Row.GetField("Subtotal").Replace("£", ""), CultureInfo.InvariantCulture));
+                Map(m => m.Total).Convert(args =>
+                    decimal.Parse(args.Row.GetField("Total").Replace("£", ""), CultureInfo.InvariantCulture));
+                Map(m => m.FeesSpread).Convert(args =>
+                    decimal.Parse(args.Row.GetField("Fees_Spread").Replace("£", ""), CultureInfo.InvariantCulture));
+                Map(m => m.Notes).Name("Notes");
+            }
+
+            Guid parseID(IReaderRow field)
+            {
+                var raw = field.GetField("ID");
+                var padded = raw.PadRight(32, '0'); // Add trailing zeros
+                return Guid.ParseExact(padded, "N"); // No hyphens format
+
+            }
+            TransactionType parseTransactionType(IReaderRow field)
+            {
+                var raw = field.GetField("Transaction Type");
+                var padded = EnumHelper.MatchByAttribute<TransactionType, DescriptionAttribute>(a => a.Description == raw);
+                return padded; // No hyphens format
+            }
+            CryptoCoin parseCryptoCoin(IReaderRow field)
+            {
+                var raw = field.GetField("Asset");
+                var padded = EnumHelper.MatchByAttribute<CryptoCoin, DescriptionAttribute>(a => a.Description == raw);
+                return padded; // No hyphens format
+            }
+
+        }
+
+        public static Task Import()
+        {
+            return Task.Run(() =>
+            {
+                var csvFileDirectory = Path.Combine(Utility.Constants.DefaultDataPath, "Coinbase");
+                var connection = new SQLiteConnection(Utility.Constants.DefaultModelsFilePath);
+                connection.CreateTable<Event>();
+                connection.CreateTable<Client>();
+                connection.CreateTable<CoinbaseTransaction>();
+
+                var client = Utility.API.Services.Infrastructure.Helpers.InsertClient(connection, Name, default, default);
+                connection.RunInTransaction(() =>
+                {
+                    foreach (var file in Directory.EnumerateFiles(csvFileDirectory, "*.csv"))
+                    {
+                        var fileName = Path.GetFileName(file);
+                        if (connection.FindWithQuery<Event>($"SELECT * FROM {nameof(Event)} WHERE {nameof(Event.Source)} = ? AND {nameof(Event.Type)} = ?", fileName, CoinbaseTransactionImport) is { } ev)
+                        {
+                            return;
+                        }
+                        foreach (var transaction in readTransactionsFromCSV(file))
+                        {
+                            connection.Insert(transaction);
+                        }
+                        connection.Insert(new Event
+                        {
+                            Guid = Guid.NewGuid(),
+                            ParentId = client.Guid,
+                            Time = DateTime.Now,
+                            Source = fileName,
+                            Type = CoinbaseTransactionImport,
+                        });
+                    }
+                });
+            });
+
+            static IEnumerable<CoinbaseTransaction> readTransactionsFromCSV(string filePath)
+            {
+                using var reader = new StreamReader(filePath);
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HeaderValidated = null,
+                    MissingFieldFound = null
+                };
+
+                using var csv = new CsvReader(reader, config);
+                csv.Context.RegisterClassMap<TransactionMap>();
+                return csv.GetRecords<CoinbaseTransaction>().ToList();
+            }
+        }
+
+        [Param(ratePerMinute: 5)]
+        public record CryptoCoinParam() : Param<Coinbase>(nameof(Coinbase.SpotPrice), "coin");
+        public record CurrencyParam() : Param<Coinbase>(nameof(Coinbase.SpotPrice), "currency");
+        public record ReturnPriceParam() : Param<Coinbase>(nameof(Coinbase.SpotPrice));
+
+
+
+        public static async Task<decimal> SpotPrice(CryptoCoin coin, Currency currency)
+        {
+            //var client = new CoinbaseClient(new OAuthConfig { AccessToken = "..." });
+
+            ////using API Key + Secret authentication
+            //var client = new CoinbaseClient(new ApiKeyConfig { ApiKey = "...", ApiSecret = "..." });
+
+            //No authentication
+            //  - Useful only for Data Endpoints that don't require authentication.
+            client ??= new CoinbaseClient();
+            var spot = await client.Data.GetSpotPriceAsync(EnumHelper.GetDescription(coin) + "-" + currency.ToString());
+            return spot.Data.Amount;
+            //spot.Data.Amount.Should().BeGreaterThan(5);
+            //spot.Data.Currency.Should().Be("USD");
+            //spot.Data.Base.Should().Be("ETH");
+        }
+    }
+}
+
---
+++ Infrastructure/Utility.API.Services/Halifax.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using CsvHelper;
+using CsvHelper.Configuration;
+using SQLite;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Utility.API.Entities;
+using Utility.Entities;
+using Utility.Helpers;
+using Utility.Services.Meta;
+
+namespace Utility.API.Services
+{
+
+    public class Halifax
+    {
+        public record InputParam() : Param<Halifax>(nameof(Halifax.InsertData));
+        public record ReturnParam() : Param<Halifax>(nameof(Halifax.InsertData));
+
+
+
+        public const string localDataFile = @"O:\Users\rytal\Data\General.sqlite";
+        public const string halifaxDataFolder = @"O:\Users\rytal\Data\Halifax";
+        public static readonly Regex regex = new(@"\d{8}");
+        private static Transaction[] oldRecords;
+        public static string Type = "Halifax_CSV_Read";
+
+        public static void InsertData()
+        {
+            DateTime dateNow = DateTime.Now;
+            var repo = new SQLiteConnection(localDataFile);
+
+            repo.CreateTable<Client>();
+            repo.CreateTable<Transaction>();
+            repo.CreateTable<Event>();
+
+            var client = Utility.API.Services.Infrastructure.Helpers.InsertClient(repo, "Halifax_CSV_Importer", default, default);
+            foreach (var (file, name) in unMatchedFiles(repo))
+            {
+                Console.WriteLine(file);
+
+                var records = Csv.GetRecords(file);
+
+                Console.WriteLine("retrieved " + records.Length);
+
+                var newRecords = records.Except(oldRecords ??= repo.Table<Transaction>().ToArray()).ToArray();
+
+                repo.RunInTransaction(() =>
+                {
+                    int insert = repo.InsertAll(newRecords);
+
+                    repo.Insert(new Event
+                    {
+                        Guid = Guid.NewGuid(),
+                        ParentId = client.Guid,
+                        Time = dateNow,
+                        Source = name,
+                        Type = Type,
+
+                    });
+
+                    Console.WriteLine("inserted " + insert + " rows");
+                });
+            }
+            static IEnumerable<(string, string)> unMatchedFiles(SQLiteConnection repo) =>
+                from file in Directory.EnumerateFiles(halifaxDataFolder)
+                let date = File.GetCreationTime(file)
+                let name = Path.GetFileNameWithoutExtension(file)
+                join ev in repo.Table<Event>() on name equals ev.Source into gj
+                from ev in gj.DefaultIfEmpty() // left join
+                where ev == null && regex.IsMatch(name)
+                orderby date
+                select (file, name);
+        }
+
+
+        class Csv
+        {
+            public static Transaction[] GetRecords(string file)
+            {
+                using var reader = new StreamReader(file);
+                return toArray(reader);
+            }
+
+            //public static HalifaxRow[] GetRecords2(FileInfo file)
+            //{
+            //    using var read = file.OpenRead();
+            //    using var reader = new StreamReader(read);
+            //    return GetRecords3(reader);
+            //}
+
+            static Transaction[] toArray(StreamReader reader)
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HeaderValidated = null,
+                    MissingFieldFound = null,
+                    ReadingExceptionOccurred = ex =>
+                    {
+                        // Log or handle exception
+                        return true;
+                    }
+                };
+
+                using var csv = new CsvReader(reader, config);
+                csv.Context.RegisterClassMap<Map>();
+                return csv.GetRecords<Transaction>().ToArray();
+            }
+
+            sealed class Map : ClassMap<Transaction>
+            {
+                public Map()
+                {
+                    AutoMap(CultureInfo.InvariantCulture);
+                    Map(m => m.CreditAmount).Convert(row => row.Row["Credit Amount"] is { } c && c.Length > 0 ? (int?)(double.Parse(c) * 100) : (int?)null);
+                    Map(m => m.Balance).Convert(row => (int)(double.Parse(row.Row["Balance"]) * 100));
+                    Map(m => m.DebitAmount).Convert(row => row.Row["Debit Amount"] is { } c && c.Length > 0 ? (int?)(double.Parse(c) * 100) : (int?)null);
+                    Map(m => m.SortCode).Convert(row =>
+                    int.Parse(row.Row["Sort Code"].TakeDigits()));
+                    Map(m => m.AccountNumber).Convert(row => int.Parse(row.Row["Account Number"]));
+                    Map(m => m.Date).Convert(row => DateTime.Parse(row.Row["Transaction Date"]));
+                    Map(m => m.Description).Name("Transaction Description");
+                    Map(m => m.Type).Convert(row => row.Row["Transaction Type"] is { } s && s.Length == 0 ? TransactionType.None : (TransactionType)Enum.Parse(typeof(TransactionType), row.Row["Transaction Type"]));
+                }
+
+                //30/12/2019,DD,'11-08-52,00864878,NW WORLD MASTERCAR,36.92,,3115.81
+
+            }
+        }
+
+    }
+
+
+
+    static class Helper
+    {
+        private static readonly char[] Digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.' };
+
+        public static string TakeDigits(this string input) => string.Concat(input.Join(Digits, a => a, a => a, (a, b) => a == b ? a : char.MinValue));
+
+    }
+}
+
+
+
