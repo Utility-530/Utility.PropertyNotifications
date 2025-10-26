2ee00d666a7d5a0e7de16cfe8746e5ffa2a4f525
dtaylor-530 <d.taylor530@gmail.com>
2025-10-26 12:29:53 +0000

    Utility.Enums Adds CLREvent

---
+++ Utility.Enums/CLREvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Utility.Enums
+{
+    public enum CLREvent
+    {
+        None = 0,
+        [Description("INotifyCollectionChanged")]
+        CollectionChanged = 1,
+        [Description("INotifyPropertyChanged")]
+        PropertyChanged = 2,
+        CustomEvent = 4 
+
+    }
+}
