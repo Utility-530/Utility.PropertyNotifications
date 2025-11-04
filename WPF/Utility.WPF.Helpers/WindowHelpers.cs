c1620fdd6d3c8165a858e18fff7f13df9d5f9936
dtaylor-530 <d.taylor530@gmail.com>
2025-11-04 09:56:01 +0000

    Utility.WPF.Helpers Adds WindowHelpers Adds reference to WpfScreenHelper

---
+++ WPF/Utility.WPF.Helpers/WindowHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfScreenHelper;
+
+namespace Utility.WPF.Helpers
+{
+    public static class WindowHelpers
+    {
+        public static void ToLeft(this Window window)
+        {
+            // Find the leftmost screen
+            Screen leftmostScreen = Screen.AllScreens
+                .OrderBy(s => s.Bounds.Left)
+                .First();
+
+            // Get its working area (excluding taskbar)
+            var area = leftmostScreen.WorkingArea;
+            window.Left = area.Left;
+            window.Top = area.Top;
+            window.Width = area.Width;
+            window.Height = area.Height;
+        }
+        public static void ToRight(this Window window)
+        {
+            // Find the leftmost screen
+            Screen rightMostScreen = Screen.AllScreens
+                .OrderBy(s => s.Bounds.Right)
+                .First();
+
+            // Get its working area (excluding taskbar)
+            var area = rightMostScreen.WorkingArea;
+            window.Left = area.Left;
+            window.Top = area.Top;
+            window.Width = area.Width;
+            window.Height = area.Height;
+        }
+
+    }
+}
