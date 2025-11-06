65d031a82135bc723edaead47937384792633b42
dtaylor-530 <d.taylor530@gmail.com>
2025-11-06 16:29:07 +0000

    Utility.Sercices.Deprecated Readds CollectionService and ElementService

--- Infrastructure/Utility.Services.Deprecated/ElementService.cs
+++ Infrastructure/Utility.Services.Deprecated/ElementService.cs
@@ -1,58 +1,58 @@
-﻿//using System.Reactive.Linq;
-//using System.Reactive.Subjects;
-//using Utility.Common.Contract;
-//using Utility.Common.Helper;
-//using Utility.Helpers.NonGeneric;
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Utility.Extensions;
+using Utility.Common.Models;
 
-//namespace Utility.Services.Deprecated
-//{
-//    public class ElementService<T> : IObserver<RepositoryMessage> where T : IEquatable<T>
-//    {
-//        private readonly ReplaySubject<RepositoryMessage> subject = new(1);
-//        private readonly ReplaySubject<T?> itemSubject = new(1);
-//        private T? value;
 
-//        public ElementService()
-//        {
-//            subject
-//                .Take(1)
-//                .Subscribe(a =>
-//                {
-//                    value = a.Service.First<T>();
-//                });
+namespace Utility.Services.Deprecated
+{
+    public class ElementService<T> : IObserver<RepositoryMessage> where T : IEquatable<T>
+    {
+        private readonly ReplaySubject<RepositoryMessage> subject = new(1);
+        private readonly ReplaySubject<T?> itemSubject = new(1);
+        private T? value;
 
-//            subject
-//                .CombineLatest(itemSubject)
-//                .Subscribe(a =>
-//                {
-//                    if ((bool)a.First.Service.Update(a) == false)
-//                        a.First.Service.Add(a);
-//                });
-//        }
+        public ElementService()
+        {
+            subject
+                .Take(1)
+                .Subscribe(a =>
+                {
+                    value = a.Service.First<T>();
+                });
 
-//        public T? Value
-//        {
-//            get => value;
-//            set
-//            {
-//                this.value = value;
-//                itemSubject.OnNext(value);
-//            }
-//        }
+            subject
+                .CombineLatest(itemSubject)
+                .Subscribe(a =>
+                {
+                    if ((bool)a.First.Service.Update(a) == false)
+                        a.First.Service.Add(a);
+                });
+        }
 
-//        public void OnCompleted()
-//        {
-//            throw new NotImplementedException();
-//        }
+        public T? Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                itemSubject.OnNext(value);
+            }
+        }
 
-//        public void OnError(Exception error)
-//        {
-//            throw new NotImplementedException();
-//        }
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
 
-//        public void OnNext(RepositoryMessage value)
-//        {
-//            subject.OnNext(value);
-//        }
-//    }
-//}
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(RepositoryMessage value)
+        {
+            subject.OnNext(value);
+        }
+    }
+}
