0ad1216dc84203e62f0fb3fea54e1a4611076b52
dtaylor-530 <d.taylor530@gmail.com>
2025-11-06 17:38:52 +0000

    Utility.Nodify.Infrastructure Adds IConverter

---
+++ Nodify/Utility.Nodify.Infrastructure/Infrastructure/IConverter.cs
@@ -0,0 +1,14 @@
+﻿using Utility.Interfaces.Exs.Diagrams;
+using Utility.Nodify.Core;
+using Utility.Nodify.Entities;
+
+namespace Utility.Nodify.Base.Abstractions
+{
+    public interface IConverter
+    {
+        IDiagramViewModel Convert(Diagram diagram);
+        INodeViewModel Convert(Node node);
+        Diagram ConvertBack(IDiagramViewModel diagramViewModel);
+
+    }
+}
