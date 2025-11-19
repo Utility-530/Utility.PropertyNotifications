330a28ae7bbf72f60b7086a28671dc7929a5cbde
dtaylor-530 <d.taylor530@gmail.com>
2025-11-19 06:02:42 +0000

    Leeprfrog.WpfFramework Updates to .Net 8 project

--- External/Leepfrog.WpfFramework/AssemblyInfo.cs
+++ External/Leepfrog.WpfFramework/AssemblyInfo.cs
@@ -8,11 +8,7 @@ using System.Windows.Markup;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Leepfrog.WpfFramework")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Leepfrog.WpfFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -49,17 +45,3 @@ using System.Windows.Markup;
                                       //(used if a resource is not found in the page, 
                                       // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
