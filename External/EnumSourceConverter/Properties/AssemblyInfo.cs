f5bf26c57223b68fc575d122255c06221161a54b
dtaylor-530 <d.taylor530@gmail.com>
2025-11-19 10:11:01 +0000

    EnumSourceConverter Fixes AssemblyInfo

--- External/EnumSourceConverter/Properties/AssemblyInfo.cs
+++ External/EnumSourceConverter/Properties/AssemblyInfo.cs
@@ -6,11 +6,8 @@ using System.Windows.Markup;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EnumSourceConverter")]
 [assembly: AssemblyDescription("Binding to enum collections")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kea Software")]
-[assembly: AssemblyProduct("EnumSourceConverter")]
+//[assembly: AssemblyCompany("Kea Software")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -23,15 +20,3 @@ using System.Windows.Markup;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7377c762-7cd4-48e1-a5eb-344c72e80ff5")]
 [assembly: XmlnsDefinition("http://toniccomputing.com/patterns/ui", "Tonic.UI")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0")]
