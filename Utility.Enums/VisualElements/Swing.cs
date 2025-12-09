cf8ba3f4e83eefc80b7d39ece365bf9bae078ad2
dtaylor-530 <d.taylor530@gmail.com>
2025-12-09 12:04:36 +0000

    Utility.Enums Adds VisualElementAttribute Adds VisualElements folder Adds Visual and VisualLayout enums

---
+++ Utility.Enums/VisualElements/Swing.cs
@@ -0,0 +1,15 @@
+﻿namespace Utility.Enums.VisualElements
+{
+    public enum Swing
+    {
+        JPanel, JLabel, JTextField, JTextArea, JCheckBox, JRadioButton,
+        JComboBox, JButton, JList, JTable, JTabbedPane, JProgressBar,
+        JPasswordField,
+        JScrollPane,
+        JText,
+        JSlider,
+        JPopupMenu,
+        JMenuBar,
+        JMenuItem
+    }
+}
