f3ef77a081fc81411ef10555566b6b40f282a97e
dtaylor-530 <d.taylor530@gmail.com>
2025-12-09 11:55:45 +0000

    Utility.Enums Adds Rank enum

---
+++ Enums/Utility.Enums/Rank.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Utility.Enums
+{
+    public enum Rank
+    {
+        Primary,
+        Secondary,
+        Tertiary,
+        Quaternary,
+        Quinary,
+        Sextenary,
+        Septenary,
+        Octonary,
+        Nonary,
+        Denary 
+    }
+}
