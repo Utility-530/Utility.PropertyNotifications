515907cf99d25f67daaa7e7ae796698c075151b5
dtaylor-530 <d.taylor530@gmail.com>
2025-12-09 12:35:01 +0000

    Utility.Trees.Extensions \Async Refactors Match

--- Trees/Utility.Trees.Extensions/Async/Match.cs
+++ Trees/Utility.Trees.Extensions/Async/Match.cs
@@ -1,4 +1,5 @@
-﻿using System.Reactive.Disposables;
+﻿using System;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Utility.Changes;
 using Utility.Interfaces.Generic;
@@ -12,91 +13,105 @@ namespace Utility.Trees.Extensions.Async
 {
     public static class Match
     {
-        public static IObservable<TreeChange<IReadOnlyTree>> SelfAndDescendants(this IReadOnlyTree tree, Predicate<(IReadOnlyTree tree, int level)>? action = null, int level = 0)
+
+        public static IObservable<IReadOnlyTree> SelfAndDescendants(
+     this IReadOnlyTree tree,
+    Predicate<(IReadOnlyTree tree, int level)>? action = null,
+    int level = 0)
         {
-            return Observable.Create<TreeChange<IReadOnlyTree>>(observer =>
+            action ??= _ => true;
+
+            return Observable.Create<IReadOnlyTree>(observer =>
             {
-                CompositeDisposable disposables = new CompositeDisposable();
-                action ??= n => true;
-                if (action((tree, level)))
-                {
-                    observer.OnNext(new(tree, null, Type.Add, level));
-                }
-                else
-                {
-                }
-                level++;
+                var disposables = new CompositeDisposable();
 
-                tree.Children.AndAdditions<ITree>()
-                .Subscribe(item =>
-                {
-                    SelfAndDescendants(item, action, level)
-                    .Subscribe(x =>
-                    {
-                        observer.OnNext(x);
-                    }).DisposeWith(disposables);
-                }).DisposeWith(disposables);
+                observer.OnNext(tree);
 
-                tree.Children.Subtractions<ITree>()
-                .Subscribe(item =>
-                {
-                    observer.OnNext(new(item, null, Type.Remove, level));
-                }).DisposeWith(disposables);
+                // Subscribe to descendants
+                var changesSubscription = tree.Children
+                     .AndAdditions<ITree>()
+                     .Subscribe(item =>
+                     {
+                         SelfAndDescendants(item)
+                         .Subscribe(observer.OnNext);
+                     });
+
+
+                disposables.Add(changesSubscription);
+
+                //// Subscribe to children removals
+                //var subtractionsSubscription = tree.Children.Subtractions<ITree>()
+                //    .Subscribe(item =>
+                //        observer.OnNext(new TreeChange<IReadOnlyTree>(item, null, Type.Remove, level + 1))
+                //    );
+
+                //disposables.Add(subtractionsSubscription);
 
                 return disposables;
             });
         }
 
-        public static IObservable<IReadOnlyTree> SelfAndAncestors(this IReadOnlyTree tree, Predicate<(IReadOnlyTree tree, int level)>? action = null, int level = 0)
-        {
-            return Observable.Create<IReadOnlyTree>(observer =>
-            {
-                CompositeDisposable disposables = new();
-                action ??= n => true;
-                if (action((tree, level)))
-                {
-                    observer.OnNext(tree);
-                }
 
-                if ((tree as IGetParent<ITree>)?.Parent is ITree parent)
-                {
-                    return SelfAndAncestors(parent, action, level++)
-                        .Subscribe(a => observer.OnNext(a))
-                        .DisposeWith(disposables);
-                }
-                else
-                {
-                    if (tree is Tree changed)
-                        return changed.WithChangesTo(a => a.Parent)
-                        .Where(a => a != null)
-                        .Subscribe(p =>
-                        {
-                            SelfAndAncestors(p, action, level++)
-                            .Subscribe(a =>
-                            observer.OnNext(a))
-                            .DisposeWith(disposables);
-                        })
-                        .DisposeWith(disposables);
-                    else if ((tree as IGetParent<IReadOnlyTree>).Parent != null)
-                    {
-                        return SelfAndAncestors((tree as IGetParent<IReadOnlyTree>).Parent, action, level++)
-                      .Subscribe(a =>
-                      observer.OnNext(a));
-                    }
-                    else
-                    {
-                        throw new Exception("SD 333333 9d");
-                    }
-                }
-            });
-        }
+        //public static IObservable<TreeChange<IReadOnlyTree>> SelfAndDescendants(
+        //    this IReadOnlyTree tree,
+        //    Predicate<(IReadOnlyTree tree, int level)>? action = null,
+        //    int level = 0)
+        //{
+        //    action ??= _ => true;
 
-        public static IObservable<IReadOnlyTree> Ancestors(this IReadOnlyTree tree, Predicate<(IReadOnlyTree tree, int level)>? action = null)
-        {
-            return SelfAndAncestors((tree as IGetParent<IReadOnlyTree>).Parent, action);
-        }
+        //    return Observable.Create<TreeChange<IReadOnlyTree>>(observer =>
+        //    {
+        //        var disposables = new CompositeDisposable();
 
-        public static IObservable<TreeChange<IReadOnlyTree>> Descendants(this IReadOnlyTree tree, Predicate<(IReadOnlyTree tree, int level)>? action = null)
+        //        if (action((tree, level)))
+        //            observer.OnNext(new TreeChange<IReadOnlyTree>(tree, null, Type.Add, level));
+
+        //        // Subscribe to descendants
+        //        var descendantsSubscription = tree.Descendants(action, level + 1)
+        //            .Subscribe(observer.OnNext);
+
+        //        disposables.Add(descendantsSubscription);
+
+        //        //// Subscribe to children removals
+        //        //var subtractionsSubscription = tree.Children.Subtractions<ITree>()
+        //        //    .Subscribe(item =>
+        //        //        observer.OnNext(new TreeChange<IReadOnlyTree>(item, null, Type.Remove, level + 1))
+        //        //    );
+
+        //        //disposables.Add(subtractionsSubscription);
+
+        //        return disposables;
+        //    });
+        //}
+
+
+        //public static IObservable<IReadOnlyTree> Descendants(
+        //    this IReadOnlyTree tree,
+        //    Predicate<(IReadOnlyTree tree, int level)>? action = null,
+        //    int level = 1)
+        //{
+        //    action ??= _ => true;
+
+        //    return Observable.Create<IReadOnlyTree>(observer =>
+        //    {
+        //        var disposables = new CompositeDisposable();
+
+        //        var changesSubscription = tree.Children
+        //            .AndAdditions<ITree>()
+        //            .Subscribe(item =>
+        //            {
+                        
+        //                Match.SelfAndDescendants(item, action, level + 1)
+        //                .Subscribe(observer.OnNext);
+        //            });    
+        //    disposables.Add(changesSubscription);
+
+        //    return disposables;
+        //});
+        //}
+
+        public static IObservable<TreeChange<IReadOnlyTree>> Descendants(this IReadOnlyTree tree,
+            Predicate<(IReadOnlyTree tree, int level)>? action = null)
         {
             return Observable.Create<TreeChange<IReadOnlyTree>>(observer =>
             {
@@ -112,7 +127,7 @@ namespace Utility.Trees.Extensions.Async
                             SelfAndDescendants(item.Value, action, 1)
                             .Subscribe(x =>
                             {
-                                observer.OnNext(x);
+                                observer.OnNext(TreeChange<IReadOnlyTree>.Add(item.Value, 1));
                             }).DisposeWith(disposables);
                         }
                         else if (item.Type == Type.Remove)
@@ -130,9 +145,66 @@ namespace Utility.Trees.Extensions.Async
             });
         }
 
-        public static IObservable<TreeChange<IReadOnlyTree>> Descendant(this IReadOnlyTree tree, Predicate<(IReadOnlyTree tree, int level)>? action = null)
+        //public static IEnumerable<IReadOnlyTree> Descendant(
+        //    this IReadOnlyTree tree,
+        //    Predicate<(IReadOnlyTree tree, int level)>? action = null)
+        //{
+        //    return tree.Descendants(action).Take(1);
+        //}
+
+        public static IObservable<IReadOnlyTree> SelfAndAncestors(
+            this IReadOnlyTree tree,
+            Predicate<(IReadOnlyTree tree, int level)>? action = null,
+            int level = 0)
         {
-            return Descendants(tree, action).Take(1);
+            action ??= _ => true;
+
+            return Observable.Create<IReadOnlyTree>(observer =>
+            {
+                var disposables = new CompositeDisposable();
+
+                if (action((tree, level)))
+                    observer.OnNext(tree);
+
+                ITree? parent = null;
+
+                if (tree is IGetParent<ITree> treeWithParent)
+                    parent = treeWithParent.Parent;
+                else if (tree is IGetParent<IReadOnlyTree> treeWithReadonlyParent)
+                    parent = treeWithReadonlyParent.Parent as ITree;
+
+                if (parent != null)
+                {
+                    var parentSubscription = SelfAndAncestors(parent, action, level + 1)
+                        .Subscribe(observer.OnNext);
+
+                    disposables.Add(parentSubscription);
+                }
+                else if (tree is Tree changed)
+                {
+                    var changesSubscription = changed.WithChangesTo(a => a.Parent)
+                        .Where(p => p != null)
+                        .SelectMany(p => SelfAndAncestors(p, action, level + 1))
+                        .Subscribe(observer.OnNext);
+
+                    disposables.Add(changesSubscription);
+                }
+
+                return disposables;
+            });
+        }
+
+
+
+        public static IObservable<IReadOnlyTree> Ancestors(
+            this IReadOnlyTree tree,
+            Predicate<(IReadOnlyTree tree, int level)>? action = null)
+        {
+            ITree? parent = (tree as IGetParent<IReadOnlyTree>)?.Parent as ITree;
+            if (parent == null)
+                return Observable.Empty<IReadOnlyTree>();
+
+            return SelfAndAncestors(parent, action, 1);
         }
     }
 }
