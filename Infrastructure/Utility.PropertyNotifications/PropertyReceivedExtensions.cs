b50918e8b928ac3e3561edfe3abde8e07d7b8539
dtaylor-530 <d.taylor530@gmail.com>
2025-12-21 09:15:47 +0000

    Utility.PropertyNotifications Changes accessibility modifier of private classes in PropertyReceivedExtensions to public

--- Infrastructure/Utility.PropertyNotifications/PropertyReceivedExtensions.cs
+++ Infrastructure/Utility.PropertyNotifications/PropertyReceivedExtensions.cs
@@ -8,55 +8,57 @@ namespace Utility.PropertyNotifications
 
     public static partial class PropertyReceivedExtensions
     {
-        private class PropertyObservable : IObservable<PropertyReception>, IGetReference
-        {
-            private readonly INotifyPropertyReceived _target;
-            private readonly bool includeNulls;
-
-            public PropertyObservable(INotifyPropertyReceived target, bool includeNulls = true)
-            {
-                _target = target;
-                this.includeNulls = includeNulls;
-            }
-
-            public object Reference => _target;
-
-            private class Subscription : IDisposable
-            {
-                private readonly INotifyPropertyReceived _target;
-                private readonly IObserver<PropertyReception> _observer;
-                private readonly bool includeNulls;
-
-                public Subscription(INotifyPropertyReceived target, IObserver<PropertyReception> observer, bool includeNulls)
-                {
-                    _target = target;
-                    _observer = observer;
-                    this.includeNulls = includeNulls;
-                    _target.PropertyReceived += onPropertyReceived;
-                }
-
-                private void onPropertyReceived(object sender, PropertyReceivedEventArgs e)
-                {
-                    if (includeNulls || !Comparison.IsDefaultValue(e.Value))
-                        _observer.OnNext(new(e.Source ?? _target, e.Value, e.OldValue, e.PropertyName));
-                }
-
-                public void Dispose()
-                {
-                    _target.PropertyReceived -= onPropertyReceived;
-                    _observer.OnCompleted();
-                }
-            }
-
-            public IDisposable Subscribe(IObserver<PropertyReception> observer)
-            {
-                return new Subscription(_target, observer, includeNulls);
-            }
-        }
 
         public static IObservable<PropertyReception> WhenReceived<TModel>(this TModel model) where TModel : INotifyPropertyReceived
         {
             return new PropertyObservable(model);
+        }
+    }
+
+    public class PropertyObservable : IObservable<PropertyReception>, IGetReference
+    {
+        private readonly INotifyPropertyReceived _target;
+        private readonly bool includeNulls;
+
+        public PropertyObservable(INotifyPropertyReceived target, bool includeNulls = true)
+        {
+            _target = target;
+            this.includeNulls = includeNulls;
+        }
+
+        public object Reference => _target;
+
+        public IDisposable Subscribe(IObserver<PropertyReception> observer)
+        {
+            return new Subscription(_target, observer, includeNulls);
+        }
+    }
+
+
+    public class Subscription : IDisposable
+    {
+        private readonly INotifyPropertyReceived _target;
+        private readonly IObserver<PropertyReception> _observer;
+        private readonly bool includeNulls;
+
+        public Subscription(INotifyPropertyReceived target, IObserver<PropertyReception> observer, bool includeNulls)
+        {
+            _target = target;
+            _observer = observer;
+            this.includeNulls = includeNulls;
+            _target.PropertyReceived += onPropertyReceived;
+        }
+
+        private void onPropertyReceived(object sender, PropertyReceivedEventArgs e)
+        {
+            if (includeNulls || !Comparison.IsDefaultValue(e.Value))
+                _observer.OnNext(new(e.Source ?? _target, e.Value, e.OldValue, e.PropertyName));
+        }
+
+        public void Dispose()
+        {
+            _target.PropertyReceived -= onPropertyReceived;
+            _observer.OnCompleted();
         }
     }
 }
--- Infrastructure/Utility.PropertyNotifications/PropertyReceivedExtensions{T}.cs
+++ Infrastructure/Utility.PropertyNotifications/PropertyReceivedExtensions{T}.cs
@@ -8,14 +8,15 @@ namespace Utility.PropertyNotifications
 {
     public static partial class PropertyReceivedExtensions
     {
-        private class PropertyObservable<TTarget, T> : IObservable<T>, IGetReference where TTarget : INotifyPropertyReceived
+
+        private class PropertyObservable<TTarget, T> : PropertyObservable, IObservable<T>, IGetReference where TTarget : INotifyPropertyReceived
         {
             private readonly TTarget _target;
             private readonly PropertyInfo _info;
             private readonly bool includeInitial;
             private readonly bool includeNulls;
 
-            public PropertyObservable(TTarget target, PropertyInfo info, bool includeInitial = true, bool includeNulls = true)
+            public PropertyObservable(TTarget target, PropertyInfo info, bool includeInitial = true, bool includeNulls = true) : base(target, includeNulls)
             {
                 _target = target;
                 _info = info;
@@ -23,55 +24,11 @@ namespace Utility.PropertyNotifications
                 this.includeNulls = includeNulls;
             }
 
-            public object Reference => _target;
-
-            private class Subscription : IDisposable
-            {
-                private readonly INotifyPropertyReceived _target;
-                private readonly PropertyInfo _info;
-                private readonly Func<object, T> _getter;
-                private readonly IObserver<T> _observer;
-                private readonly bool includeNulls;
-
-                public Subscription(INotifyPropertyReceived target, PropertyInfo info, IObserver<T> observer, bool includeInitial = true, bool includeNulls = true)
-                {
-                    _target = target;
-                    _info = info;
-                    _getter = info.ToGetter<T>();
-                    _observer = observer;
-                    this.includeNulls = includeNulls;
-                    _target.PropertyReceived += onPropertyReceived;
-
-                    if (includeInitial)
-                    {
-                        var value = _getter.Invoke(_target);
-                        if (includeNulls || !Comparison.IsDefaultValue(value))
-                            _observer.OnNext(value);
-                    }
-                }
-
-                private void onPropertyReceived(object sender, PropertyReceivedEventArgs e)
-                {
-                    if (e.PropertyName == _info.Name)
-                    {
-                        var value = _getter.Invoke(e.Source ?? _target);
-                        if (includeNulls || !Comparison.IsDefaultValue(value))
-                        {
-                            _observer.OnNext(value);
-                        }
-                    }
-                }
-
-                public void Dispose()
-                {
-                    _target.PropertyReceived -= onPropertyReceived;
-                    _observer.OnCompleted();
-                }
-            }
+            public PropertyInfo PropertyInfo => _info;
 
             public IDisposable Subscribe(IObserver<T> observer)
             {
-                return new Subscription(_target, _info, observer, includeInitial, includeNulls);
+                return new Subscription<T>(_target, _info, observer, includeInitial, includeNulls);
             }
         }
 
@@ -88,4 +45,54 @@ namespace Utility.PropertyNotifications
             return new PropertyObservable<TModel, TRes>(model, prop, includeInitialValue, includeNulls);
         }
     }
+
+    public class Subscription<T> : IDisposable
+    {
+        private readonly INotifyPropertyReceived _target;
+        private readonly PropertyInfo _info;
+        private readonly Func<object, T> _getter;
+        private readonly Action<T, object> _setter;
+        private readonly IObserver<T> _observer;
+        private readonly bool includeNulls;
+        private T value;
+
+        public Subscription(INotifyPropertyReceived target, PropertyInfo info, IObserver<T> observer, bool includeInitial = true, bool includeNulls = true)
+        {
+            _target = target;
+            _info = info;
+            _getter = info.ToGetter<T>();
+            //_setter = info.ToSetter<T>();
+            _observer = observer;
+            this.includeNulls = includeNulls;
+            _target.PropertyReceived += onPropertyReceived;
+
+            if (includeInitial)
+            {
+                var value = _getter.Invoke(_target);
+                if (includeNulls || !Comparison.IsDefaultValue(value))
+                    _observer.OnNext(value);
+            }
+        }
+
+        private void onPropertyReceived(object sender, PropertyReceivedEventArgs e)
+        {
+            if (e.PropertyName == _info.Name)
+            {
+                var value = _getter.Invoke(e.Source ?? _target);
+                if (includeNulls || !Comparison.IsDefaultValue(value))
+                {
+                    _observer.OnNext(value);
+                }
+            }
+        }
+
+        public T Value { get => _getter.Invoke(_target); /*set => _setter.Invoke(value, _target);*/ }
+
+        public void Dispose()
+        {
+            _target.PropertyReceived -= onPropertyReceived;
+            _observer.OnCompleted();
+        }
+    }
+
 }
