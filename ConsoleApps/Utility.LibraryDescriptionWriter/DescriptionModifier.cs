5caa656ef8b17caf5e99db65ba06ca44078ff699
dtaylor-530 <d.taylor530@gmail.com>
2026-01-12 15:59:03 +0000

    ConsoleApps Adds Utility.LibraryDescriptionWriter

---
+++ ConsoleApps/Utility.LibraryDescriptionWriter/DescriptionModifier.cs
@@ -0,0 +1,75 @@
+﻿# nullable enable
+
+using Microsoft.Build.Evaluation;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ProjectFileEdit
+{
+    public class DescriptionCreator
+    {
+        public static string Create(Assembly assembly, Predicate<Type>? predicate = null, string? prependage = null, string? appendage = null)
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(prependage);
+
+            foreach (Type aType in assembly.GetTypes().Where(x => predicate?.Invoke(x) != false).OrderBy(a => a.Name))
+            {
+                stringBuilder.Append("- "+aType.Name + "\n");
+            }
+
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            stringBuilder.Append(appendage);
+
+            return stringBuilder.ToString();
+        }
+
+        public static string CreateForMarkdown(Assembly assembly, Predicate<Type>? predicate = null, string? prependage = null, string? appendage = null)
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(prependage);
+
+            foreach (Type aType in assembly.GetTypes().Where(x => predicate?.Invoke(x) != false).OrderBy(a => a.Name))
+            {
+                stringBuilder.Append("- " +aType.Name + Environment.NewLine);
+            }
+
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+            stringBuilder.Append(Environment.NewLine + appendage);
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    public class DescriptionModifier
+    {
+        public static string ModifyDescription(string description, FileInfo fileInfo)
+        {
+            var parent = fileInfo.Directory.Parent;
+            var name = System.IO.Path.ChangeExtension(fileInfo.Name, "csproj");
+            var file = parent.GetFiles(name, System.IO.SearchOption.AllDirectories).Single();
+
+            SetDescription(description, file.FullName);
+
+            return description;
+
+            static void SetDescription(string description, string projectPath)
+            {
+                MSBuildHelper.SetMsBuildExePath();
+
+                var project = new Project(projectPath);
+
+                project.SetProperty("Description", description);
+
+                project.Save();
+            }
+        }
+
+        public static Predicate<Type> EnumPredicate() => new Predicate<Type>(x => x.IsSubclassOf(typeof(Enum)));
+    }
+}
---
+++ ConsoleApps/Utility.LibraryDescriptionWriter/Program.cs
@@ -0,0 +1,84 @@
+﻿using ProjectFileEdit;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace UtilityEnum.App
+{
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            string assemblyPath = string.Empty;
+            if (args == null)
+            {
+                Console.WriteLine("Provide assembly path");
+                assemblyPath = Console.ReadLine();
+            }
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Provide assembly path");
+                assemblyPath = Console.ReadLine();
+            }
+            else if (args.Length > 1)
+            {
+
+            }
+            else
+            {
+                assemblyPath = args[0];
+            }
+
+
+            if (File.Exists(assemblyPath) == false)
+            {
+                Console.WriteLine("File not found: " + args[0]);
+                Console.ReadLine();
+            }
+
+            Assembly assembly = Assembly.LoadFrom(assemblyPath);
+            //Assembly assembly = Assembly.GetAssembly(typeof(YesNo));
+
+            string description = string.Empty;
+            if (assembly.GetName().Name == "Utility.Enums")
+            {
+                description = DescriptionCreator.Create(
+                    assembly,
+                    DescriptionModifier.EnumPredicate(),
+                    appendage: string.Empty);
+            }
+            Console.WriteLine(description);
+            Console.ReadLine();
+
+            //var lineSeparated = DescriptionCreator.CreateForMarkdown(
+            //    assembly,
+            //    DescriptionModifier.EnumPredicate(),
+            //    appendage: " enums." + Environment.NewLine + AppendDescription);
+            //Console.WriteLine(lineSeparated);
+
+            //Console.ReadLine();
+
+            void write(string description)
+            {
+                var dir = new System.IO.FileInfo(assembly.Location);
+                var parent = dir.Directory.Parent.Parent.Parent.Parent;
+                var name = System.IO.Path.ChangeExtension(dir.Name, "csproj");
+                var file = parent.GetFiles(name, System.IO.SearchOption.AllDirectories).Single();
+
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("modifying...");
+                try
+                {
+                    DescriptionModifier.ModifyDescription(description, file);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine("completed");
+                Console.ReadLine();
+            }
+        }
+    }
+}
