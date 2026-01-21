fbb2857821963fbdbefe269bba8625971ba42875
dtaylor-530 <d.taylor530@gmail.com>
2026-01-21 15:34:55 +0000

    Utility.WPF.Demo.Controls Removes DynamicData and ReactiveUI references

--- WPF/Utility.WPF.Demo.Controls/DataGridVirtualisationUserControl.xaml.cs
+++ WPF/Utility.WPF.Demo.Controls/DataGridVirtualisationUserControl.xaml.cs
@@ -2,8 +2,8 @@
 using System.Linq;
 using System.Reactive.Linq;
 using System.Windows.Controls;
-using DynamicData;
-using ReactiveUI;
+//using DynamicData;
+//using ReactiveUI;
 using Utility.WPF.Demo.Controls;
 
 namespace Utility.WPF.Demo.View
@@ -17,20 +17,20 @@ namespace Utility.WPF.Demo.View
         {
             InitializeComponent();
 
-            var dc2 = new ProfileCollectionVirtualise(this.Behavior2.WhenAny(a => a.FirstIndex, b => (b.Sender.FirstIndex, b.Sender.LastIndex, b.Sender.Size))
-                .Select(a => new VirtualRequest(a.FirstIndex, a.Size + 40))
-                .Skip(1)
-                .StartWith(new VirtualRequest(0, 60)), initialSize: 1000);
+            //var dc2 = new ProfileCollectionVirtualise(this.Behavior2.WhenAny(a => a.FirstIndex, b => (b.Sender.FirstIndex, b.Sender.LastIndex, b.Sender.Size))
+            //    .Select(a => new VirtualRequest(a.FirstIndex, a.Size + 40))
+            //    .Skip(1)
+            //    .StartWith(new VirtualRequest(0, 60)), initialSize: 1000);
 
-            this.Behavior2.WhenAny(a => a.FirstIndex, b => (b.Sender.FirstIndex, b.Sender.LastIndex, b.Sender.Size)).Subscribe(s =>
-              {
-                  var (first, last, size) = s;
-                  FirstIndexBox2.Text = first.ToString();
-                  LastIndexBox2.Text = last.ToString();
-                  SizeBox2.Text = size.ToString();
-              });
+            //this.Behavior2.WhenAny(a => a.FirstIndex, b => (b.Sender.FirstIndex, b.Sender.LastIndex, b.Sender.Size)).Subscribe(s =>
+            //  {
+            //      var (first, last, size) = s;
+            //      FirstIndexBox2.Text = first.ToString();
+            //      LastIndexBox2.Text = last.ToString();
+            //      SizeBox2.Text = size.ToString();
+            //  });
 
-            Grid2.DataContext = dc2;
+            //Grid2.DataContext = dc2;
         }
     }
 }
