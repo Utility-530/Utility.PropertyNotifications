84bd43c91bdd4fac7d0fa918b32a85b05779ae4f
dtaylor-530 <d.taylor530@gmail.com>
2026-01-21 11:13:29 +0000

    Utility.WPF.Converters Refactors FirstItemConverter into IndexItemConverter for greater control over the index being targeted

--- WPF/Utility.WPF.Converters/FirstItemConverter.cs
+++ WPF/Utility.WPF.Converters/FirstItemConverter.cs
@@ -3,21 +3,32 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using Utility.Helpers.NonGeneric;
 
 namespace Utility.WPF.Converters
 {
-    public class FirstItemConverter : IMultiValueConverter
+    public class FirstItemConverter : IndexItemConverter
+    {     
+    }
+
+    public class IndexItemConverter : IMultiValueConverter
     {
+        public int Index { get; set; }
+
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             ItemsControl itemscontrol = values[0] as ItemsControl;
             int count = itemscontrol.Items.Count;
-
-            if (values != null && values.Length == 2 && count > 0)
+            int index = Index;
+            if (values != null && values.Length >= 2 && count > 0)
             {
-                var itemContext = (values[1] as ContentPresenter).DataContext;
-                var lastItem = itemscontrol.Items[0];
-                return Equals(lastItem, itemContext);
+                if (values.Length == 3 && values[2] is int i)
+                {
+                    index = i;
+                }
+                var itemContext = (values[1] as FrameworkElement).DataContext;
+                var item = itemscontrol.ItemsSource.ElementAt(index);
+                return Equals(item, itemContext);
             }
 
             return DependencyProperty.UnsetValue;
