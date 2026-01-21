92d0239006505674cf8a8a5cf62645da4e74c3c0
dtaylor-530 <d.taylor530@gmail.com>
2026-01-21 11:56:16 +0000

    Utility.WPF.Demo.Lists Improves VirtualisationUserControl

--- WPF/Utility.WPF.Demo.Lists/VirtualisationUserControl.xaml.cs
+++ WPF/Utility.WPF.Demo.Lists/VirtualisationUserControl.xaml.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using Fasterflect;
+using LanguageExt.ClassInstances;
 using Microsoft.Xaml.Behaviors;
+using UnitsNet;
+using Utility.Collections;
 using Utility.PropertyNotifications;
+using Utility.Reactives;
+using Utility.WPF.Demo.Common.Infrastructure;
+using Utility.WPF.Demo.Common.ViewModels;
 using Utility.WPF.Demo.Data.Model;
 using Utility.WPF.Helpers;
 using Utility.WPF.Reactives;
@@ -23,31 +32,128 @@ namespace Utility.WPF.Demo.Lists
         {
             InitializeComponent();
         }
-
-        public IEnumerable Collection { get; }
     }
+
+    public sealed class ContactPlaceholder
+    {
+        public bool IsPlaceholder => true;
+    }
+
+    public class ContactsViewModel : INotifyPropertyChanged
+    {
+        private int firstVisible;
+        private int visibleCount;
+
+        public ExtendableWindowedCollection<Contact> Contacts { get; }
+
+        public ContactsViewModel()
+        {
+            Contacts = new ExtendableWindowedCollection<Contact>(Program.Contacts(), new ContactPlaceholder());
+        }
+
+        public int FirstVisible { get => firstVisible; set { firstVisible = value; UpdateVisibleRange(firstVisible, visibleCount); } }
+        public int VisibleCount { get => visibleCount; set { visibleCount = value; UpdateVisibleRange(firstVisible, visibleCount); } }
+
+        // Called by scroll tracking
+        public void UpdateVisibleRange(int first, int viewportSize)
+        {
+            Contacts.ShiftWindow(first, viewportSize);
+            // Optional: raise property changed if you bind directly
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+
 
     public class VirtualisationViewModel : NotifyPropertyClass
     {
         private int first, second;
+        private int previousFirst = -1, previousSecond = -1;
+        private List<Contact> collection;
+        private ObservableRangeCollection<object> virtualisedCollection = new();
 
         public VirtualisationViewModel()
         {
-            Collection = Finance.SelectSectors().SelectMany(a => a.Stocks)
-                .Concat(Finance.SelectSectors().SelectMany(a => a.Stocks))
-                .Concat(Finance.SelectSectors().SelectMany(a => a.Stocks))
-                .ToArray();
+            this.collection = Program.Contacts();
+            Length = collection.Count;
+            // Initialize with nulls
+            for (int i = 0; i < this.collection.Count; i++)
+                this.virtualisedCollection.Add(new Contact());
+
+            this.WithChangesTo(a => a.First).CombineLatest(this.WithChangesTo(a => a.Second))
+                .Subscribe(ab =>
+                {
+                    int newFirst = ab.First;
+                    int newSecond = ab.Second;
+
+                    // First time initialization
+                    if (previousFirst == -1 && previousSecond == -1)
+                    {
+                        var itemsToAdd = new List<(int index, Contact item)>();
+                        for (int i = newFirst; i <= newSecond && i < this.collection.Count; i++)
+                        {
+                            itemsToAdd.Add((i, this.collection[i]));
+                        }
+
+                        foreach (var (index, item) in itemsToAdd)
+                        {
+                            this.virtualisedCollection[index] = item;
+                        }
+                    }
+                    else
+                    {
+                        // Remove items that are no longer in range
+                        for (int i = previousFirst; i <= previousSecond && i < this.collection.Count; i++)
+                        {
+                            if (i < newFirst || i > newSecond)
+                            {
+                                this.virtualisedCollection[i] = new Contact();
+                            }
+                        }
+
+                        // Add items that are newly in range
+                        for (int i = newFirst; i <= newSecond && i < this.collection.Count; i++)
+                        {
+                            if (i < previousFirst || i > previousSecond)
+                            {
+                                this.virtualisedCollection[i] = this.collection[i];
+                            }
+                        }
+                    }
+
+                    previousFirst = newFirst;
+                    previousSecond = newSecond;
+                });
         }
 
-        public IEnumerable Collection { get; }
-
+        public int Length { get; set; }
+        public IEnumerable Collection => virtualisedCollection;
         public int First { get => first; set => RaisePropertyChanged(ref first, value); }
         public int Second { get => second; set => RaisePropertyChanged(ref second, value); }
     }
-
     public static class TrackVisibleItems
     {
         private static Panel[] panels;
+
+
+        public static readonly DependencyProperty LengthProperty =
+            DependencyProperty.RegisterAttached("Length", typeof(int), typeof(TrackVisibleItems), new PropertyMetadata(0, lengthChanged));
+
+
+        private static void lengthChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ItemsControl itemsControl && e.NewValue is int length)
+            {
+
+                // Trigger once at load
+                UpdateVisibleRange(itemsControl, length);
+
+            }
+        }
+
+        public static void SetLength(DependencyObject element, int value) => element.SetValue(LengthProperty, value);
+
+        public static int GetLength(DependencyObject element) => (int)element.GetValue(LengthProperty);
 
         public static readonly DependencyProperty EnabledProperty =
             DependencyProperty.RegisterAttached(
@@ -92,47 +198,55 @@ namespace Utility.WPF.Demo.Lists
                     if (scrollViewer != null)
                     {
                         scrollViewer.ScrollChanged += (s, args) =>
-                            UpdateVisibleRange(itemsControl);
+                            UpdateVisibleRange(itemsControl, GetLength(d));
 
                         // Trigger once at load
-                        UpdateVisibleRange(itemsControl);
+                        UpdateVisibleRange(itemsControl, GetLength(d));
                     }
                 };
             }
         }
 
-        private static async void UpdateVisibleRange(ItemsControl itemsControl)
+        private static async void UpdateVisibleRange(ItemsControl itemsControl, int length)
         {
-            panels ??= itemsControl.FindChildren<Panel>().ToArray();
+            if (panels == null || panels.Length == 0)
+                panels = itemsControl.ChildrenOfType<Panel>().ToArray();
 
-            if (panels.SingleOrDefault(a => a is VirtualizingStackPanel) is VirtualizingStackPanel vsp)
-                await Task.Run(() =>
-                {
-                    int first = (int)vsp.VerticalOffset;
-                    int visibleCount = (int)Math.Ceiling(vsp.ViewportHeight);
-                    int last = Math.Min(first + visibleCount - 1, itemsControl.Items.Count - 1);
+            //if (panels.SingleOrDefault(a => a is VirtualizingStackPanel) is VirtualizingStackPanel vsp)
 
-                    Application.Current.Dispatcher.InvokeAsync(() =>
-                    {
-                        SetFirstVisibleIndex(itemsControl, first);
-                        SetLastVisibleIndex(itemsControl, last);
-                    });
-                });
-            else if (panels.SingleOrDefault(a => a is StackPanel) is StackPanel stackPanel)
-                await Task.Run(() =>
-                {
-                    int first = (int)stackPanel.VerticalOffset;
-                    int visibleCount = (int)Math.Ceiling(stackPanel.ViewportHeight);
-                    int last = Math.Min(first + visibleCount - 1, itemsControl.Items.Count - 1);
+            //{
+            //    int first = (int)vsp.VerticalOffset;
+            //    int visibleCount = (int)Math.Ceiling(vsp.ViewportHeight);
+            //    int last = Math.Min(first + visibleCount - 1, length - 1);
 
-                    Application.Current.Dispatcher.InvokeAsync(() =>
-                    {
-                        SetFirstVisibleIndex(itemsControl, first);
-                        SetLastVisibleIndex(itemsControl, last);
-                    });
-                });
+
+
+            //    SetFirstVisibleIndex(itemsControl, first);
+            //    SetLastVisibleIndex(itemsControl, last);
+            //    //SetVirtualisedCollection(itemsControl, dsf.UpdateVisibleRangeOptimized(first, last));
+
+            //}
+            //;
+            if (panels.SingleOrDefault(a => a is StackPanel) is StackPanel stackPanel)
+
+            {
+                int first = (int)stackPanel.VerticalOffset;
+                int visibleCount = (int)Math.Ceiling(stackPanel.ViewportHeight);
+                int last = Math.Min(first + visibleCount, length);
+
+
+
+                SetFirstVisibleIndex(itemsControl, first);
+                SetLastVisibleIndex(itemsControl, last);
+                //SetVirtualisedCollection(itemsControl, dsf.UpdateVisibleRangeOptimized(first, last));
+
+            }
+            ;
         }
+
+
     }
+
 
     /// <summary>
     /// Returns the indices of rows in the DataGrid that are visible to the user
