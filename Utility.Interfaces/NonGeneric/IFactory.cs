c754f4a8764aa3bd97034c5cb38772f78c5d7efb
dtaylor-530 <d.taylor530@gmail.com>
2026-01-22 19:50:20 +0000

    Utility.Interfaces NonGeneric Adds ITaskFactory Adds IEnumerableFactory

--- Utility.Interfaces/NonGeneric/IFactory.cs
+++ Utility.Interfaces/NonGeneric/IFactory.cs
@@ -1,7 +1,20 @@
-﻿namespace Utility.Interfaces.NonGeneric
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Utility.Interfaces.NonGeneric
 {
     public interface IFactory
     {
         object Create(object config);
+    }
+    public interface ITaskFactory
+    {
+        Task<object> Create(object config);
+    }
+
+    public interface IEnumerableFactory
+    {
+        IEnumerable Create(object config);
     }
 }
