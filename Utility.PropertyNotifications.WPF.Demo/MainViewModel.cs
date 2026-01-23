35c47ba3d641fd068cd5c767394fd5fa726932d9
dtaylor-530 <d.taylor530@gmail.com>
2026-01-23 15:34:44 +0000

    UtilityPropertyNotifications.WPF.Demo Adds project

---
+++ Utility.PropertyNotifications.WPF.Demo/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Utility.PropertyNotifications.WPF.Demo
+{
+    public class MainViewModel : NotifyPropertyClass
+    {
+
+        private string _someProperty;
+        private string _otherProperty;
+
+        public string SomeProperty
+        {
+            get
+            {
+                RaisePropertyCalled(_someProperty);
+                return _someProperty;
+            }
+            set
+            {
+                RaisePropertyReceived(ref _someProperty, value);
+            }
+        }
+
+        public string OtherProperty
+        {
+            get => _otherProperty;
+            set
+            {
+                RaisePropertyChanged(ref _otherProperty, value);
+            }
+        }
+    }
+}
