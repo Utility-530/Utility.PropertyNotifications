a3825c9f4509a50600a3756b935bee824e97f124
dtaylor-530 <d.taylor530@gmail.com>
2026-01-23 15:34:44 +0000

    UtilityPropertyNotifications.WPF.Demo Adds project

---
+++ PropertyNotifications/Utility.PropertyNotifications.WPF.Demo/Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Utility.PropertyNotifications.WPF.Demo
+{
+    public class Service
+    {
+        public Service(MainViewModel mainViewModel, IDataStore dataStore)
+        {
+            mainViewModel
+                .WithChangesTo(a => a.OtherProperty, includeNulls: false, includeInitialValue: true, includeDefaultValues: true)
+                .CombineLatest(mainViewModel.WithChangesTo(a => a.SomeProperty))
+                .Subscribe(o =>
+                {
+                    Console.WriteLine(o.First + o.Second);
+                });
+
+            mainViewModel
+                .WhenReceivedFrom(a => a.SomeProperty, includeNulls: true, includeInitialValue: false, includeDefaultValues: true)
+                .Subscribe(value =>
+                {
+                    if (value == "name")
+                    {
+                        mainViewModel.RaisePropertyChanged(nameof(MainViewModel.SomeProperty));
+                    }
+                });
+
+            mainViewModel
+                .WhenCalledFrom(a => a.SomeProperty)
+                .Subscribe(async o =>
+                {
+                    var value = await dataStore.Value<string>(nameof(MainViewModel.SomeProperty));
+                    mainViewModel.SomeProperty = value;
+                });
+        }
+    }
+
+    public interface IDataStore
+    {
+        Task<T> Value<T>(string name);
+    }
+}
