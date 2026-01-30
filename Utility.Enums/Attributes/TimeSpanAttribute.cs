7126aa9f5672d7f1b14e31b25b3e7de4f4ddb1cf
dtaylor-530 <d.taylor530@gmail.com>
2026-01-30 09:59:42 +0000

    Utility.Enums Fixes issue with TimeSpanAttribute

--- Utility.Enums/Attributes/TimeSpanAttribute.cs
+++ Utility.Enums/Attributes/TimeSpanAttribute.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
 namespace Utility.Enums.Attributes
 {
     public class TimeSpanAttribute(string name, double interval) : Attribute
     {
         private static readonly Lazy<Type> type = new(() => typeof(TimeSpan));
-        private readonly Lazy<TimeSpan> timeSpan = new(() => (TimeSpan)type.Value.GetMethod(name).Invoke(null, [interval]));
+        private readonly Lazy<TimeSpan> timeSpan = new(() => (TimeSpan)type.Value.GetMethod(name, new[] { typeof(double) }).Invoke(null, [interval]));
 
         public string Name { get; } = name;
         public double Interval { get; } = interval;
