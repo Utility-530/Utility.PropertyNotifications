7de9f5c50626b7bce4d2b781a443eb3f2706aa31
dtaylor-530 <d.taylor530@gmail.com>
2026-02-12 10:03:45 +0000

    Utility.WPF.Demo.Sandbox Adds PathsWindow

---
+++ WPF/Utility.WPF.Demo.SandBox/PathsWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Utility.WPF.SandBox
+{
+    /// <summary>
+    /// Interaction logic for PathsWindow.xaml
+    /// </summary>
+    public partial class PathsWindow : Window
+    {
+        public PathsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
